be45818d30419d0a1755c095ae45a27213c87663
Tom Short <tshort@epri.com>
2010-05-08 21:05:26 +0000

    Initial check-in of integer date and time classes.

---
+++ R/IDateTime.R
@@ -0,0 +1,210 @@
+
+###################################################################
+# IDate -- a simple wrapper class around Date using integer storage
+###################################################################
+
+as.IDate <- function(x, ...) UseMethod("as.IDate")
+
+as.IDate.default <-
+    function(x, ...) as.IDate(as.Date(x, ...))
+
+as.IDate.Date <- function(x, ...) {
+    structure(as.integer(x), class=c("IDate","Date"))
+}    
+
+as.IDate.IDate <- function(x, ...) x
+
+as.Date.IDate <- function(x, ...) { 
+    structure(as.numeric(x), class="Date")
+}
+
+mean.IDate <-
+cut.IDate <-
+seq.IDate <-
+mean.IDate <-
+c.IDate <-
+rep.IDate <-
+split.IDate <-
+as.list.IDate <-    
+    function(x, ...) {
+        as.IDate(NextMethod())
+    }
+
+###################################################################
+# ITime -- Integer time-of-day class
+#          Stored as seconds in the day
+###################################################################
+
+as.ITime <- function(x, ...) UseMethod("as.ITime")
+
+as.ITime.default <- function(x, ...) {
+    as.ITime(as.POSIXlt(x, ...))
+}
+
+as.ITime.character <- function(x, format, ...) {
+    x <- unclass(x)
+    if (!missing(format)) 
+        return(as.ITime(strptime(x, format = format, ...)))
+    y <- strptime(x, format = "%H:%M:%OS", ...)
+    if (!is.na(y))
+        return(as.ITime(y))
+    y <- strptime(x, format = "%H:%M", ...)
+    return(as.ITime(y))
+}
+
+as.ITime.POSIXlt <- function(x, ...) {
+    structure(with(x, as.integer(sec) + min * 60L + hour * 3600L),
+              class = "ITime")
+}
+
+as.character.ITime <- format.ITime <- function(x, ...) {
+    # adapted from chron's format.times
+    x <- unclass(x)
+    hh <- x %/% 3600
+    mm <- (x - hh * 3600) %/% 60
+    ss <- trunc(x - hh * 3600 - 60 * mm)
+    paste(substring(paste("0", hh, sep = ""), nchar(paste(hh))), 
+          substring(paste("0", mm, sep = ""), nchar(paste(mm))), 
+          substring(paste("0", ss, sep = ""), nchar(paste(ss))), sep = ":")
+}
+
+print.ITime <- function(x, ...) {
+    print(format(x))
+}
+
+rep.ITime <- function (x, ...) 
+{
+    y <- rep(unclass(x), ...)
+    structure(y, class = "ITime")
+}
+
+"[.ITime" <- function(x, ..., drop = TRUE)
+{
+    cl <- oldClass(x)
+    class(x) <- NULL
+    val <- NextMethod("[")
+    class(val) <- cl
+    val
+}
+
+
+# create a data.table with IDate and ITime columns
+#   should work for most date/time formats like chron or POSIXct
+
+IDateTime <- function(x, ...) UseMethod("IDateTime")
+IDateTime.default <- function(x, ...) {
+    data.table(date = as.IDate(x), time = as.ITime(x))
+}
+
+# POSIXt support
+
+as.POSIXct.IDate <- function(x, time = 0, tz = "UTC", ...) {
+    as.POSIXct(as.POSIXlt(x), tz = tz) + time
+}
+
+as.POSIXct.ITime <- function(x, date = as.Date(Sys.time()), tz = "UTC", ...) {
+    as.POSIXct(as.POSIXlt(date), tz = tz) + x
+}
+
+as.POSIXlt.ITime <- function(x, ...) {
+    as.POSIXlt(as.POSIXct(x, ...))
+}
+
+# chron support
+
+as.chron.IDate <- function(x, time = NULL, ...) {
+    if (!is.null(time)) {
+        chron(dates. = as.chron(as.Date(x)), times. = as.chron(time))
+    } else {
+        chron(dates. = as.chron(as.Date(x)))
+    }    
+}
+
+as.chron.ITime <- function(x, date = NULL, ...) {
+    if (!is.null(date)) {
+        chron(dates. = as.chron(as.Date(date)), times. = as.chron(x))
+    } else {
+        chron(times. = as.character(x))
+    }    
+}
+
+as.ITime.times <- function(x, ...) {
+    x <- unclass(x)
+    daypart <- x - floor(x)
+    secs <- as.integer(round(daypart * 86400))
+    structure(secs,
+              class = "ITime")
+}
+
+###################################################################
+# Date - time extraction functions
+#   Adapted from Hadley Wickham's routines cited below to ensure
+#   integer results.
+#     http://gist.github.com/10238
+#   See also Hadley's more advanced and complex lubridate package:
+#     http://github.com/hadley/lubridate
+#   lubridate routines do not return integer values.
+###################################################################
+
+hour    <- function(x) as.POSIXlt(x)$hour
+yday    <- function(x) as.POSIXlt(x)$yday + 1L
+wday    <- function(x) as.POSIXlt(x)$wday + 1L
+mday    <- function(x) as.POSIXlt(x)$mday
+week    <- function(x) yday(x) %/% 7L + 1L
+month   <- function(x) as.POSIXlt(x)$mon + 1L
+quarter <- function(x) as.POSIXlt(x)$mon %/% 3L + 1L
+year    <- function(x) as.POSIXlt(x)$year + 1900L
+
+###################################################################
+# Examples
+###################################################################
+
+examples.IDateTime <- function() {
+
+    # create ITime:
+    (t <- as.ITime("10:45"))
+
+    is.integer(t)
+
+    (t <- as.ITime("10:45:04"))
+
+    (t <- as.ITime("10:45:04", format = "%H:%M:%S"))
+    
+    if (!identical(as.POSIXct("2001-01-01") + as.ITime("10:45"),
+                   as.POSIXct("2001-01-01 10:45") + 0)) print("test failed")
+
+    # create IDate:
+    (d <- as.IDate("2001-01-01"))
+
+    is.integer(d)
+
+    datetime <- seq(as.POSIXct("2001-01-01"), as.POSIXct("2001-01-03"), by = "5 hour")    
+    (a <- data.table(IDateTime(datetime), a = rep(1:2, 5), key = "a,date,time"))
+
+    a[, mean(a), by = "time"]
+    a[, mean(a), by = "date"]
+    
+    datetime <- seq(as.POSIXct("2001-01-01"), as.POSIXct("2001-01-03"), by = "6 hour")
+    (af <- data.table(IDateTime(datetime), a = rep(1:3, 3), key = "a,date,time"))
+    af[, mean(a), by = "time"] 
+    af[, mean(a), by = "wday = factor(weekdays(date))"] 
+    af[, mean(a), by = "wday = wday(date)"] 
+
+    as.POSIXct(af$date) 
+    as.POSIXct(af$date, time = af$time) 
+    as.POSIXct(af$date, af$time) 
+    as.POSIXct(af$date, time = af$time, tz = "GMT") 
+    
+    as.POSIXct(af$time, af$date)
+    as.POSIXct(af$time) # uses today's date
+    
+    if (require(chron)) {
+        as.chron(as.IDate("2000-01-01"))
+        as.chron(as.ITime("10:45"))
+        as.chron(as.IDate("2000-01-01"), as.ITime("10:45"))
+        as.chron(as.ITime("10:45"), as.IDate("2000-01-01"))
+        as.ITime(chron(times = "11:01:01"))
+        IDateTime(chron("12/31/98","10:45:00"))
+    }
+    
+}
