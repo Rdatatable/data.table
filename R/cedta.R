aebb1014687ea4c354a65dac5916cb8bd587e3a5
Matt Dowle <mdowle@mdowle.plus.com>
2010-09-29 21:37:38 +0000

    Fixed inheritance for packages which import data.table, bug #1093.

--- R/cedta.R
+++ R/cedta.R
@@ -1,12 +1,14 @@
 cedta = function() {
     # Calling Environment Data Table Aware
+
     te = topenv(parent.frame(2))
     identical(te,.GlobalEnv) ||
-    ("data.table" %in% loadedNamespaces() && identical(te,environment(data.table::data.table))) ||
-    (exists(".Depends",te,inherits=FALSE) && "data.table" %in% get(".Depends",te,inherits=FALSE))
-    # the check if data.table is in loadedNamespaces is needed because 'data.table::data.table' itself
-    # will load the namespace if not already loaded, and we don't want that.
-    # TO DO: or we could create .Depends in data.table itself and remove the middle line.
+    (isNamespace(te) &&
+        (getNamespaceName(te) == "data.table" ||
+         "data.table" %in% names(getNamespaceImports(te))))
+
+    #pkg = tryCatch(as.environment(paste("package:",name,sep="")),function(e){NULL})
+    #!is.null(pkg) && "data.table" %in% mget(".Depends",envir=pkg,ifnotfound=list(NULL))[[1]]
 }
 
 
