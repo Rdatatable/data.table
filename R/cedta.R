adee79677abaa5e4ce3c4ba6b9721e72f0dfe18c
Matt Dowle <mdowle@mdowle.plus.com>
2011-01-05 08:58:09 +0000

    + Tom's suggestion to remove identical(te,.GlobalEnv) for compatibility with test_that. Thread 'Streamlining unit tests with test_hat' with Steve L. + Fix for bug 1131 so that a debugger frame is considered data.table aware.

--- R/cedta.R
+++ R/cedta.R
@@ -1,11 +1,10 @@
 cedta = function() {
     # Calling Environment Data Table Aware
-
     te = topenv(parent.frame(2))
-    identical(te,.GlobalEnv) ||
-    (isNamespace(te) &&
-        (getNamespaceName(te) == "data.table" ||
-         "data.table" %in% names(getNamespaceImports(te))))
+    !isNamespace(te) ||   # .GlobalEnv has no ns => dt-aware
+    getNamespaceName(te) == "data.table" ||
+    "data.table" %in% names(getNamespaceImports(te)) ||
+    (getNamespaceName(te) == "utils" && exists("debugger.look",parent.frame(3)))  # fixes bug #1131 
 
     #pkg = tryCatch(as.environment(paste("package:",name,sep="")),function(e){NULL})
     #!is.null(pkg) && "data.table" %in% mget(".Depends",envir=pkg,ifnotfound=list(NULL))[[1]]
