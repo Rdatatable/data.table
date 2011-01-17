fca324859efb9a2008d0da50764a8991e8760f0f
Matt Dowle <mdowle@mdowle.plus.com>
2011-01-17 23:01:40 +0000

    Minor tidying to merge e.g. using key, key<- and single name i's.

--- R/merge.R
+++ R/merge.R
@@ -3,19 +3,17 @@ merge.data.table <- function(x, y, all = FALSE, all.x = all, all.y = all, ...) {
     # Relatively quick merge by common keys
     # Keys must be in the same order
 
-    key1 <- attr(x, "sorted")
-    key2 <- attr(y, "sorted")
-    keylen <- min(length(key1), length(key2))
-    if (keylen == 0 || !all(key1[1:keylen] == key2[1:keylen])) stop("data table keys don't match")
-    key <- key1[1:keylen]
-    ykey <- J(y[,key,with=FALSE])
+    keylen <- min(length(key(x)), length(key(y)))
+    if (keylen == 0 || !all(key(x)[1:keylen] == key(y)[1:keylen])) stop("data table keys don't match")
+    key <- key(x)[1:keylen]
+    ykey <- y[,key,with=FALSE]
     xidx <- x[ykey, nomatch = 0, mult = 'all', which = TRUE]
-    xkey <- J(x[,key,with=FALSE])
+    xkey <- x[,key,with=FALSE]
     yidx <- y[xkey, nomatch = 0, mult = 'all', which = TRUE]
 
-    
     if (any(xidx)) {
-        xx <- x[sort(xidx)]
+        tmp = sort(xidx)
+        xx <- x[tmp]
         yy <- y[yidx, setdiff(names(y), key), with = FALSE]
         dt <- xx
         if (ncol(yy) > 0)  dt <- cbind(xx, yy)
@@ -26,9 +24,10 @@ merge.data.table <- function(x, y, all = FALSE, all.x = all, all.y = all, ...) {
         missingxidx <- setdiff(1:nrow(x), xidx)
         if (length(missingxidx) > 0) {
             othercols <- setdiff(names(y), key)
-            if (length(othercols) > 0)
-                xx <- cbind(x[missingxidx], y[NA, othercols, with = FALSE][rep(1, length(missingxidx))])
-            else 
+            if (length(othercols) > 0) {
+                tmp = rep(1, length(missingxidx))
+                xx <- cbind(x[missingxidx], y[NA, othercols, with = FALSE][tmp])
+            } else 
                 xx <- x[missingxidx]
             dt <- rbind(xx, dt)
         }
@@ -38,16 +37,17 @@ merge.data.table <- function(x, y, all = FALSE, all.x = all, all.y = all, ...) {
         if (length(missingyidx) > 0) {
             yy <- y[missingyidx, key, with = FALSE]
             othercolsx <- setdiff(names(x), key)
-            if (length(othercolsx) > 0)
-                yy <- cbind(yy, x[NA, othercolsx, with = FALSE][rep(1, length(missingyidx))])
+            if (length(othercolsx) > 0) {
+                tmp = rep(1, length(missingyidx))
+                yy <- cbind(yy, x[NA, othercolsx, with = FALSE][tmp])
+            }
             othercolsy <- setdiff(names(y), key)
             if (length(othercolsy) > 0) 
                 yy <- cbind(yy, y[missingyidx, othercolsy, with = FALSE])
             dt <- rbind(dt, yy)
         }
     }
-    if (nrow(dt) > 0)
-        eval(parse(text=paste("setkey(dt,", paste(key, collapse=","), ")",sep="")))
+    if (nrow(dt) > 0) key(dt) = key
     dt
 }
 
