3089994524148e3e219ee07567e4eaaefafa02a4
Matt Dowle <mdowle@mdowle.plus.com>
2011-02-16 21:08:41 +0000

    + Added link to IDateTime in 'see also' section of ?data.table. + Minor fixes to FAQ and ?data.table. + Added ?data.table to banner (apparently people install packages from CRAN, know to use library() but are unsure what to do next, so the banner tells them quickly).

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,6 @@
 ".onAttach"=function(libname, pkgname) {
     cat("Quick start guide : vignette(\"datatable-intro\")\n")
     cat("Homepage : http://datatable.r-forge.r-project.org/\n")
+    cat('Help : help("data.table") or ?data.table\n')
 }
 
