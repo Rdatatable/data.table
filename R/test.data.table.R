2d770c84ac5958e43950e00c4894a1273d498eb3
Matt Dowle <mdowle@mdowle.plus.com>
2011-02-25 20:54:51 +0000

    Comment removed from test.data.table; already written more briefly at the top of tests.R

--- R/test.data.table.R
+++ R/test.data.table.R
@@ -4,9 +4,6 @@ test.data.table = function() {
         # package developer
         d = path.expand("~/R/datatable/pkg/tests")
         assign(".devtesting",TRUE,envir=.GlobalEnv)
-        # ggplot takes over 10 seconds for the two 2 simple tests and slows me down
-        # since I run test.data.table from my cc() funcion and run it a lot. The
-        # ggplot tests will be run by R CMD check only.
     } else {
         # user
         d = getNamespaceInfo("data.table","path")
