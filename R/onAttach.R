c79439da87a5781fd31638260bec48405e28d106
Matt Dowle <mdowle@mdowle.plus.com>
2011-02-25 19:54:56 +0000

    + Added datatable version to banner + Moved stat counter in .Rd to avoid check warning about /if in examples. (about to rearrange test framework ...)

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,4 +1,5 @@
 ".onAttach"=function(libname, pkgname) {
+    cat("data.table",as.character(packageVersion("data.table")),"\n")
     cat("Quick start guide : vignette(\"datatable-intro\")\n")
     cat("Homepage : http://datatable.r-forge.r-project.org/\n")
     cat('Help : help("data.table") or ?data.table\n')
