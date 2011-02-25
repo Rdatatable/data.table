5af14753642f75e650d14cc4893fdf9e15e68a0d
Steve Lianoglou <slianoglou@gmail.com>
2011-02-25 04:23:55 +0000

    Added S4 stuff along with skeleton documentation.
    Fixed usage section in man page for merge.data.frame.
    
    The S4 addition doesn't break anything, and once we figure out best way to test it we can advertise it better by adding it to vignettes and cross-linking the S4 documentation from within the See Also section of ?data.table.
    
    I just wanted to get it into SVN for now.

---
+++ R/AllS4.R
@@ -0,0 +1,16 @@
+## Functions to let data.table play nicer with S4
+
+## Allows data.table to be defined as an object of an S4 class,
+## or even have data.table be a super class of an S4 class.
+setOldClass(c('data.frame'))
+setOldClass(c('data.table', 'data.frame'))
+
+## as(some.data.frame, "data.table")
+setAs("data.frame", "data.table", function(from) {
+  as.data.table(from)
+})
+
+## as(some.data.table, "data.frame")
+setAs("data.table", "data.frame", function(from) {
+  as.data.frame(from)
+})
