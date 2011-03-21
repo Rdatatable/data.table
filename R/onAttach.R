3ec49980850bcb1efbdf78f7b5dd8be14486d0e5
Matt Dowle <mdowle@mdowle.plus.com>
2011-03-21 21:28:07 +0000

    + Added "FR#1315" to NEWS for when it becomes a link in NEWS.Rd. + Minor addition to banner pointing users to examples in ?data.table.

--- R/onAttach.R
+++ R/onAttach.R
@@ -2,6 +2,6 @@
     cat("data.table",as.character(packageVersion("data.table")),"\n")
     cat("Quick start guide : vignette(\"datatable-intro\")\n")
     cat("Homepage : http://datatable.r-forge.r-project.org/\n")
-    cat('Help : help("data.table") or ?data.table\n')
+    cat('Help : help("data.table") or ?data.table (includes fast start examples)\n')
 }
 
