52f09b24ec29f8cadf24f77f4d9fad7acb4abaf4
Matt Dowle <mdowle@mdowle.plus.com>
2011-04-28 20:51:13 +0000

    Added first steps in fast file reader straight into data.table. See comments in read.R and readfile.c.  The test in read.R is working and demonstrates a 4 times speedup so far.  See comments regarding columns 11 and 12 of BED format (thanks to the blog post recently). Perhaps, data.table for genomics.

---
+++ R/read.R
@@ -0,0 +1,46 @@
+
+read = function(fnam="test.csv",primary=",") {   #,secondary=" ")
+    ans = .Call("readfile",fnam)
+    nr = length(ans[[1]])
+    attr(ans,"row.names") = .set_row_names(nr)
+    attr(ans, "class") <- c("data.table","data.frame")
+    names(ans) = paste("V",1:length(ans),sep="")
+    ans
+}
+
+if (FALSE) {
+# test speedup so far
+n=1e6
+test = data.table(a=sample(1:1000,n,replace=TRUE), b=sample(1:1000,n,replace=TRUE))
+write.table(test,"test.csv",sep=",",row.names=FALSE,col.names=FALSE)
+
+system.time(xx <- read.csv("test.csv",header=FALSE))
+system.time(yy <- read("test.csv"))
+identical(yy$V1,xx$V1)  # TRUE
+identical(yy$V2,xx$V2)  # TRUE
+
+# with n=1e6 I got :
+#> system.time(xx <- read.csv("out.csv",header=FALSE))
+#   user  system elapsed 
+#  5.388   0.092   5.486 
+#> system.time(yy <- read("out.csv"))
+#   user  system elapsed 
+#  1.276   0.052   1.329    
+#
+# That's only a 7.5MB file on disk, so perhaps a larger file, and more
+# columns (because that would be no more calls to fscanf), would
+# be a greater speedup. Especially the BED format (columns 11 and 12)
+# seems well matched for data.table's features with list() columns.
+# Also see comments in readfile.c
+}
+
+# TO DO: 
+#    use read.table on first 10 rows to get column names and types
+#    estimate number of rows using file size and info from first 10 rows
+#    allocate estimated rows + 5% in C, once. Even a memcpy afterwards
+#    to the precise number of rows would be faster than incrementally
+#    allocated memory as the scan progresses, or just leave the vectors
+#    slightly over allocated until they are copied anyway by something else.
+#    add the secondary separator for list() columns with vector cell
+#    allocation in C. Really, not difficult at all.
+
---
+++ src/readfile.c
@@ -0,0 +1,57 @@
+#include <R.h>
+#define USE_RINTERNALS
+#include <Rinternals.h>
+#include <Rdefines.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#ifdef BUILD_DLL
+// For Windows only
+#define EXPORT __declspec(dllexport)
+EXPORT SEXP readfile();
+#endif
+
+SEXP readfile(SEXP fnam)
+{
+    SEXP c1,c2,ans;
+    void *p[64];
+    int i, res, nrow=0;
+    FILE *f=fopen(CHAR(STRING_ELT(fnam,0)),"r");
+    if (f == NULL) error("file not found");
+    ans=PROTECT(allocVector(VECSXP,2));  // 2 column data.table, first steps
+    c1=PROTECT(allocVector(INTSXP,1000000));  // hard coded to match n=1e6 test up in read.R
+    c2=PROTECT(allocVector(INTSXP,1000000));
+    SET_VECTOR_ELT(ans,0,c1);
+    SET_VECTOR_ELT(ans,1,c2);
+    p[0] = DATAPTR(c1);
+    p[1] = DATAPTR(c2);
+    char format[100]="%d,%d\n";
+    while (fscanf(f,format, p[0], p[1], p[2], p[3])!=EOF) {
+        // Unused and uninitialized p[2], p[3] (, ..., p[64]
+        // to be added) is deliberate.
+        // It isn't possible in C to construct a vargs
+        // and pass it to fscanf in C or C99. A '...' can
+        // be passed on, but not created unless we depend
+        // on a specialist library or go to asm.
+        // Living with a, say, 64 column limit, seems fine for many
+        // cases. Or, perhaps fscanf could be called
+        // twice per row to read in file with 128 columns, etc.
+        // Assumption is that fscanf is optimised internally for
+        // navigating the format, and is faster than character by
+        // character code with branching and function calls at the
+        // C level (such as in scan.c).
+        // Already with only 2 columns we see a 4 times speedup
+        // and perhaps with more columns this factor will be greater.
+        nrow++;
+        for (i=0;i<2;i++) p[i]+=4;
+        // TO DO: secondary separator for list() columns, such as
+        // columns 11 and 12 in BED.
+    }
+    fclose(f);
+    LENGTH(c1) = nrow;
+    LENGTH(c2) = nrow;
+    UNPROTECT(3);
+    return(ans);
+}
+
+
