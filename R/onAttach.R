4ff443dbc7553dbe6452d3d160e792dd13abd866
Steve Lianoglou <slianoglou@gmail.com>
2011-04-29 20:00:23 +0000

    Silenced data.table 'intro/banner' .onAttach when R is not being run interactively.
    Loading data.table in a Sweave document was (previously) dumping this message
    into the generated tex/pdf file ...

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,7 +1,9 @@
 ".onAttach"=function(libname, pkgname) {
-    cat("data.table",as.character(packageVersion("data.table")),"\n")
-    cat("Quick start guide : vignette(\"datatable-intro\")\n")
-    cat("Homepage : http://datatable.r-forge.r-project.org/\n")
-    cat('Help : help("data.table") or ?data.table (includes fast start examples)\n')
+    if (interactive()) {
+        cat("data.table",as.character(packageVersion("data.table")),"\n")
+        cat("Quick start guide : vignette(\"datatable-intro\")\n")
+        cat("Homepage : http://datatable.r-forge.r-project.org/\n")
+        cat('Help : help("data.table") or ?data.table (includes fast start examples)\n')
+    }
 }
 
