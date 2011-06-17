6d50af0b45315ab62920671d923ddc244ee61c0a
Matt Dowle <mdowle@mdowle.plus.com>
2011-06-17 00:04:27 +0000

    onAttach now uses packageStartupMessage so the banner can be suppressed by those annoyed by banners, whilst still being helpful to new users

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,9 +1,9 @@
 ".onAttach"=function(libname, pkgname) {
     if (interactive()) {
-        cat("data.table",as.character(packageVersion("data.table")),"\n")
-        cat("Quick start guide : vignette(\"datatable-intro\")\n")
-        cat("Homepage : http://datatable.r-forge.r-project.org/\n")
-        cat('Help : help("data.table") or ?data.table (includes fast start examples)\n')
+        packageStartupMessage("data.table ",as.character(packageVersion("data.table")))
+        packageStartupMessage("Quick start guide : vignette(\"datatable-intro\")")
+        packageStartupMessage("Homepage : http://datatable.r-forge.r-project.org/")
+        packageStartupMessage('Help : help("data.table") or ?data.table (includes fast start examples)')
     }
 }
 
