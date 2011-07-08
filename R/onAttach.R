f460e13d235a05c10c4cb46a24dc8bf4f356668d
Matt Dowle <mdowle@mdowle.plus.com>
2011-07-08 00:55:30 +0000

    Banner now checks to see if newer version is available.

--- R/onAttach.R
+++ R/onAttach.R
@@ -4,6 +4,17 @@
         packageStartupMessage("Quick start guide : vignette(\"datatable-intro\")")
         packageStartupMessage("Homepage : http://datatable.r-forge.r-project.org/")
         packageStartupMessage('Help : help("data.table") or ?data.table (includes fast start examples)')
+        try({
+            # Check if later version is available
+            setTimeLimit(elapsed=1,transient=TRUE)
+            suppressWarnings(download.file("http://c.statcounter.com/7025334/0/7603caf6/0/",tt<-tempfile(),quiet=TRUE,cacheOK=FALSE))
+            latestv = readBin(tt,1L,47,size=1,signed=FALSE)[42:47]
+            unlink(tt)
+            if (!identical(latestv[1:3],latestv[4:6])) return() else latestv=paste(latestv[1:3],collapse=".")
+            if (packageVersion("data.table") < latestv) {
+                packageStartupMessage("*** Version ",latestv," is released. Please upgrade by typing update.packages() ***")
+            }
+        },TRUE)
     }
 }
 
