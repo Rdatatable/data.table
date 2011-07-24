95cb17af87c1ea9f9cdbded7070f4ee8c638eeab
Matt Dowle <mdowle@mdowle.plus.com>
2011-07-24 23:07:56 +0000

    + Replaced "New user?" in banner with "For help type:". + Minor typos fixed in .Rd and .Rnw

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,6 @@
 ".onAttach"=function(libname, pkgname) {
     if (interactive()) {
-        packageStartupMessage('data.table ',as.character(packageVersion("data.table")),' New user? Type help("data.table")')
+        packageStartupMessage('data.table ',as.character(packageVersion("data.table")),'  For help type: help("data.table")')
     }
 }
 
