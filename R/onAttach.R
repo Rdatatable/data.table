6b10cc21b05a90a92cfd61f79ff65fcca224694b
Steve Lianoglou <slianoglou@gmail.com>
2012-03-31 20:55:58 +0000

    Moved data.table package setup mojo from onAttach to onLoad
    The default option values were not being set when data.table was simply
    imported by a package when this code lived in .onAttach.
    
    Should fix #1916.

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,43 +1,5 @@
-".onAttach"=function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
     if (interactive()) {
         packageStartupMessage('data.table ',as.character(packageVersion("data.table")),'  For help type: help("data.table")')
     }
-    "Please read FAQ 2.23 (vignette('datatable-faq')) which explains in detail why data.table adds one line to base::cbind.data.frame and base::rbind.data.frame. If there is a better solution we will gladly change it."
-    # Commented as a character string so this message is retained and seen by anyone who types data.table:::.onAttach
-    tt = base::cbind.data.frame
-    ss = body(tt)
-    if (class(ss)!="{") ss = as.call(c(as.name("{"), ss))
-    if (!length(grep("data.table",ss[[2]]))) {
-        ss = ss[c(1,NA,2:length(ss))]
-        ss[[2]] = parse(text="if (inherits(..1,'data.table')) return(data.table(...,key=key(..1)))")[[1]]
-        body(tt)=ss
-        (unlockBinding)("cbind.data.frame",baseenv())
-        assign("cbind.data.frame",tt,envir=asNamespace("base"),inherits=FALSE)
-        lockBinding("cbind.data.frame",baseenv())
-    }
-    tt = base::rbind.data.frame
-    ss = body(tt)
-    if (class(ss)!="{") ss = as.call(c(as.name("{"), ss))
-    if (!length(grep("data.table",ss[[2]]))) {
-        ss = ss[c(1,NA,2:length(ss))]
-        ss[[2]] = parse(text="if (inherits(..1,'data.table')) return(`.rbind.data.table`(...))")[[1]]
-        body(tt)=ss
-        (unlockBinding)("rbind.data.frame",baseenv())
-        assign("rbind.data.frame",tt,envir=asNamespace("base"),inherits=FALSE)
-        lockBinding("rbind.data.frame",baseenv())
-    }
-    "Please read FAQ 2.23 (vignette('datatable-faq')) which explains in detail why data.table adds one line to base::cbind.data.frame and base::rbind.data.frame. If there is a better solution we will gladly change it."
-    # Commented as a character string so this message is retained and seen by anyone who types data.table:::.onAttach
-    
-    # Set options for the speed boost in v1.8.0 by avoiding 'default' arg of getOption(,default=)
-    opts = c("datatable.verbose"="FALSE",
-             "datatable.dfdispatchwarn"="TRUE",
-             "datatable.alloccol"="quote(max(100,2*ncol(DT)))",
-             "datatable.nomatch"="NA_integer_")
-    for (i in setdiff(names(opts),names(options()))) {
-        eval(parse(text=paste("options(",i,"=",opts[i],")",sep="")))
-    }    
-    invisible()
 }
-
-
