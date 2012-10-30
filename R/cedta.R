126f360317cc2a68f9a3d132b1db79257cdafc16
Matt Dowle <mdowle@mdowle.plus.com>
2012-10-30 00:15:59 +0000

    New data.table:::cedta.override containing just gWidgetsWWW currently, fixing #2340.

--- R/cedta.R
+++ R/cedta.R
@@ -1,12 +1,22 @@
+
+cedta.override = c("gWidgetsWWW")
+# user may add more to this (e.g. adding statET) using :
+# assignInNamespace("cedta.override",c("gWidgetsWWW","statET"),"data.table")
+
 cedta = function(n=2L) {
     # Calling Environment Data Table Aware
     te = topenv(parent.frame(n))
     if (!isNamespace(te)) return(TRUE)  # e.g. DT queries in .GlobalEnv
     nsname = getNamespaceName(te)
-    nsname == "data.table" ||
-    "data.table" %in% names(getNamespaceImports(te)) ||
-    "data.table" %in% tryCatch(get(".Depends",paste("package",nsname,sep=":")),error=function(e)NULL) ||
-    (nsname == "utils" && exists("debugger.look",parent.frame(n+1L)))
+    ans = nsname == "data.table" ||
+        "data.table" %chin% names(getNamespaceImports(te)) ||
+        "data.table" %chin% tryCatch(get(".Depends",paste("package",nsname,sep=":")),error=function(e)NULL) ||
+        (nsname == "utils" && exists("debugger.look",parent.frame(n+1L))) ||
+        nsname %chin% cedta.override
+    if (!ans && getOption("datatable.verbose"))
+        cat("cedta decided '",nsname,"' wasn't data.table aware\n",sep="")
+        # so we can trace the namespace name that may need to be added (very unusually)
+    ans
 }
 
 
