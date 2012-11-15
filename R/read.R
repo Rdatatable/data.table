b4e023df736fed8c4dc536ac0061e895a565b375
Matt Dowle <mdowle@mdowle.plus.com>
2012-11-15 08:57:19 +0000

    Added fast file reader: read().  Work in progress.

--- R/read.R
+++ R/read.R
@@ -1,46 +1,29 @@
 
-read = function(fnam="test.csv",primary=",") {   #,secondary=" ")
-    ans = .Call(Creadfile,fnam)
+read = function(x="test.csv",primary=",",secondary="",header=TRUE,verbose=TRUE) {
+    h = read.table(x,header=header,sep=",",stringsAsFactors=FALSE,nrows=10)
+    f = c("integer"="%d","double"="%lf","character"="%s")[sapply(h,typeof)]
+    f = paste(paste(f,collapse=","),"\n",sep="")
+    types = lapply(sapply(h,typeof,USE.NAMES=FALSE),vector,length=0L)
+    
+    h = sapply(readLines(x,n=11),nchar)   # This could be even faster version of wc -l in C just for top 10 rows
+    if (header) {
+        hrowsize = h[1]+1
+        hsize = sum(h[2:11]) + 10
+    } else {
+        hrowsize = 0
+        hsize = sum(h[1:10]) + 10
+    }
+    estn = 10 * (file.info(x)$size - hrowsize) / hsize
+    if (verbose) {
+        cat("Estimated nrows =",estn,"\n")
+        cat("Format =",f)
+    }
+    ans = .Call(Creadfile,x,f,types,as.integer(header),as.integer(1.05*estn))
     nr = length(ans[[1]])
     setattr(ans,"row.names",.set_row_names(nr))
     setattr(ans,"class",c("data.table","data.frame"))
-    setnames(ans,paste("V",1:length(ans),sep=""))
+    setattr(ans,"names",paste("V",1:length(ans),sep=""))
     ans
 }
 
-if (FALSE) {
-# test speedup so far
-n=1e7
-test = data.table(a=sample(1:1000,n,replace=TRUE), b=sample(1:1000,n,replace=TRUE))
-write.table(test,"test.csv",sep=",",row.names=FALSE,col.names=FALSE)
-
-system.time(xx <- read.csv("test.csv",header=FALSE))
-system.time(yy <- read("test.csv"))
-identical(yy$V1,xx$V1)  # TRUE
-identical(yy$V2,xx$V2)  # TRUE
-
-# with n=1e6 I got :
-#> system.time(xx <- read.csv("test.csv",header=FALSE))
-#   user  system elapsed 
-#  5.388   0.092   5.486 
-#> system.time(yy <- read("test.csv"))
-#   user  system elapsed 
-#  1.276   0.052   1.329    
-#
-# That's only a 7.5MB file on disk, so perhaps a larger file, and more
-# columns (because that would be no more calls to fscanf), would
-# be a greater speedup. Especially the BED format (columns 11 and 12)
-# seems well matched for data.table's features with list() columns.
-# Also see comments in readfile.c
-}
-
-# TO DO: 
-#    use read.table on first 10 rows to get column names and types
-#    estimate number of rows using file size and info from first 10 rows
-#    allocate estimated rows + 5% in C, once. Even a memcpy afterwards
-#    to the precise number of rows would be faster than incrementally
-#    allocated memory as the scan progresses, or just leave the vectors
-#    slightly over allocated until they are copied anyway by something else.
-#    add the secondary separator for list() columns with vector cell
-#    allocation in C. Really, not difficult at all.
 
--- src/readfile.c
+++ src/readfile.c
@@ -5,48 +5,67 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-SEXP readfile(SEXP fnam)
+// TO DO: secondary separator for list() columns, such as columns 11 and 12 in BED.
+// TO DO: missing values (on return value of fscanf rescan, but in most cases ok first time)
+// TO DO: loop fscanf in batches of 32 (or 128) to remove column limit
+// TO DO: test if increasing p from 32 to 128 decreases performance for 10 columns.
+
+extern int sizes[];
+SEXP readfile(SEXP fnam, SEXP formatarg, SEXP types, SEXP skip, SEXP estnarg)
 {
-    SEXP c1,c2,ans;
-    char *p[64];
-    int i, nrow=0;
+    SEXP thiscol, ans;
+    R_len_t i, protecti=0, nrow=0, ncol, estn;
+    int size[32], thistype, nc=0, charcol[32];
+    char *buffer[32];
+    char *p[32], *format;
+    if (LENGTH(types)>32) error("Currently limited to 32 columns just for dev. Will be increased.");
+    for (i=0; i<32; i++) p[i] = 0;
     FILE *f=fopen(CHAR(STRING_ELT(fnam,0)),"r");
     if (f == NULL) error("file not found");
-    ans=PROTECT(allocVector(VECSXP,2));  // 2 column data.table, first steps
-    c1=PROTECT(allocVector(INTSXP,1000000));  // hard coded to match n=1e6 test up in read.R
-    c2=PROTECT(allocVector(INTSXP,1000000));
-    SET_VECTOR_ELT(ans,0,c1);
-    SET_VECTOR_ELT(ans,1,c2);
-    for (i=0;i<64;i++) p[i] = 0;
-    p[0] = (char *)DATAPTR(c1);
-    p[1] = (char *)DATAPTR(c2);
-    char format[100]="%d,%d\n";
-    while (fscanf(f,format, p[0], p[1], p[2], p[3])!=EOF) {
-        // Unused and zero'd p[2], p[3] (, ..., p[64]
-        // to be added) is deliberate.
-        // It isn't possible in C to construct a vargs
-        // and pass it to fscanf in C or C99. A '...' can
-        // be passed on, but not created unless we depend
-        // on a specialist library or go to asm.
-        // Living with a, say, 64 column limit, seems fine for many
-        // cases. Or, perhaps fscanf could be called
-        // twice per row to read in file with 128 columns, etc.
-        // Assumption is that fscanf is optimised internally for
-        // navigating the format, and is faster than character by
-        // character code with branching and function calls at the
-        // C level (such as in scan.c).
-        // Already with only 2 columns we see a 4 times speedup
-        // and perhaps with more columns this factor will be greater.
+    estn = INTEGER(estnarg)[0];
+    ncol = LENGTH(types);
+    ans=PROTECT(allocVector(VECSXP,ncol));  // TO DO, could over-allocate here directly, or maybe safer to go via alloccol.
+    protecti++;
+    for (i=0; i<ncol; i++) {
+        thistype = TYPEOF(VECTOR_ELT(types,i));
+        thiscol = PROTECT(allocVector(thistype,estn));
+        protecti++;
+        SET_TRUELENGTH(thiscol, estn);
+        SET_VECTOR_ELT(ans,i,thiscol);
+        if (thistype == STRSXP) {
+            charcol[nc] = i;
+            p[i] = buffer[nc] = malloc(1024);   // TO DO tie in %1024s and return value from fscanf
+            size[i] = 0;
+            nc++;
+        } else {
+            p[i] = (char *)DATAPTR(thiscol);
+            size[i] = sizes[thistype];
+        }
+    }
+    for (i=0; i<INTEGER(skip)[0]; i++) while (fgetc(f) != '\n');
+    format = (char *)CHAR(STRING_ELT(formatarg,0));
+    while (fscanf(f, format, p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],            // see note (*) below
+                             p[8],p[9],p[10],p[11],p[12],p[13],p[14],p[15],
+                             p[16],p[17],p[18],p[19],p[20],p[21],p[22],p[23],
+                             p[24],p[25],p[26],p[27],p[28],p[29],p[30],p[31]) != EOF) {
+        for (i=0; i<ncol; i++) p[i]+=size[i];
+        for (i=0; i<nc; i++) SET_STRING_ELT(VECTOR_ELT(ans, charcol[i]), nrow, mkChar(buffer[i]));
         nrow++;
-        for (i=0;i<2;i++) p[i]+=4;
-        // TO DO: secondary separator for list() columns, such as
-        // columns 11 and 12 in BED.
     }
     fclose(f);
-    SETLENGTH(c1, nrow);
-    SETLENGTH(c2, nrow);
-    UNPROTECT(3);
+    for (i=0; i<ncol; i++) SETLENGTH(VECTOR_ELT(ans,i), nrow);
+    UNPROTECT(protecti);
+    for (i=0; i<nc; i++) free(buffer[i]);
     return(ans);
 }
+
+/*  
+(*) (Often) unused and zero'd p[*] is deliberate
+It isn't possible in C to construct a vargs and pass it to fscanf in C or C99. A '...' can
+be passed on, but not created unless we depend on a specialist library or go to asm.
+Living with a, say, 32 column limit, seems fine for very many common scientific and business file formats. 
+Assumption is that fscanf is optimised internally for navigating the format, and is faster than character by
+character code with branching and function calls at the C level (such as in R's scan.c).
+*/
 
 
