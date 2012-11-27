717211e5f800ed314ab375ae329273e790426ad5
Matt Dowle <mdowle@mdowle.plus.com>
2012-11-27 19:38:38 +0000

    + Type detection in C hooked up and removed from R level. + Number of rows estimate hard coded to 1 million, temporarily. (git would be better at times like this, then I could commit locally without an inflict)

--- R/read.R
+++ R/read.R
@@ -1,39 +1,19 @@
 
 read = function(fnam="test.csv",sep="auto",sep2="auto",header="auto",na.strings=NULL,stringsAsFactors=FALSE,verbose=TRUE) {
     if (!is.character(fnam) || length(fnam)!=1) stop("fnam must be a single character string containing a file name, full path to a file, or URL starting 'http://', 'https://' or 'file://'")
-    header=TRUE
     if (substring(fnam,1,7) %chin% c("http://","https:/","file://")) {
         tt = tempfile()
         on.exit(unlink(tt), add=TRUE)
         download.file(fnam, tt)
         fnam = tt
     }
-    .Call(Cfinddelim,fnam)
-    h = read.table(fnam,header=header,sep=",",stringsAsFactors=FALSE,nrows=10)
-    type = sapply(h,typeof)
-    f = c("integer"="%d","double"="%lg","character"="%[^,\n]")[type]
-    f = paste(paste(f,collapse=","),sep="")
-    types = lapply(sapply(h,typeof,USE.NAMES=FALSE),vector,length=0L)
-    
-    h = sapply(readLines(fnam,n=11),nchar)   # This could be even faster version of wc -l in C just for top 10 rows
-    if (header) {
-        hrowsize = h[1]+1
-        hsize = sum(h[2:11]) + 10
-    } else {
-        hrowsize = 0
-        hsize = sum(h[1:10]) + 10
-    }
-    estn = as.integer(1.05 * 10 * (file.info(fnam)$size - hrowsize) / hsize)
-    if (verbose) {
-        cat("Allocating nrows =",estn,"\n")
-        cat("Format =",gsub("[\n]","\\\\n",f),"\n")
-    }
-    ans = .Call(Creadfile,fnam,f,types,as.integer(header),estn,verbose)
+    if (identical(header,"auto")) header=NA
+    else if (!identical(header,TRUE) && !identical(header,FALSE)) stop("'header' must be 'auto', TRUE or FALSE")
+    ans = .Call(Creadfile,fnam,header,verbose)
     nr = length(ans[[1]])
     setattr(ans,"row.names",.set_row_names(nr))
     setattr(ans,"class",c("data.table","data.frame"))
     setattr(ans,"names",paste("V",1:length(ans),sep=""))
-    
     # if (read any integer64, then require(bit64) else cat("integer64 have been read, you'll need bit64 to make those columns appear correctly, which isn't installed")
     ans
 }
--- src/readfile.c
+++ src/readfile.c
@@ -8,19 +8,21 @@
 
 /*****
 TO DO:
-Read bit64 directly without going via character
+Add test reading integer64 directly without going via character
 Read middle and end to check types
 Whitespace at the end of the line before \n should be ignored (warn about non white unprotected by comment char)
-Invalid but not ,, and not NA should promote type and coerce existing (reporting on which line it did that).
+Invalid but not ,, and not NA should promote type and coerce existing (reporting on which line it did that)
 test using "grep read.table ...Rtrunk/tests/
 loop fscanf in batches of 32 (or 128) to remove column limit
 test if increasing p from 32 to 128 decreases performance for 10 columns.
 secondary separator for list() columns, such as columns 11 and 12 in BED.
 Allow logical columns (currently will be character). T/True/TRUE/true are allowed in main/src/util.c
+Test files with under 30 rows
 *****/
 
 extern int sizes[];
 char sep, sep2;
+FILE *f;
 
 static void protprint(char *str)   // protected print i.e. \n is protected by replacing with \\n
 {
@@ -32,41 +34,157 @@ static void protprint(char *str)   // protected print i.e. \n is protected by re
     }
 }
 
-SEXP readfile(SEXP fnam, SEXP formatarg, SEXP types, SEXP skip, SEXP estnarg, SEXP verbosearg)
+static int countfields()
+{
+    int ncol=0;
+    char ch, protected='0';
+    ch = getc(f);
+    if (ch!='\n') ncol=1;
+    while (ch!='\n') {   // TO DO: add tests of this. Add to documentation that you cannot protect \n.
+        if (ch=='\"' || ch=='\'') {
+            if (protected=='0') protected = ch;
+            else if (protected==ch) protected = '0';
+            continue;
+        }
+        ncol += (protected=='0' ? ch==sep : 0);
+        ch = getc(f);
+    }
+    return(ncol);
+}
+
+SEXP readfile(SEXP fnam, SEXP verbosearg)
 {
     SEXP thiscol, ans;
-    R_len_t i, protecti=0, nrow=0, ncol, estn;
-    int size[32], type[32], thistype, nc=0, charcol[32], c1, c2, c3;
-    char *buffer[32];
+    R_len_t i, protecti=0, nrow=0, ncharcol=0, ncol=0, estn=1000010, nline=0;
+    int size[32], type[32], thistype, charcol[32], c1, c2, c3;
+    char *ansbuffer[32];
     char *p[32], *format, *formatcopy, *fmttok[32], *q, ch;
     long pos=0;
     Rboolean verbose = LOGICAL(verbosearg)[0];
-    if (LENGTH(types)>32) error("Currently limited to 32 columns just for dev. Will be increased.");
-    for (i=0; i<32; i++) p[i] = 0;
-    FILE *f=fopen(CHAR(STRING_ELT(fnam,0)),"r");
+    f=fopen(CHAR(STRING_ELT(fnam,0)),"r");
     if (f == NULL) error("file not found");
-    estn = INTEGER(estnarg)[0];
-    ncol = LENGTH(types);
+    
+    // ********************************************************************************************
+    // Make informed guess at column types
+    // ********************************************************************************************
+    
+    while (nline<30 && (ch=getc(f))!=EOF) nline += ch=='\n';   // skip first 30 lines (in case of human readable banner)
+    if (verbose) Rprintf("Found line %d\n", nline);
+    pos = ftell(f);
+    
+    // If the first column is protected (quoted character column) then skip over it in case it contains a different sep
+    if ((ch=getc(f)) == '\"') while(getc(f)!='\"');
+    else if (ch=='\'') while(getc(f)!='\'');
+    
+    if (fscanf(f,"%*[^, \t|:;~\n]")==EOF) error("File ends on line %d before any separator was found", nline);
+    sep = getc(f);
+    if (sep=='\n') error("No separator found on line %d. Please specify 'sep' manually, see ?read.", nline);
+    if (verbose) Rprintf("Detected sep as '%c'\n", sep);
+    // TO DO: Test single column files where there is no sep.
+    // TO DO: Test files where line 30 is the last line and doesn't end \n
+    fseek(f,pos,SEEK_SET);  // back to beginning of line
+    ncol = countfields();
+    if (verbose) Rprintf("Detected %d columns\n", ncol);
+    if (ncol>32) error("Currently limited to 32 columns just for dev. Will be increased.");
+    rewind(f);  // To the start
+    
+    nline = 1;
+    pos = ftell(f);
+    while (countfields()!=ncol) nline++,pos=ftell(f); // discard human readable header (if any)
+    
+    if (verbose) Rprintf("First row with %d fields detected on line %d\n", ncol, nline);
+    // if there is a header row, it will have just been read. Start on 2nd row regardless to detect types
+    // Now keep looping until all fields are character, or we have seen 10 non-NA values in all columns.
+    // Potentially, could scan whole file here (an entirely NA column). TO DO: limit that and test middle and end of file.
+    
+    char typef[3][7] = {"%9d%n","%lld%n","%lg%n"};
+    // We'll promote types from long long => double => character
+    // R is C99 so ll ok => bit64::integer64. We don't use %d, as the result is undefined in C99 for large numbers that
+    // overflow int hence protected field width 9. Where int wider than 9 chars but < INT_MAX, we'll detect that later.
+    
+    int typec[ncol], nread;  // typec = count of non-NA data in that col
+    for (i=0; i<ncol; i++) type[i]=0, typec[i]=0;
+    char strf[] = "%[^,\n]";
+    strf[3] = sep;  // replace comma with sep, in case sep isn't comma
+    char *buffer = malloc(sizeof(char) * 1024);
+    Rboolean needMoreObs;
+    double v;
+    do {   // all non character columns need 10 non-NA items for good guess. Type character can stop there.
+        needMoreObs = FALSE;
+        for (i=0;i<ncol;i++) {
+            *buffer='\0';
+            if (fscanf(f,strf,buffer)==EOF) error("Premature EOF!");
+            // Rprintf("Field contents are '%s'\n", buffer);
+            if (*buffer=='\"') {type[i]=3; if (fscanf(f,"%*[^\n\"]")==EOF) error("Premature EOF"); getc(f); }
+            else if (*buffer=='\'') {type[i]=3; if (fscanf(f,"%*[^'\n]")==EOF) error("Premature EOF"); getc(f); }
+            else if (*buffer!='\0' && strcmp(buffer,"NA")!=0) { // ,, or ,NA, doesn't help to detect type, skip
+                // Rprintf("Testing type, type before = %d with format %s\n", type[i],typef[type[i]]);
+                while (type[i]<3 && (sscanf(buffer,typef[type[i]],&v,&nread)==0 || nread<strlen(buffer))) type[i]++;
+                // Rprintf("Type after = %d with nread=%d\n", type[i], nread);
+                // if (type[i]==0) Rprintf("Int value read=%lld with errno=%d\n",*(long long *)&v,errno);
+                typec[i]++;
+            }
+            ch = getc(f);
+            if (i+1<ncol && ch!=sep) error("Unexpected separator ending field %d of line %d", i+1, nline);
+            if (type[i]<3 && typec[i]<10) needMoreObs=TRUE;
+        }
+        while (ch!='\n') ch = getc(f);
+    } while( needMoreObs && ch != EOF );
+    
+    // TO DO: If user has overridden any types, change these now. Not if user has selected a lesser type, though, with warning.
+    
+    q = format = malloc(sizeof(char)*1024);
+    char typef2[4][7] = {"%9d","%lld","%lg","%[^,\n]"};
+    int typesxp[4] = {INTSXP,REALSXP,REALSXP,STRSXP};
+    q += sprintf(q, "%s", typef2[type[0]]);
+    strcpy(strf, ",%s");
+    strf[0] = sep;
+    for (i=1; i<ncol; i++) q += sprintf(q, strf, typef2[type[i]]);
+    if (verbose) {Rprintf("Format = "); protprint(format); Rprintf("\n");}
+    
+    /*
+    h = sapply(readLines(fnam,n=11),nchar)
+    if (header) {
+        hrowsize = h[1]+1
+        hsize = sum(h[2:11]) + 10
+    } else {
+        hrowsize = 0
+        hsize = sum(h[1:10]) + 10
+    }
+    estn = as.integer(1.05 * 10 * (file.info(fnam)$size - hrowsize) / hsize)
+    */
+    
+    // ********************************************************************************************
+    // Now read data
+    // ********************************************************************************************
+    
+    fseek(f,pos,SEEK_SET);  // back to the start of (likely) header row
+    while ((ch=getc(f))!=EOF && ch!='\n');
+    /*
+    Read the header row using the split format into v.
+    If header row fails (nfields read < ncol) then it's the header row, otherwise that's the first data row (unless header has
+    been set) then proceed as before ...
+    */
+    
+    for (i=0; i<32; i++) p[i] = 0;
     ans=PROTECT(allocVector(VECSXP,ncol));  // TO DO, could over-allocate here directly, or maybe safer to go via alloccol.
     protecti++;
     for (i=0; i<ncol; i++) {
-        thistype = type[i] = TYPEOF(VECTOR_ELT(types,i));
+        thistype = type[i] = typesxp[ type[i] ];  // map from 0-3 to INTSXP,REALSXP and STRSXP.
         thiscol = PROTECT(allocVector(thistype,estn));
         protecti++;
         SET_TRUELENGTH(thiscol, estn);
         SET_VECTOR_ELT(ans,i,thiscol);
         if (thistype == STRSXP) {
-            charcol[nc] = i;
-            p[i] = buffer[nc] = malloc(sizeof(char)*1024);   // TO DO tie in %1024s and return value from fscanf
+            charcol[ncharcol] = i;
+            p[i] = ansbuffer[ncharcol] = malloc(sizeof(char)*1024);   // TO DO tie in %1024s and return value from fscanf
             size[i] = 0;
-            nc++;
+            ncharcol++;
         } else {
             p[i] = (char *)DATAPTR(thiscol);
             size[i] = sizes[thistype];
         }
     }
-    for (i=0; i<INTEGER(skip)[0]; i++) while (fgetc(f) != '\n');
-    format = (char *)CHAR(STRING_ELT(formatarg,0));
     formatcopy = malloc(sizeof(char)*strlen(format));
     strcpy(formatcopy,format);
     fmttok[i=0] = q = strtok(formatcopy,",");
@@ -77,7 +195,7 @@ SEXP readfile(SEXP fnam, SEXP formatarg, SEXP types, SEXP skip, SEXP estnarg, SE
         }
         fmttok[++i] = q = strtok(NULL,",");
     }
-    if (verbose) {Rprintf("Split format:"); for (i=0; i<ncol; i++){Rprintf(" "); protprint(fmttok[i]);}; Rprintf("\n");}
+    if (verbose) {Rprintf("Fsplit ="); for (i=0; i<ncol; i++){Rprintf(" "); protprint(fmttok[i]);}; Rprintf("\n");}
     while ((i=fscanf(f, format,
             p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7],            // see footnote 1
             p[8], p[9], p[10],p[11],p[12],p[13],p[14],p[15],
@@ -113,129 +231,18 @@ SEXP readfile(SEXP fnam, SEXP formatarg, SEXP types, SEXP skip, SEXP estnarg, SE
             // TO DO - cater for EOF here on last line of file if there's a missing on last row.
         }
         for (i=0; i<ncol; i++) p[i]+=size[i];
-        for (i=0; i<nc; i++) SET_STRING_ELT(VECTOR_ELT(ans, charcol[i]), nrow, mkChar(buffer[i]));
+        for (i=0; i<ncharcol; i++) SET_STRING_ELT(VECTOR_ELT(ans, charcol[i]), nrow, mkChar(ansbuffer[i]));
         pos=0;
         nrow++;
     }
     fclose(f);
     for (i=0; i<ncol; i++) SETLENGTH(VECTOR_ELT(ans,i), nrow);
     UNPROTECT(protecti);
-    for (i=0; i<nc; i++) free(buffer[i]);
+    for (i=0; i<ncharcol; i++) free(ansbuffer[i]);
     free(formatcopy);
-    return(ans);
-}
-
-FILE *f;
-
-static int countfields()
-{
-    int ncols=0;
-    char ch, protected='0';
-    ch = getc(f);
-    if (ch!='\n') ncols=1;
-    while (ch!='\n') {   // TO DO: add tests of this.
-        if (ch=='\"' || ch=='\'') {
-            if (protected=='0') protected = ch;
-            else if (protected==ch) protected = '0';
-            continue;
-        }
-        ncols += (protected=='0' ? ch==sep : 0);
-        ch = getc(f);
-    }
-    return(ncols);
-}
-
-SEXP finddelim(SEXP fnam)
-{
-    int nline=0, ncols=0, i;
-    char ch, *format, *p;
-    long pos=0;
-    Rboolean verbose=TRUE;
-    f=fopen(CHAR(STRING_ELT(fnam,0)),"r");
-    
-    while (nline<30 && (ch=getc(f))!=EOF) nline += ch=='\n';   // skip first 30 lines (in case of human readable banner)
-    Rprintf("Found line %d\n", nline);
-    pos = ftell(f);
-    
-    // If the first column is protected (quoted character column) then skip over it in case it contains a different sep
-    if ((ch=getc(f)) == '\"') while(getc(f)!='\"');
-    else if (ch=='\'') while(getc(f)!='\'');
-    
-    if (fscanf(f,"%*[^, \t|:;~\n]")==EOF) error("File ends on line %d before any separator was found", nline);
-    sep = getc(f);
-    if (sep=='\n') error("No separator found on line %d. Please specify 'sep' manually, see ?read.", nline);
-    if (verbose) Rprintf("Detected sep as '%c'\n", sep);
-    // TO DO: Test single column files where there is no sep.
-    // TO DO: Test files where line 30 is the last line and doesn't end \n
-    fseek(f,pos,SEEK_SET);  // back to beginning of line
-    ncols = countfields();
-    if (verbose) Rprintf("Detected %d columns\n", ncols);
-    rewind(f);  // To the start
-    
-    nline = 1;
-    pos = ftell(f);
-    while (countfields()!=ncols) nline++,pos=ftell(f); // discard human readable header (if any)
-    
-    if (verbose) Rprintf("First row with %d fields detected on line %d\n", ncols, nline);
-    // if there is a header row, it will have just been read. Start on 2nd row regardless to detect types
-    // Now keep looping until all fields are character, or we have seen 10 non-NA values in all columns.
-    // Potentially, could scan whole file here (an entirely NA column). TO DO: limit that and test middle and end of file.
-    
-    char typef[3][7] = {"%9d%n","%lld%n","%lg%n"};
-    // We'll promote types from long long => double => character
-    // R is C99 so ll ok => bit64::integer64. We don't use %d, as the result is undefined in C99 for large numbers that
-    // overflow int hence protected field width 9. Where int wider than 9 chars but < INT_MAX, we'll detect that later.
-    
-    int typei[ncols], typec[ncols], nread;  // typec = count of non-NA data in that col
-    for (i=0; i<ncols; i++) typei[i]=0, typec[i]=0;
-    char strf[] = "%[^,\n]";
-    strf[3] = sep;  // replace comma with sep, in case sep isn't comma
-    char *buffer = malloc(sizeof(char) * 1024);
-    Rboolean needMoreObs;
-    double v;
-    do {   // all non character columns need 10 non-NA items for good guess. Type character can stop there.
-        needMoreObs = FALSE;
-        for (i=0;i<ncols;i++) {
-            *buffer='\0';
-            if (fscanf(f,strf,buffer)==EOF) error("Premature EOF!");
-            // Rprintf("Field contents are '%s'\n", buffer);
-            if (*buffer=='\"') {typei[i]=3; if (fscanf(f,"%*[^\n\"]")==EOF) error("Premature EOF"); getc(f); }
-            else if (*buffer=='\'') {typei[i]=3; if (fscanf(f,"%*[^'\n]")==EOF) error("Premature EOF"); getc(f); }
-            else if (*buffer!='\0' && strcmp(buffer,"NA")!=0) { // ,, or ,NA, doesn't help to detect type, skip
-                // Rprintf("Testing type, type before = %d with format %s\n", typei[i],typef[typei[i]]);
-                while (typei[i]<3 && (sscanf(buffer,typef[typei[i]],&v,&nread)==0 || nread<strlen(buffer))) typei[i]++;
-                // Rprintf("Type after = %d with nread=%d\n", typei[i], nread);
-                // if (typei[i]==0) Rprintf("Int value read=%lld with errno=%d\n",*(long long *)&v,errno);
-                typec[i]++;
-            }
-            ch = getc(f);
-            if (i+1<ncols && ch!=sep) error("Unexpected separator ending field %d of line %d", i+1, nline);
-            if (typei[i]<3 && typec[i]<10) needMoreObs=TRUE;
-        }
-        while (ch!='\n') ch = getc(f);
-    } while( needMoreObs && ch != EOF );
-    
-    for (i=0; i<ncols; i++) Rprintf("Type %d = %d\n", i, typei[i]);
-    
-    // TO DO: If user has overridden any types, change these now. Not if user has selected a lesser type, though, with warning.
-    
-    p = format = malloc(sizeof(char)*1024);
-    char typef2[4][7] = {"%9d","%lld","%lg","%[^,\n]"};
-    p += sprintf(p, "%s", typef2[typei[0]]);
-    strcpy(strf, ",%s");
-    strf[0] = sep;
-    for (i=1; i<ncols; i++) p += sprintf(p, strf, typef2[typei[i]]);
-    Rprintf("Format on C side = "); protprint(format); Rprintf("\n");
-    
-    fseek(f,pos,SEEK_SET);  // back to the start of (likely) header row
-    /* Read the header row using the split format into v.
-    If header row fails (nfields read < ncol) then it's the header row, otherwise that's the first data row (unless header has
-    been set) then proceed as before ...
-    */
-    fclose(f);
     free(buffer);
     free(format);
-    return(R_NilValue);
+    return(ans);
 }
 
 /*
