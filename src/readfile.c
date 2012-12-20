b6064df1db19923f3bead62932255680e1337975
Matt Dowle <mdowle@mdowle.plus.com>
2012-12-20 18:07:02 +0000

    fread(): + nrow now counted up front (like wc -l). Estimate formula removed. Small cost but safer, avoids having to implement reallocation, avoids heap cleanup of overallocated rows and allows ch<eof checks to be left to ch<eol as future speed improvement. + crlf now simulated by unix checks to catch Windows issues earlier. + Fully passes R CMD check on Windows using winbuilder.

--- src/readfile.c
+++ src/readfile.c
@@ -19,13 +19,13 @@
 
 /*****
 TO DO:
-Whitespace at the end of the line before eol should be ignored, and warn about non white unprotected by comment char
-Count \n up front. Allocate nrow exactly, and save continual ch<eof checking throughout (although v fast).
-Countfields on the last (non blank) line of file and stop early if no good (to be sure we can save ch<eof throughout).  
+893.5 should be two empty integer cols not character
 Stroll speedup
+* Save continual ch<eof checking throughout. Last line might still be tricky if last line has no eol.
+* Whitespace at the end of the line before eol should be ignored, and warn about non white unprotected by comment char
+* Warning about any blank lines skipped in the middle.
 Check and correct nline in error messages
 Go through fread tests and remove all all.equal()s 
-Add a mapChunk argument, by default 10% of getsysinfo.ram. Entire file when 0 (if users knowns file will be reread a few times). 
 Allow logical columns (currently read as character). T/True/TRUE/true are allowed in main/src/util.c
 ---
 Add a way to pick out particular columns only, by name or position.
@@ -35,8 +35,7 @@ CoerceVector should only coerce items read so far.
 Read middle and end to check types
 A few TO DO inline in the code, including some speed fine tuning
 test using at least "grep read.table ...Rtrunk/tests/
-Clear up slightly over-allocated rows on heap during gc().
-Implement realloc if estn wasn't large enough.
+Add a mapChunk argument, by default 10% of getsysinfo.ram. Entire file when 0 (if users knowns file will be reread a few times). 
 Secondary separator for list() columns, such as columns 11 and 12 in BED.
 Allow 2 character separators? Some companies have internal formats that do that.
 *****/
@@ -67,9 +66,9 @@ static int countfields()
 SEXP readfile(SEXP input, SEXP nrowsarg, SEXP headerarg, SEXP nastrings, SEXP verbosearg, SEXP autostart)
 {
     SEXP thiscol, ans, thisstr;
-    R_len_t i, j, k, protecti=0, nrow=0, ncol=0, estn, nline, flines;
+    R_len_t i, j, k, protecti=0, nrow=0, ncol=0, nline, flines;
     int thistype;
-    char *fnam=NULL, *pos, *pos1, *pos2, *mmp, *ch2;
+    char *fnam=NULL, *pos, *pos1, *mmp, *ch2;
     Rboolean verbose=LOGICAL(verbosearg)[0], header=LOGICAL(headerarg)[0], allchar;
     double t0 = currentTime(), tMap;
     size_t filesize;
@@ -251,12 +250,11 @@ SEXP readfile(SEXP input, SEXP nrowsarg, SEXP headerarg, SEXP nastrings, SEXP ve
         while (ch<eof && *ch!=eol) ch++;
         if (ch<eof && *ch==eol) ch+=eolLen;
     }
-    pos2 = ch;
     if (verbose) { Rprintf("Type codes: "); for (i=0; i<ncol; i++) Rprintf("%d",type[i]); Rprintf("\n"); }
     int typesxp[4] = {INTSXP,REALSXP,REALSXP,STRSXP};
     
     // ********************************************************************************************
-    // Search backwards to find column names row / first data row
+    // Search backwards to find column names row or first data row
     // ********************************************************************************************
     ch = pos;  // back to start of autostart.  nline is already this line number
     if (ch>mmp) {
@@ -315,31 +313,54 @@ SEXP readfile(SEXP input, SEXP nrowsarg, SEXP headerarg, SEXP nastrings, SEXP ve
         }
         while (ch<eof && *ch!=eol) ch++;
         if (ch<eof && *ch==eol) ch+=eolLen;  // now on first data row (row after column names)
+        pos = ch;
     }
     
     // ********************************************************************************************
-    // Estimate number of rows
+    // Count number of rows
     // ********************************************************************************************
-    estn = (R_len_t)ceil(1.05 * flines * (filesize-(pos-mmp)) / (pos2-pos1)) +5;  // +5 for small files
-    if (verbose) Rprintf("Estimated nrows: %d ( 1.05*%d*(%ld-(%ld-%ld))/(%ld-%ld) )\n",estn,flines,filesize,pos,mmp,pos2,pos1);
-    i = INTEGER(nrowsarg)[0];
-    if (i>-1 && i<estn) {
-        estn = i;
-        if (verbose) Rprintf("estn limited to nrows passed in (%d)\n", estn);
+    if (pos==eof || *pos==eol) {
+        nrow=0;
+        if (verbose) Rprintf("Byte after header row is eof or eol, 0 data rows present.\n");
+    } else {
+        nrow=1; while (ch<eof) nrow+=(*ch++==eol);
+        if (verbose) Rprintf("Count of eol after pos: %d\n",nrow);
+        // Advantages of exact count: i) no need to slightly over allocate (by 5%, say) and no need to clear up on heap during gc(),
+        // and ii) no need to implement realloc if estimate doesn't turn out to be large enough (e.g. sample rows wider than file average).
+        // The old estimate method :
+        // estn = (R_len_t)ceil(1.05 * flines * (filesize-(pos-mmp)) / (pos2-pos1)) +5;  // +5 for small files
+        // if (verbose) Rprintf("Estimated nrows: %d ( 1.05*%d*(%ld-(%ld-%ld))/(%ld-%ld) )\n",estn,flines,filesize,pos,mmp,pos2,pos1);
+        if (ch!=eof) error("Internal error: ch!=eof after counting eol");
+        i=0; Rboolean blank=TRUE;
+        while (blank && ch>pos) {   // subtract blank lines at the end from the row count
+            while (ch>pos && *--ch!=eol2);
+            if (ch>pos) ch++;
+            i += (blank=(countfields()!=ncol));
+            ch -= eolLen;
+            // TO DO Warn about any non blank lines with incorrect number of fields being removed.
+        }
+        nrow-=i;
+        if (verbose) Rprintf("Subtracted %d for last eol, plus any trailing empty lines, leaving %d data rows\n",i,nrow);
     }
+    i = INTEGER(nrowsarg)[0];
+    if (i>-1 && i<nrow) {
+        nrow = i;
+        if (verbose) Rprintf("nrow limited to nrows passed in (%d)\n", nrow);
+    }
+    ch = pos;  
 
     // ********************************************************************************************
-    // Allocate columns for the estimated nrows
+    // Allocate columns for known nrow
     // ********************************************************************************************
     ans=PROTECT(allocVector(VECSXP,ncol));  // safer to leave over allocation to alloc.col on return in fread.R
     protecti++;
     setAttrib(ans,R_NamesSymbol,names);
     for (i=0; i<ncol; i++) {
         thistype  = typesxp[ type[i] ];  // map from 0-3 to INTSXP(0), REALSXP(1), REALSXP(2) and STRSXP(3).
-        thiscol = PROTECT(allocVector(thistype,estn));
+        thiscol = PROTECT(allocVector(thistype,nrow));
         protecti++;
         if (type[i]==1) setAttrib(thiscol, R_ClassSymbol, ScalarString(mkChar("integer64")));
-        SET_TRUELENGTH(thiscol, estn);
+        SET_TRUELENGTH(thiscol, nrow);
         SET_VECTOR_ELT(ans,i,thiscol);
     }
     
@@ -349,17 +370,16 @@ SEXP readfile(SEXP input, SEXP nrowsarg, SEXP headerarg, SEXP nastrings, SEXP ve
     double t1=currentTime(), tCoerce0, tCoerce=0.0;
     double nexttime = t0+2;  // start printing % done after a few seconds, if doesn't appear then you know mmap is taking a while
     Rboolean isna;
-    // ch has been positioned at the start of the first data row
-    pos = ch;
-    while (ch<eof) {
-        //Rprintf("Row %d : %.10s\n", nrow+1, ch);
+    ch = pos;   // back to start of first data row
+    for (i=0; i<nrow; i++) {
+        //Rprintf("Row %d : %.10s\n", i+1, ch);
         while (ch<eof && isspace(*ch) && *ch!=sep && *ch!=eol) ch++;       // skip over any empty lines.
         if (ch<eof && *ch==eol) { ch+=eolLen; nline++; continue; }
         ch = pos;                                 //  back to start in case first column is character with leading space to be retained
-        for (i=0;i<ncol;i++) {
-            //Rprintf("Field %d: '%.10s'\n", i+1, ch);
-            thiscol = VECTOR_ELT(ans, i);
-            switch (type[i]) {
+        for (j=0;j<ncol;j++) {
+            //Rprintf("Field %d: '%.10s'\n", j+1, ch);
+            thiscol = VECTOR_ELT(ans, j);
+            switch (type[j]) {
             case 0: // integer
                 errno=0; ch2=ch;
                 while (ch<eof && isspace(*ch) && *ch!=sep && *ch!=eol) ch++; // TO DO: remove. Only because stroll* skips leading isspace (could include sep)
@@ -367,13 +387,13 @@ SEXP readfile(SEXP input, SEXP nrowsarg, SEXP headerarg, SEXP nastrings, SEXP ve
                 else {ch2=ch; u.l = strtoll(ch, &ch2, 10);}
                 if (ch2>ch && (ch2==eof || *ch2==sep || *ch2==eol) && INT_MIN<=u.l && u.l<=INT_MAX && errno==0) {
                     // TO DO: do we really need the check on ch2?  sep can't be true unless ch2 has moved?
-                    INTEGER(thiscol)[nrow] = (int)u.l;
+                    INTEGER(thiscol)[i] = (int)u.l;
                     ch=ch2;
                     break;   // field now done, we prefer fully populated files
                 }
                 isna=FALSE;
                 if (ch==eof || *ch==sep || *ch==eol || (isna=(ch<eof-1 && *ch=='N' && *(ch+1)=='A' && (ch+2==eof || *(ch+2)==sep || *(ch+2)==eol)))) {
-                    INTEGER(thiscol)[nrow] = NA_INTEGER;
+                    INTEGER(thiscol)[i] = NA_INTEGER;
                     ch+=2*isna;
                     break;
                 }  
@@ -383,14 +403,14 @@ SEXP readfile(SEXP input, SEXP nrowsarg, SEXP headerarg, SEXP nastrings, SEXP ve
                 if (ch==eof || *ch==sep || *ch==eol) ch2=ch;
                 else {ch2=ch; u.l = strtoll(ch, &ch2, 10);}   // wasteful repeated call on fall through, but only once (ever, per column) on bump.
                 if (ch2>ch && (ch2==eof || *ch2==sep || *ch2==eol) && errno==0) {
-                    if (type[i]==0) { error("Coercing int to integer64 needs to be implemented"); type[i]=1; }
-                    REAL(thiscol)[nrow] = u.d;
+                    if (type[j]==0) { error("Coercing int to integer64 needs to be implemented"); type[i]=1; }
+                    REAL(thiscol)[i] = u.d;
                     ch=ch2;
                     break;
                 }
                 isna=FALSE;
                 if (ch==eof || *ch==sep || *ch==eol || (isna=(ch<eof-1 && *ch=='N' && *(ch+1)=='A' && (ch+2==eof || *(ch+2)==sep || *(ch+2)==eol)))) {
-                    REAL(thiscol)[nrow] = NA_REAL;
+                    REAL(thiscol)[i] = NA_REAL;
                     ch+=2*isna;
                     break;
                 }
@@ -400,21 +420,21 @@ SEXP readfile(SEXP input, SEXP nrowsarg, SEXP headerarg, SEXP nastrings, SEXP ve
                 if (ch==eof || *ch==sep || *ch==eol) ch2=ch;
                 else {ch2=ch; u.d = strtod(ch, &ch2);}
                 if (ch2>ch && (ch==eof || *ch2==sep || *ch2==eol) && errno==0) {
-                    if (type[i]==1) { error("Coercing integer64 to real needs to be implemented"); type[i]=2; }
-                    else if (type[i]==0) {
+                    if (type[j]==1) { error("Coercing integer64 to real needs to be implemented"); type[j]=2; }
+                    else if (type[j]==0) {
                         tCoerce0 = currentTime();
-                        SET_VECTOR_ELT(ans, i, thiscol = coerceVector(thiscol, REALSXP));
-                        if (verbose) Rprintf("Column %d bumped from code %d to double on line %d, field contains '%.10s'\n", i+1, type[i], nrow+nline, ch);
-                        type[i]=2;
+                        SET_VECTOR_ELT(ans, j, thiscol = coerceVector(thiscol, REALSXP));
+                        if (verbose) Rprintf("Column %d bumped from code %d to double on line %d, field contains '%.10s'\n", j+1, type[j], i+2, ch);
+                        type[j]=2;
                         tCoerce += currentTime()-tCoerce0;
                     }
-                    REAL(thiscol)[nrow] = u.d;
+                    REAL(thiscol)[i] = u.d;
                     ch=ch2;
                     break;
                 }
                 isna=FALSE;
                 if (ch==eof || *ch==sep || *ch==eol || (isna=(ch<eof-1 && *ch=='N' && *(ch+1)=='A' && (ch2==eof || *(ch+2)==sep || *(ch+2)==eol)))) {
-                    REAL(thiscol)[nrow] = NA_REAL;
+                    REAL(thiscol)[i] = NA_REAL;
                     ch+=2*isna;
                     break;
                 }
@@ -422,33 +442,34 @@ SEXP readfile(SEXP input, SEXP nrowsarg, SEXP headerarg, SEXP nastrings, SEXP ve
                 ch2=ch;
                 if (ch<eof && *ch=='\"') {ch++; while(++ch2<eof && *ch2!=eol && !(*ch2=='\"' && *(ch2-1)!='\\'));}
                 else while (ch2<eof && *ch2!=sep && *ch2!=eol) ch2++;
-                if (type[i]<3) {
-                    if (type[i]==1) error("Coercing integer64 to character needs to be implemented");
+                if (type[j]<3) {
+                    if (type[j]==1) error("Coercing integer64 to character needs to be implemented");
                     tCoerce0 = currentTime();
-                    SET_VECTOR_ELT(ans, i, thiscol = coerceVector(thiscol, STRSXP));
-                    for (j=0; j<nrow; j++) if (STRING_ELT(thiscol,j)==NA_STRING) SET_STRING_ELT(thiscol,j,R_BlankString);
+                    SET_VECTOR_ELT(ans, j, thiscol = coerceVector(thiscol, STRSXP));
+                    for (k=0; k<nrow; k++) if (STRING_ELT(thiscol,k)==NA_STRING) SET_STRING_ELT(thiscol,k,R_BlankString);
                     // If a character column was blank on earlier rows (and in the guess rows), they'll have been coerced to
                     // NAs. Set back to "" and these will be converted back to NA at the end if na.strings contains "".
-                    if (verbose) Rprintf("Column %d bumped from code %d to character on line %d, field contains '%.*s'\n", i+1, type[i], nrow+nline, ch2-ch, ch);
-                    type[i]=3;
+                    if (verbose) Rprintf("Column %d bumped from code %d to character on line %d, field contains '%.*s'\n", j+1, type[j], i+2, ch2-ch, ch);
+                    type[j]=3;
                     tCoerce += currentTime()-tCoerce0;
                 }
-                SET_STRING_ELT(thiscol, nrow, mkCharLen(ch,ch2-ch));
+                SET_STRING_ELT(thiscol, i, mkCharLen(ch,ch2-ch));
                 if (ch2<eof && *ch2=='\"') ch=ch2+1; else ch=ch2;
             }
-            if (ch<eof && *ch==sep && i<ncol-1) {ch++; continue;}  // most common case first, done, next field
-            if (i<ncol-1) error("Expected sep ('%c') but '%c' ends field %d on line %d: %.*s", sep, *ch, i+1, nline+nrow, ch-pos+1, pos);
+            if (ch<eof && *ch==sep && j<ncol-1) {ch++; continue;}  // most common case first, done, next field
+            if (j<ncol-1) error("Expected sep ('%c') but '%c' ends field %d on line %d: %.*s", sep, *ch, j+1, i+2, ch-pos+1, pos);
         }
-        //Rprintf("At end of line with nrow=%d and ch='%.10s'\n", nrow, ch);
+        //Rprintf("At end of line with i=%d and ch='%.10s'\n", i, ch);
         while (ch<eof && *ch!=eol) ch++; // discard after end of line, but before \n. TO DO: warn about uncommented text here
         if (ch<eof && *ch==eol) ch+=eolLen;
         pos = ch;  // start of line position only needed to include the whole line in any error message
         //Rprintf("Start of next line '%.10s'\n", ch);
-        if (++nrow == estn) {
-            if (INTEGER(nrowsarg)[0]==-1) warning("Filled all estn rows (%d), even after estimating 5%% more. Reallocation at this point fairly trivial but not yet implemented. Returning the rows read so far and discarding the rest (if any), for now.", estn);
-            break;
-        } else if (nrow%10000==0 && currentTime()>nexttime) {  // %10000 && saves a little bit (apx 0.2 in 4.9 secs, 5%)
-            Rprintf("%.0f%%\r", 100.0*nrow/estn);
+        //if (++nrow == estn) {
+        //    if (INTEGER(nrowsarg)[0]==-1) warning("Filled all estn rows (%d), even after estimating 5%% more. Reallocation at this point fairly trivial but not yet implemented. Returning the rows read so far and discarding the rest (if any), for now.", estn);
+        //    break;
+        //} else
+        if (i%10000==0 && currentTime()>nexttime) {  // %10000 && saves a little bit (apx 0.2 in 4.9 secs, 5%)
+            Rprintf("%.0f%%\r", 100.0*i/nrow);         // prints straight away (0%10000) if the mmap took a while
             nexttime = currentTime()+1;
             R_CheckUserInterrupt();
         }
@@ -479,9 +500,11 @@ SEXP readfile(SEXP input, SEXP nrowsarg, SEXP headerarg, SEXP nastrings, SEXP ve
     }
     if (verbose) {
         double tn = currentTime(), tot=tn-t0;
-        Rprintf("%8.3fs (%3.0f%%) Memory mapping file\n", tMap-t0, 100*(tMap-t0)/tot);
+        Rprintf("%8.3fs (%3.0f%%) Memory map (quicker if you rerun)\n", tMap-t0, 100*(tMap-t0)/tot);
         Rprintf("%8.3fs (%3.0f%%) Format detection\n", t1-tMap, 100*(t1-tMap)/tot);
-        Rprintf("%8.3fs (%3.0f%%) Reading\n", t2-t1-tCoerce, 100*(t2-t1-tCoerce)/tot);
+        //Rprintf("%8.3fs (%3.0f%%) Count rows (wc -l)\n", .......);
+        //Rprintf("%8.3fs (%3.0f%%) Allocation of %dx%d result (%dMB) in RAM\n", ....., nrow, ncol);
+        Rprintf("%8.3fs (%3.0f%%) Reading data\n", t2-t1-tCoerce, 100*(t2-t1-tCoerce)/tot);
         Rprintf("%8.3fs (%3.0f%%) Bumping column type midread and coercing data already read\n", tCoerce, 100*tCoerce/tot);
         Rprintf("%8.3fs (%3.0f%%) Changing na.strings to NA\n", tn-t2, 100*(tn-t2)/tot);
         Rprintf("%8.3fs        Total\n", tot);
