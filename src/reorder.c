69dce0a2208953b7ab5b956956e6f5dcd34912cb
Matt Dowle <mdowle@mdowle.plus.com>
2014-01-14 17:59:02 +0000

    Creorder now ignores the beginning and end of ordering vectors where that region doesn't change the row ordering,  such as c(1:500, 502, 501, 503:nrow).   Creorder(DT, 1:nrow) will now return instantly.

--- src/reorder.c
+++ src/reorder.c
@@ -81,9 +81,11 @@ SEXP reorder(SEXP dt, SEXP order)
     // For internal use only by fastorder().
     // Reordering a vector in-place doesn't change generations so we can skip SET_STRING_ELT overhead etc.
     // Speed is dominated by page fetch when input is randomly ordered so we're at the software limit here (better bus etc should shine).
+    // 'order' must strictly be a permutation of 1:n (i.e. no repeats, zeros or NAs)
+    // If only a small subset is reordered, this is detected using start and end.
     char *tmp, *tmpp, *vd;
     SEXP v;
-    R_len_t i, j, nrow, size;
+    R_len_t i, j, nrow, size, start, end;
     nrow = length(VECTOR_ELT(dt,0));
     for (i=0;i<length(dt);i++) {
         if (length(VECTOR_ELT(dt,i))!=nrow) error("Column %d is length %d which differs from length of column 1 (%d). Invalid data.table. Check NEWS link at top of ?data.table for latest bug fixes. If not already reported and fixed, please report to datatable-help.", i+1, length(VECTOR_ELT(dt,i)), nrow);
@@ -91,8 +93,13 @@ SEXP reorder(SEXP dt, SEXP order)
     if (length(order) != nrow) error("logical error nrow(dt)!=length(order)");
     if (sizeof(int)!=4) error("sizeof(int) isn't 4");
     if (sizeof(double)!=8) error("sizeof(double) isn't 8");   // 8 on both 32bit and 64bit.
-    tmp=(char *)Calloc(nrow,double);   // Enough working space for one column of the largest type. setSizes() has a check too.
-                                       // So we can reorder a 10GB table in 16GB of RAM
+    start = 0;
+    while (start<nrow && INTEGER(order)[start] == start+1) start++;
+    if (start==nrow) return(R_NilValue);  // input is 1:n, nothing to do
+    end = nrow-1;
+    while (INTEGER(order)[end] == end+1) end--;
+    tmp=(char *)Calloc(end-start+1,double);   // Enough working space for one column of the largest type. setSizes() has a check too.
+                                              // So we can reorder a 10GB table in 16GB of RAM
     if (!tmp) error("unable to allocate temporary working memory for reordering data.table");
     for (i=0;i<length(dt);i++) {
         v = VECTOR_ELT(dt,i);
@@ -101,18 +108,18 @@ SEXP reorder(SEXP dt, SEXP order)
         tmpp=tmp;
         vd = (char *)DATAPTR(v);
         if (size==4) {
-            for (j=0;j<nrow;j++) {
+            for (j=start;j<=end;j++) {
                 *(int *)tmpp = ((int *)vd)[INTEGER(order)[j]-1];  // just copies 4 bytes (pointers on 32bit too)
                 tmpp += 4;
             }
         } else {
             if (size!=8) error("Size of column %d's type isn't 4 or 8", i+1);
-            for (j=0;j<nrow;j++) {
+            for (j=start;j<=end;j++) {
                 *(double *)tmpp = ((double *)vd)[INTEGER(order)[j]-1];  // just copies 8 bytes (pointers on 64bit too)
                 tmpp += 8;
             }
         }
-        memcpy(vd, tmp, ((size_t)nrow)*size);
+        memcpy(vd + start*size, tmp, (end-start+1) * size);
     }
     Free(tmp);
     return(R_NilValue);
