12bc63a245c6ddfea62a0192475a1d96a4f80d8f
Arunkumar Srinivasan <aragorn168b@gmail.com>
2014-03-03 22:39:16 +0000

    setorder sets 'key' to NULL by default now. Not sure if it makes sense for setorder to check and retain key in certain special cases, but that's to revisit later.

--- R/setkey.R
+++ R/setkey.R
@@ -210,9 +210,8 @@ setorderv = function(x, cols, order=1L)
     if (!is.character(cols) || length(cols)<1) stop("'cols' should be character at this point in setkey.")
 
     o = forder(x, cols, sort=TRUE, retGrp=FALSE, order=order)
-    if (length(o)) {
-        .Call(Creorder, x, o)
-    }
+    if (length(o)) .Call(Creorder, x, o)
+    setattr(x, 'sorted', NULL) # setorder will by default set key to NULL, TODO: revisit cases where we can retain key
     invisible(x)
 }
 
