c5973dcc47d8264f73ec33a0836d8e6d5a74a8e4
Arunkumar Srinivasan <aragorn168b@gmail.com>
2014-06-14 01:52:09 +0200

    Some updates to homepage.

---
+++ www/engine/run_file.R
@@ -0,0 +1,13 @@
+in_file  = "~/Dropbox/Public/github/Rdatatable/datatable/www/engine/R/01.introduction.md"
+out_file = "~/Dropbox/Public/github/Rdatatable/datatable/www/engine/R/01.introduction.Rmd"
+source("~/Dropbox/Public/github/Rdatatable/datatable/www/engine/scripts/parser.R")
+parser(in_file, out_file)
+
+# pandoc --smart --standalone -c ../css/bootstrap.css --from markdown --to html -o ../about/index.html R/01.introduction.Rmd
+
+in_file  = "~/Dropbox/Public/github/Rdatatable/datatable/www/engine/R/00.parser.md"
+out_file = "~/Dropbox/Public/github/Rdatatable/datatable/www/engine/R/00.parser.Rmd"
+source("~/Dropbox/Public/github/Rdatatable/datatable/www/engine/scripts/parser.R")
+parser(in_file, out_file)
+
+# pandoc --smart --standalone -c ../css/bootstrap.css --from markdown --to html -o ../internals/index.html R/00.parser.Rmd
