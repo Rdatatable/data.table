6aa97fa7a90cc0f2b8bbfc204bb7f307a4555aa8
Matt Dowle <mdowle@mdowle.plus.com>
2014-10-09 12:03:53 +0100

    Printing test number in last 13 lines of CRAN output resolved.

--- R/test.data.table.R
+++ R/test.data.table.R
@@ -72,7 +72,7 @@ test = function(num,x,y,error=NULL,warning=NULL,output=NULL) {
     v = getOption("datatable.verbose")
     d = exists(".devtesting",parent.frame()) && get(".devtesting", parent.frame())
     if (v || d) {
-        cat(if (v && !d) "\n\n" else "\r", "Running test id ", num, "     ",sep="")
+        cat(if (v && !d && !interactive()) "\n\n" else "\r", "Running test id ", num, "     ",sep="")
     }
     # TO DO: every line that could possibly fail should ideally be inside test()
     xsub = substitute(x)
