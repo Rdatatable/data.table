59854c126e38081822c4068b59c4b5e86df2ad61
Arun Srinivasan <aragorn168b@gmail.com>
2014-10-15 21:31:57 +0200

    Tweaked na.omit() with a much faster version.

--- R/data.table.R
+++ R/data.table.R
@@ -1966,18 +1966,17 @@ subset.data.table <- function (x, subset, select, ...)
     ans
 }
 
+# Equivalent of 'rowSums(is.na(dt) > 0L)' but much faster and memory efficient.
+# Also handles bit64::integer64. TODO: export this?
+is_na <- function(x) .Call(Cdt_na, x)
+
 na.omit.data.table <- function (object, ...)
 {
     if (!cedta()) return(NextMethod())
-    omit = FALSE
-    for (i in seq_len(ncol(object))) omit = omit | is.na(object[[i]])
-    object[!omit]
+    if (!length(object)) return(object)
+    .Call(CsubsetDT, object, which(!is_na(object)), seq_along(object))
     # compare the above to stats:::na.omit.data.frame
 }
-
-# Equivalent of 'rowSums(is.na(dt) > 0L)' but much faster and memory efficient
-# TODO: export this?
-is_na <- function(x) .Call(Cdt_na, x)
 
 is.na.data.table <- function (x) {
     if (!cedta()) return(`is.na.data.frame`(x))
