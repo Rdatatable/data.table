09b5d71fd1517a2578399dda9d3cb4cdbeea51ca
Arun Srinivasan <aragorn168b@gmail.com>
2015-01-17 08:26:05 +0100

    Add 'zoo' to suggests and use zoo::index in as.data.table.xts.

--- R/xts.R
+++ R/xts.R
@@ -4,7 +4,7 @@ as.data.table.xts <- function(x, keep.rownames = TRUE, ...){
   if("index" %in% names(x)) stop("Input xts object should not have 'index' column because it would result in duplicate column names. Rename 'index' column in xts or use `keep.rownames=FALSE` and add index manually as another column.")
   r = setDT(as.data.frame(x, row.names=FALSE))
   index = NULL # fix for "no visible binding for global variable ‘index’"
-  r[, index := xts:::index.xts(x)]
+  r[, index := zoo::index(x)]
   setcolorder(r,c("index",names(r)[names(r)!="index"]))[]
 }
 
