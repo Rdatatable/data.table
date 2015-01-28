84ba1151299ba49e833e68a2436630216b653306
Arun Srinivasan <aragorn168b@gmail.com>
2015-01-29 00:59:38 +0100

    Another fix for transpose/setDF, this time with tests.

--- R/transpose.R
+++ R/transpose.R
@@ -1,7 +1,12 @@
 transpose <- function(l, fill=NA, ignore.empty=FALSE) {
 	ans = .Call(Ctranspose, l, fill, ignore.empty)
-	if (is.data.frame(l)) setDT(ans)
-	if (!is.data.table(l)) setDF(ans)
+	if (is.data.table(l)) setDT(ans)
+	else if (is.data.frame(l)) { 
+		if (is.null(names(ans))) 
+        	setattr(ans, "names", paste("V", seq_along(ans), sep = ""))
+    	setattr(ans, "row.names", .set_row_names(length(ans[[1L]])))
+    	setattr(ans, "class", "data.frame")
+	}
 	ans[]
 }
 
