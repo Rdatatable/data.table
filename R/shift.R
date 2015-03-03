a1b012c886054a41a9cb8aa161cc33384de079c5
arunsrinivasan <aragorn168b@gmail.com>
2015-03-03 01:04:41 +0100

    Closes #1056. shift()'s give.names names vector input as well.

--- R/shift.R
+++ R/shift.R
@@ -2,8 +2,11 @@ shift <- function(x, n=1L, fill=NA, type=c("lag", "lead"), give.names=FALSE) {
     type = match.arg(type)
     ans = .Call(Cshift, x, as.integer(n), fill, type)
     if (give.names) {
-        if (is.null(names(x))) 
-            nx = paste("V", if (is.atomic(x)) 1L else seq_along(x), sep="")
+        if (is.null(names(x))) {
+        		xsub = substitute(x)
+        		if (is.atomic(x) && is.name(xsub)) nx = deparse(xsub, 500L)
+        		else nx = paste("V", if (is.atomic(x)) 1L else seq_along(x), sep="")
+        }
         else nx = names(x)
         setattr(ans, 'names', do.call("paste", c(CJ(nx, type, n, sorted=FALSE), sep="_")))
     }
