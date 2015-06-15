270ef42165c121962df1aaa8f97751d42fd69f6c
arunsrinivasan <aragorn168b@gmail.com>
2015-06-15 20:35:29 +0200

    Added helper function 'patterns()' for melting multiple cols.

--- R/fmelt.R
+++ R/fmelt.R
@@ -6,12 +6,20 @@ melt <- function(data, ..., na.rm = FALSE, value.name = "value") {
       reshape2::melt(data, ..., na.rm=na.rm, value.name=value.name)
 }
 
+patterns <- function(...) {
+    p = unlist(list(...), use.names=FALSE)
+}
+
 melt.data.table <- function(data, id.vars, measure.vars, variable.name = "variable", 
            value.name = "value", ..., na.rm = FALSE, variable.factor = TRUE, value.factor = FALSE, 
            verbose = getOption("datatable.verbose")) {
     if (!is.data.table(data)) stop("'data' must be a data.table")
     if (missing(id.vars)) id.vars=NULL
     if (missing(measure.vars)) measure.vars = NULL
+    measure.sub = substitute(measure.vars)
+    if (is.call(measure.sub) && measure.sub[[1L]] == "patterns") {
+        measure.vars = lapply(eval(measure.sub), grep, names(data))    
+    }
     if (is.list(measure.vars)) {
         if (length(value.name) == 1L)  
           value.name = paste(value.name, seq_along(measure.vars), sep="")
