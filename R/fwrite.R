8696b9fc156ef1f021da35f549d6a71cf56844f9
Otto Seiskari <otto.seiskari@gmail.com>
2016-03-28 11:17:23 +0300

    minor changes in fwrite: renamed param dt to x, removed requirement of unique column names, replaced %in% -> %chin%

--- R/fwrite.R
+++ R/fwrite.R
@@ -1,11 +1,10 @@
-fwrite <- function(dt, file.path, append = FALSE, quote = TRUE,
+fwrite <- function(x, file.path, append = FALSE, quote = TRUE,
                    sep = ",", eol = "\n", col.names = TRUE, qmethod = "double",
                    block.size = 10000) {
   
   # validate arguments
-  stopifnot(is.data.frame(dt))
-  stopifnot(ncol(dt) > 0)
-  stopifnot(identical(unique(names(dt)), names(dt)))
+  stopifnot(is.data.frame(x))
+  stopifnot(ncol(x) > 0)
   
   stopifnot(length(quote) == 1 && class(quote) == "logical")
   stopifnot(length(sep) == 1 && class(sep) == "character" && nchar(sep) == 1)
@@ -15,25 +14,25 @@ fwrite <- function(dt, file.path, append = FALSE, quote = TRUE,
   stopifnot(length(append) == 1 && class(append) == "logical")
   stopifnot(length(block.size) == 1 && block.size > 0)
   
-  quoted_cols <- rep(quote, ncol(dt))
+  quoted_cols <- rep(quote, ncol(x))
   
-  # special case: single-column data.frame, doing dt[block_begin:block_end,]
+  # special case: single-column data.frame, doing x[block_begin:block_end,]
   # for such data frame gives a vector
-  if (!is.data.table(dt) && ncol(dt) == 1) dt <- as.data.table(dt)
+  if (!is.data.table(x) && ncol(x) == 1) x <- as.data.table(x)
   
   # write header row separately for correct quoting of row names
   if (col.names && !append) {
-    .Call(Cwritefile, as.list(names(dt)), file.path, sep, eol, quoted_cols, qmethod == "escape", append)
+    .Call(Cwritefile, as.list(names(x)), file.path, sep, eol, quoted_cols, qmethod == "escape", append)
     append <- TRUE
   }
   
-  # handle empty dt
-  if (nrow(dt) == 0) return()
+  # handle empty x
+  if (nrow(x) == 0) return()
   
   # determine from column types, which ones should be quoted
   if (quote) {
-    column_types <- lapply(dt, class)
-    quoted_cols <- column_types %in% c('character', 'factor')
+    column_types <- sapply(x, class)
+    quoted_cols <- column_types %chin% c('character', 'factor')
   }
   
   # write in blocks of given size to avoid generating full copies
@@ -41,9 +40,9 @@ fwrite <- function(dt, file.path, append = FALSE, quote = TRUE,
   block_begin <- 1
   
   repeat {
-    block_end <- min(block_begin+(block.size-1), nrow(dt))
+    block_end <- min(block_begin+(block.size-1), nrow(x))
     
-    dt_block <- dt[c(block_begin:block_end),]
+    dt_block <- x[c(block_begin:block_end),]
     
     # convert data.frame row block to a list of columns
     col_list <- lapply(dt_block, function(column) {
@@ -54,7 +53,7 @@ fwrite <- function(dt, file.path, append = FALSE, quote = TRUE,
     
     .Call(Cwritefile, col_list, file.path, sep, eol, quoted_cols, qmethod == "escape", append)
     
-    if (block_end == nrow(dt)) break
+    if (block_end == nrow(x)) break
     
     append <- TRUE
     block_begin <- block_end+1
