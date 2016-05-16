2bb7b5ca975c31cdbabe19016c758748cd4bf3f2
Seth Wenchel <wenchel@gmail.com>
2016-05-16 12:21:05 -0400

    Closes #1706, library(data.table) fails if "Packaged" field is not present in DESCRIPTION

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,13 +1,18 @@
 .onAttach <- function(libname, pkgname) {
-    # Runs when attached to search() path such as by library() or require()
-    if (interactive()) {
-        v = packageVersion("data.table")
-        d = read.dcf(system.file("DESCRIPTION", package="data.table"))[,"Packaged"]
-        dev = as.integer(v[1,3])%%2 == 1  # version number odd
-        packageStartupMessage("data.table ", v, if(dev) paste0(" IN DEVELOPMENT built ", d))
-        if (dev && (Sys.Date() - as.Date(d))>28) packageStartupMessage("**********\nThis development version of data.table was built more than 4 weeks ago. Please update.\n**********")
-        packageStartupMessage('For help type ?data.table or https://github.com/Rdatatable/data.table/wiki')
-        packageStartupMessage('The fastest way to learn (by data.table authors): https://www.datacamp.com/courses/data-analysis-the-data-table-way')
+  # Runs when attached to search() path such as by library() or require()
+  if (interactive()) {
+    v = packageVersion("data.table")
+    d = try(read.dcf(system.file("DESCRIPTION", package="data.table"))[,"Packaged"])
+    if(inherits(d, "try-error")){
+      d = try(strsplit(read.dcf(system.file("DESCRIPTION", package="data.table"))[,"Built"], split="; ")$Built[3])
+      if(inherits(d, "try-error")){
+        return()
+      }
     }
+    dev = as.integer(v[1,3])%%2 == 1  # version number odd
+    packageStartupMessage("data.table ", v, if(dev) paste0(" IN DEVELOPMENT built ", d))
+    if (dev && (Sys.Date() - as.Date(d))>28) packageStartupMessage("**********\nThis development version of data.table was built more than 4 weeks ago. Please update.\n**********")
+    packageStartupMessage('For help type ?data.table or https://github.com/Rdatatable/data.table/wiki')
+    packageStartupMessage('The fastest way to learn (by data.table authors): https://www.datacamp.com/courses/data-analysis-the-data-table-way')
+  }
 }
-
