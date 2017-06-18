063002239f7a1ed7239ca73489664e8994e6e854
Pasha Stetsenko <pasha@h2o.ai>
2017-06-18 02:01:16 -0700

    Simplify branching logic inside skip_white() by introducing global variable antisep

--- src/fread.c
+++ src/fread.c
@@ -26,6 +26,7 @@
 // Private globals to save passing all of them through to highly iterated field processors
 static const char *eof;
 static char sep, eol, eol2;
+static char antisep;
 static int eolLen;
 static char quote, dec;
 static int quoteRule;
@@ -99,7 +100,7 @@ void freadCleanup(void)
     // if (eof) for when file is empty and STOP() is called before eof has been set (test 885)
   }
   fileSize = 0;
-  sep = eol = eol2 = quote = dec = '\0';
+  sep = antisep = eol = eol2 = quote = dec = '\0';
   eolLen = 0;
   quoteRule = -1;
   any_number_like_NAstrings = false;
@@ -142,7 +143,11 @@ static void printTypes(int ncol) {
 static inline void skip_white(const char **this) {
   // skip space so long as sep isn't space and skip tab so long as sep isn't tab
   const char *ch = *this;
-  while(ch<eof && (*ch==' ' || *ch== '\t') && *ch!=sep) ch++;
+  if (antisep == 0) {
+    while (*ch == ' ' || *ch == '\t') ch++;
+  } else {
+    while (*ch == antisep) ch++;
+  }
   *this = ch;
 }
 
@@ -844,6 +849,7 @@ int freadMain(freadMainArgs __args) {
     int numLines[JUMPLINES+1];
     for (int s=0; s<nseps; s++) {
       sep = seps[s];
+      antisep = (sep == ' ')? '\t' : (sep == '\t')? ' ' : 0;
       for (quoteRule=0; quoteRule<4; quoteRule++) {  // quote rule in order of preference
         ch = pos;
         // if (args.verbose) DTPRINT("Trying sep='%c' with quoteRule %d ...\n", sep, quoteRule);
@@ -886,6 +892,7 @@ int freadMain(freadMainArgs __args) {
     int ncol;
     quoteRule = topQuoteRule;
     sep = topSep;
+    antisep = (sep == ' ')? '\t' : (sep == '\t')? ' ' : 0;
     ch = pos;
     if (fill) {
       // start input from first populated line, already pos.
