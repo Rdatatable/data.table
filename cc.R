e83c34cc3a664fecd9ead4956a127f95fdf458ba
Matt Dowle <mattjdowle@gmail.com>
2017-08-09 15:40:47 -0700

    Added -j to MAKEFLAGS for compilation of *.c in parallel

--- cc.R
+++ cc.R
@@ -54,9 +54,9 @@ cc = function(test=TRUE, clean=FALSE, debug=FALSE, cc_dir=Sys.getenv("CC_DIR")) 
   if (clean) system("rm *.o *.so")
 
   if (debug) {
-    ret = system("MAKEFLAGS='CC=gcc CFLAGS=-std=gnu99\\ -O0\\ -ggdb\\ -Wall\\ -pedantic' R CMD SHLIB -d -o data.table.so *.c")
+    ret = system("MAKEFLAGS='-j CC=gcc CFLAGS=-std=gnu99\\ -O0\\ -ggdb\\ -Wall\\ -pedantic' R CMD SHLIB -d -o data.table.so *.c")
   } else {
-    ret = system("MAKEFLAGS='CC=gcc CFLAGS=-fopenmp\\ -std=gnu99\\ -O3\\ -pipe\\ -Wall\\ -pedantic' R CMD SHLIB -o data.table.so *.c")
+    ret = system("MAKEFLAGS='-j CC=gcc CFLAGS=-fopenmp\\ -std=gnu99\\ -O3\\ -pipe\\ -Wall\\ -pedantic' R CMD SHLIB -o data.table.so *.c")
   }
   if (ret) return()
   # clang -Weverything includes -pedantic and issues many more warnings than gcc
