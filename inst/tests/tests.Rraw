Rdatatable/data.table

commit c98142cbf7aeaf19c4071be1ac6b1b2a4b679c5b

Do not include symbol on lhs of := in othervars (#2329)
Do not include symbol on lhs of := in othervars. Closes #2329

R/data.table.R
@@ -1051,9 +1051,15 @@ chmatch2 <- function(x, table, nomatch=NA_integer_) {
                 }
                 # fix for long standing FR/bug, #495 and #484
                 allcols = c(names(x), xdotprefix, names(i), idotprefix)
-                if ( length(othervars <- setdiff(intersect(av, allcols), c(bynames, ansvars))) ) {
-                    # we've a situation like DT[, c(sum(V1), lapply(.SD, mean)), by=., .SDcols=...] or 
-                    # DT[, lapply(.SD, function(x) x *v1), by=, .SDcols=...] etc., 
+
+                # Do not include z in othervars when j is z := expr (#2326)
+                av_allcols <- intersect(av, allcols)
+                if (is.call(jsub) && length(jsub[[1L]]) == 1L && jsub[[1L]] == ":=" && is.symbol(jsub[[2L]])) {
+                  av_allcols <- setdiff(av_allcols, as.character(jsub[[2L]]))
+                }
+                if ( length(othervars <- setdiff(av_allcols, c(bynames, ansvars))) ) {
+                    # we've a situation like DT[, c(sum(V1), lapply(.SD, mean)), by=., .SDcols=...] or
+                    # DT[, lapply(.SD, function(x) x *v1), by=, .SDcols=...] etc.,
                     ansvars = union(ansvars, othervars)
                     ansvals = chmatch(ansvars, names(x))
                 }

inst/tests/tests.Rraw
@@ -10488,6 +10488,14 @@ src = paste(c("A,B",
             collapse="\n")
 test(1822, fread(src), error="Expecting 2 cols but row .+ contains only 1 cols")
 
+# Issue 2326: .SD mistakenly includes column being set when get() appears in j
+DT <- data.table(x = seq(1, 10), y = seq(10, 1))
+DT[, z := ncol(.SD) + y, .SDcols = "x"]
+test(1823.1, DT$z, seq(11, 2))
+DT[, z := ncol(.SD) + get("y"), .SDcols = "x"]
+test(1823.2, DT$z, seq(11, 2))
+DT[, z := ncol(.SD) + get("y"), .SDcols = c("x", "z")]
+test(1823.3, DT$z, seq(12, 3))
 
 ##########################
 

commit cbf13f112653f173f434ca456dc2163005eaaf44

Fix a regression introduced in #2329. Closes #2338.

R/data.table.R
@@ -1051,13 +1051,7 @@ chmatch2 <- function(x, table, nomatch=NA_integer_) {
                 }
                 # fix for long standing FR/bug, #495 and #484
                 allcols = c(names(x), xdotprefix, names(i), idotprefix)
-
-                # Do not include z in othervars when j is z := expr (#2326)
-                av_allcols <- intersect(av, allcols)
-                if (is.call(jsub) && length(jsub[[1L]]) == 1L && jsub[[1L]] == ":=" && is.symbol(jsub[[2L]])) {
-                  av_allcols <- setdiff(av_allcols, as.character(jsub[[2L]]))
-                }
-                if ( length(othervars <- setdiff(av_allcols, c(bynames, ansvars))) ) {
+                if ( length(othervars <- setdiff(intersect(av, allcols), c(bynames, ansvars))) ) {
                     # we've a situation like DT[, c(sum(V1), lapply(.SD, mean)), by=., .SDcols=...] or
                     # DT[, lapply(.SD, function(x) x *v1), by=, .SDcols=...] etc.,
                     ansvars = union(ansvars, othervars)
@@ -1084,6 +1078,14 @@ chmatch2 <- function(x, table, nomatch=NA_integer_) {
                     cat("'(m)get' found in j. ansvars being set to all columns. Use .SDcols or a single j=eval(macro) instead. Both will detect the columns used which is important for efficiency.\nOld:", paste(ansvars,collapse=","),"\n")
                     # get('varname') is too difficult to detect which columns are used in general
                     # eval(macro) column names are detected via the  if jsub[[1]]==eval switch earlier above.
+                }
+
+                # Do not include z in .SD when dt[, z := {.SD; get("x")}, .SDcols = "y"] (#2326, #2338)
+                if (is.call(jsub) && length(jsub[[1L]]) == 1L && jsub[[1L]] == ":=" && is.symbol(jsub[[2L]])) {
+                  jsub_lhs_symbol <- as.character(jsub[[2L]])
+                  if (jsub_lhs_symbol %chin% othervars) {
+                    ansvars <- setdiff(ansvars, jsub_lhs_symbol)
+                  }
                 }
                 if (length(ansvars)) othervars = ansvars # #1744 fix
                 allcols = c(names(x), xdotprefix, names(i), idotprefix)

inst/tests/tests.Rraw
@@ -10494,6 +10494,12 @@ DT[, z := ncol(.SD) + get("y"), .SDcols = "x"]
 test(1823.2, DT$z, seq(11, 2))
 DT[, z := ncol(.SD) + get("y"), .SDcols = c("x", "z")]
 test(1823.3, DT$z, seq(12, 3))
+DT[, x := {.SD; x + 1}, .SDcols = "y"]
+test(1823.4, DT$x, seq(2, 11))
+DT[, z := ncol(.SD) + x, .SDcols = "y"]
+test(1823.5, DT$z, seq(3, 12))
+DT[, z := ncol(.SD) + x, .SDcols = c("x", "y")]
+test(1823.6, DT$z, seq(4, 13))
 
 ##########################
 