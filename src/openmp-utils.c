a0e38f672af518c7b398085b2201fdff2ae87049
Matt Dowle <mattjdowle@gmail.com>
2017-10-10 20:29:10 -0700

    Backport for CRAN patch (1.10.4-1) already on CRAN.

--- src/openmp-utils.c
+++ src/openmp-utils.c
@@ -39,27 +39,42 @@ SEXP setDTthreads(SEXP threads) {
         error("Argument to setDTthreads must be a single integer >= 0. \
             Default 0 is recommended to use all CPU.");
     }
-    // do not call omp_set_num_threads() here as that affects other openMP
-    // packages and base R as well potentially.
     int old = DTthreads;
     DTthreads = INTEGER(threads)[0];
+    if (omp_get_max_threads() < omp_get_thread_limit()) {
+      if (DTthreads==0) {
+        // for example after test 1705 has auto switched to single-threaded for parallel's fork,
+        // we want to return to multi-threaded.
+        // omp_set_num_threads() sets the value returned by omp_get_max_threads()
+        omp_set_num_threads(omp_get_thread_limit());
+      } else if (DTthreads > omp_get_max_threads()) {
+        omp_set_num_threads( MIN(DTthreads, omp_get_thread_limit()) );
+      }
+    }
     return ScalarInteger(old);
 }
 
 // auto avoid deadlock when data.table called from parallel::mclapply
-static int preFork_DTthreads = 0;
 void when_fork() {
-    preFork_DTthreads = DTthreads;
+
+    // attempted workaround for Intel's OpenMP implementation which leaves threads running after
+    // parallel region; these crash when forked.
+    omp_set_num_threads(1);
+
+    // GNU OpenMP seems ok with just setting DTthreads to 1 which limits the next parallel region
+    // if data.table is used within the fork'd proceess.
     DTthreads = 1;
+
+    // We used to have an after_fork() callback too, to return to multi-threaded mode after parallel's
+    // fork completes. But now in an attempt to alleviate problems propogating (likely Intel's OpenMP only)
+    // we now leave data.table in single-threaded mode after parallel's fork. User can call setDTthreads(0)
+    // to return to multi-threaded as we do in tests on Linux.
 }
-void when_fork_end() {
-    DTthreads = preFork_DTthreads;
-}
+
 void avoid_openmp_hang_within_fork() {
     // Called once on loading data.table from init.c
 #ifdef _OPENMP
-    pthread_atfork(&when_fork, &when_fork_end, NULL);
+    pthread_atfork(&when_fork, NULL, NULL);
 #endif
 }
-
 
