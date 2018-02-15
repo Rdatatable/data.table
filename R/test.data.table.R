c03189c8441a983f56411a28f18e9af50c8ead0f
Matt Dowle <mattjdowle@gmail.com>
2018-02-15 13:41:02 -0800

    Change compactprint() to show 5 rows, to reveal data difference on Windows fail in AppVeyor log

--- R/test.data.table.R
+++ R/test.data.table.R
@@ -50,11 +50,11 @@ compactprint <- function(DT, topn=2L) {
   cn = paste0(" [Key=",makeString(key(DT)),
              " Types=", makeString(substring(sapply(DT, typeof), 1L, 3L)),
              " Classes=", makeString(tt), "]")
-  if (nrow(DT)) {
+  if (nrow(DT)>(topn+topn+1)) {
     print(copy(DT)[,(cn):=""], topn=topn)
   } else {
-    print(DT)  # "Empty data.table (0 rows) of <ncol> columns ...
-    if (ncol(DT)) cat(cn,"\n")
+    print(DT)
+    if (nrow(DT)==0 && ncol(DT)>0) cat(cn,"\n")  # "Empty data.table (0 rows) of <ncol> columns ...
   }
   invisible()
 }
