e7841d2fc0229498281454a77ed807adc46cef07
Matt Dowle <mattjdowle@gmail.com>
2018-03-08 10:50:44 -0800

    Get now-superfluous PROTECT on ScalarLogical out of the way first, now we depend on R3.1

--- CRAN_Release.cmd
+++ CRAN_Release.cmd
@@ -12,49 +12,53 @@ q("no")
 # Ensure latest version of R otherwise problems with CRAN not finding dependents that depend on latest R
 # e.g. mirror may have been disabled in sources.list when upgrading ubuntu
 
+rm ./src/*.so
+rm ./src/*.o
+rm -rf ./data.table.Rcheck
+
 # Ensure no non-ASCII, other than in README.md is ok
 # tests.Rraw in particular have failed CRAN Solaris (only) due to this.
-# No unicode either. Put these tests in DtNonAsciiTests package.
-grep -RI --exclude-dir=".git" --exclude="*.md" --exclude="*~" --color='auto' -P -n "[\x80-\xFF]" data.table/
-grep -RI --exclude-dir=".git" --exclude="*.md" --exclude="*~" --color='auto' -n "[\]u[0-9]" data.table/
+grep -RI --exclude-dir=".git" --exclude="*.md" --exclude="*~" --color='auto' -P -n "[\x80-\xFF]" ./
+
+# No unicode either?! Put these tests in DtNonAsciiTests package. Trying this again to see what happens now that Solaris is dead. Tests 1864.1 and 1864.2
+grep -RI --exclude-dir=".git" --exclude="*.md" --exclude="*~" --color='auto' -n "[\]u[0-9]" ./
 
 # Ensure no calls to omp_set_num_threads() [to avoid affecting other packages and base R]
-grep --exclude="data.table/src/openmp-utils.c" omp_set_num_threads data.table/src/*
+grep --exclude="./src/openmp-utils.c" omp_set_num_threads ./src/*
 
 # Ensure no calls to omp_get_max_threads() also since access should be via getDTthreads()
-grep --exclude="data.table/src/openmp-utils.c" omp_get_max_threads data.table/src/*
+grep --exclude="./src/openmp-utils.c" omp_get_max_threads ./src/*
 
 # Ensure all #pragama omp parallel directives include a num_threads() clause
-grep "pragma omp parallel" data.table/src/*.c
+grep "pragma omp parallel" ./src/*.c
 
 # Ensure all .Call's first argument are unquoted.  TODO - change to use INHERITS()
-grep "[.]Call(\"" data.table/R/*.R
+grep "[.]Call(\"" ./R/*.R
 
 # Ensure no Rprintf in init.c
-grep "Rprintf" data.table/src/init.c
+grep "Rprintf" ./src/init.c
 
 # workaround for IBM AIX - ensure no globals named 'nearest' or 'class'.
 # See https://github.com/Rdatatable/data.table/issues/1351
-grep "nearest *=" data.table/src/*.c  # none
-grep "class *=" data.table/src/*.c    # quite a few but none global
+grep "nearest *=" ./src/*.c  # none
+grep "class *=" ./src/*.c    # quite a few but none global
 
 # No undefined type punning of the form:  *(long long *)&REAL(column)[i]
 # Failed clang 3.9.1 -O3 due to this, I think.
-grep "&REAL" data.table/src/*.c
+grep "&REAL" ./src/*.c
 
 # No use of long long, instead use int64_t. TODO
-# grep "long long" data.table/src/*.c
+# grep "long long" ./src/*.c
 
-# No tabs in C or R code (sorry Richard Hendricks)
-grep -P "\t" data.table/R/*.R
-grep -P "\t" data.table/src/*.c
-
+# No tabs in C or R code (sorry, Richard Hendricks)
+grep -P "\t" ./R/*.R
+grep -P "\t" ./src/*.c
 
 # seal leak potential where two unprotected API calls are passed to the same
 # function call, usually involving install() or mkChar()
 # Greppable thanks to single lines and wide screens
 # See comments in init.c
-cd data.table/src
+cd ./src
 grep install.*alloc *.c   --exclude init.c
 grep install.*Scalar *.c
 grep alloc.*install *.c   --exclude init.c
@@ -71,10 +75,10 @@ grep "install(" *.c       --exclude init.c   # TODO: perhaps in future pre-insta
 
 # ScalarInteger and ScalarString allocate and must be PROTECTed unless i) returned (which protects),
 # or ii) passed to setAttrib (which protects, providing leak-seals above are ok)
-# ScalarLogical in R now returns R's global TRUE but from R 3.1.0; Apr 2014. Before that it allocates.
+# ScalarLogical in R now returns R's global TRUE from R 3.1.0; Apr 2014. Before that it allocated.
 # Aside: ScalarInteger may return globals for small integers in future version of R.
 grep ScalarInteger *.c   # Check all Scalar* either PROTECTed, return-ed or passed to setAttrib.
-grep ScalarLogical *.c   # When we move R dependency to 3.1.0+, no need to protect ScalarLogical
+grep ScalarLogical *.c   # Now we depend on 3.1.0, check ScalarLogical is NOT PROTECTed.
 grep ScalarString *.c
 
 cd
