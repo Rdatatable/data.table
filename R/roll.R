e84b30d307c9a7604f1097e225acb17acf755c51
jangorecki <j.gorecki@wit.edu.pl>
2018-04-17 21:31:12 +0530

    dev

--- R/roll.R
+++ R/roll.R
@@ -3,3 +3,14 @@ rollmean <- function(x, n, fill=NA) {
   ans
 }
 
+srollmean <- function(x, n, fill=NA, na.rm=TRUE) {
+  stopifnot(is.numeric(x))
+  if (!(l<-length(x))) return(x)
+  ans = rep(as.double(fill), l)
+  for (i in seq.int(length(x))) {
+    if (i < n) next
+    ans[i] = mean(x[(i-n+1L):i], na.rm=na.rm)
+  }
+  ans
+}
+
