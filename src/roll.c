0e5c6259584c7d014480c111ce8d48553ae77faf
jangorecki <j.gorecki@wit.edu.pl>
2018-04-20 12:39:07 +0530

    getting closer

--- src/roll.c
+++ src/roll.c
@@ -3,10 +3,9 @@
 
 SEXP rollmean(SEXP obj, SEXP k, SEXP fill) {
 
-  //size_t size;
-  R_len_t i=0, j, m, nx, nk, xrows, thisk, protecti=0, sum=0;
-  SEXP x, tmp=R_NilValue, this, ans, thisfill; //, class;
-  //unsigned long long *dthisfill;
+  R_len_t i=0, j, m, nx, nk, xrows, thisk, protecti=0;
+  SEXP x, tmp=R_NilValue, this, ans, thisfill;
+  long double window;
   if (!length(obj)) return(obj); // NULL, list()
   if (isVectorAtomic(obj)) {
     x = allocVector(VECSXP, 1);
@@ -20,7 +19,7 @@ SEXP rollmean(SEXP obj, SEXP k, SEXP fill) {
     error("fill must be a vector of length 1");
 
   nx = length(x); nk = length(k);
-  i=0;
+  i = 0;
   while(i < nk && INTEGER(k)[i] >= 0) i++;
   if (i != nk)
     error("n must be non-negative integer values (>= 0)");
@@ -30,42 +29,38 @@ SEXP rollmean(SEXP obj, SEXP k, SEXP fill) {
   
   // loop over columns in 'obj' input
   for (i=0; i<nx; i++) {
-    Rprintf("rollmean.c: column: %d\n", i);
+    Rprintf("rollmean.c: col %d\n", i);
     this  = VECTOR_ELT(x, i);
     if (!isReal(this)) error("Currently only 'double' type is supported in rollmean");
-    //size  = SIZEOF(this);
     xrows = length(this);
-      
+
     // loop over multiple windows provided as 'k' input
     for (j=0; j<nk; j++) {
-      Rprintf("rollmean.c: window: %d\n", j);
+      Rprintf("rollmean.c: col %d: win %d\n", i, j);
       thisk = (xrows >= INTEGER(k)[j]) ? INTEGER(k)[j] : xrows;
       tmp = allocVector(REALSXP, xrows);
       SET_VECTOR_ELT(ans, i*nk+j, tmp);
-      //// shift - to remove
-      //if (xrows - INTEGER(k)[j] >= 0) {
-      //  memcpy((char *)DATAPTR(tmp)+(INTEGER(k)[j]*size),
-      //       (char *)DATAPTR(this),
-      //       (xrows-INTEGER(k)[j])*size);
-      //}
-      //for (m=0; m<thisk; m++) {
-      //  Rprintf("rollmean.c: fill: %d\n", m);
-      //  REAL(tmp)[m] = REAL(thisfill)[0];
-      //}
-      sum = 0;
+      
+      window = 0.;
       for (m=0; m<xrows; m++) {
-        Rprintf("rollmean.c: row: %d\n", m);
-        sum += REAL(VECTOR_ELT(tmp, m));
-        if (m - INTEGER(k)[j] > 0) {
-          sum -= REAL(VECTOR_ELT(tmp, m - INTEGER(k)[j] + 1));
+        Rprintf("rollmean.c: col %d: win %d: row %d\n", i, j, m);
+        // add current row to window sum
+        Rprintf("rollmean.c: col %d: win %d: row %d: to add to window: %8.3f\n", i, j, m, REAL(this)[m]);
+        window += REAL(this)[m];
+        // remove row from window sum that is outside of the window already
+        if (m - INTEGER(k)[j] >= 0) {
+          //REAL(VECTOR_ELT(tmp, m - INTEGER(k)[j] + 1))[0]
+          Rprintf("rollmean.c: col %d: win %d: row %d: to subtract from window: %8.3f\n", i, j, m, REAL(this)[m-INTEGER(k)[j]+1]);
+          window -= REAL(this)[m-INTEGER(k)[j]+1];
+        } else {
+          Rprintf("rollmean.c: col %d: win %d: row %d: no subtract from window because window does not have full length yet\n", i, j, m);
         }
-        Rprintf("rollmean.c: row: current sum: %8.3f\n", sum);
+        Rprintf("rollmean.c: col %d: win %d: row %d: window sum: %8.3f\n", i, j, m, window);
         if (m<thisk) {
           REAL(tmp)[m] = REAL(thisfill)[0];
         } else {
-          //s[thisgrp] += REAL(x)[ix];
           REAL(tmp)[m] = REAL(thisfill)[0]; //TODO
-          //REAL(tmp)[m] = sum / INTEGER(k)[j]
+          //REAL(tmp)[m] = window / INTEGER(k)[j]
         }
       }
       copyMostAttrib(this, tmp);
