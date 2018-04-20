ce9155984516134fd8b24c24ffd8f5556d940246
jangorecki <j.gorecki@wit.edu.pl>
2018-04-20 12:39:07 +0530

    getting closer

--- roll-dev.R
+++ roll-dev.R
@@ -1,7 +1,8 @@
 cc(F)
-d = as.data.table(list(as.double(1:10), as.double(2:11)))
-d
-ans = rollmean(d, 4)
-dd = as.data.table(ans)
-dd
-str(dd)
+d = as.data.table(list(1:10/2, 1:10/0.64))
+replicate(5, cat("====================================================================\n")) -> devnul
+
+ans = rollmean(d, c(3, 4))
+as.data.table(ans)
+
+d[, paste0("R",1:4) := rollmean(.SD, c(3, 4)), .SDcols=c("V1","V2")][]
