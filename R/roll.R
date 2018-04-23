b2949882d80256e405010050c5275e0481434b73
jangorecki <j.gorecki@wit.edu.pl>
2018-04-23 22:30:57 +0530

    new na.rm argument

--- R/roll.R
+++ R/roll.R
@@ -1,5 +1,5 @@
-rollmean <- function(x, n, fill=NA, align=c("right", "left", "center"), adaptive=is.list(n)) {
+rollmean <- function(x, n, fill=NA, align=c("right", "left", "center"), na.rm=FALSE, adaptive=is.list(n)) {
   align =  match.arg(align)
-  ans = .Call(Crollmean, x, as.integer(n), fill, align, adaptive)
+  ans = .Call(Crollmean, x, as.integer(n), fill, align, na.rm, adaptive)
   ans
 }
