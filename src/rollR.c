da143a033289a5818d435e2ef217ec73582a3982
jangorecki <j.gorecki@wit.edu.pl>
2018-05-20 15:41:18 +0530

    window width argument in plain C

--- src/rollR.c
+++ src/rollR.c
@@ -88,19 +88,21 @@ SEXP rollfun(SEXP fun, SEXP obj, SEXP k, SEXP fill, SEXP exact, SEXP align, SEXP
     // array of pointer to 'this'
   }
 
-  SEXP thisfill = PROTECT(coerceVector(fill, REALSXP)); protecti++;
-  double dfill = REAL(thisfill)[0];
-
   enum {MEAN, SUM} sfun;
   if (!strcmp(CHAR(STRING_ELT(fun, 0)), "mean")) sfun = MEAN;
   else if (!strcmp(CHAR(STRING_ELT(fun, 0)), "sum")) sfun = SUM;
   else error("Internal error: invalid fun argument in rolling function, should have been caught before. please report to data.table issue tracker.");
 
+  int* ik = INTEGER(k);
+  
   int ialign;
   if (!strcmp(CHAR(STRING_ELT(align, 0)), "right")) ialign = 1;
   else if (!strcmp(CHAR(STRING_ELT(align, 0)), "center")) ialign = 0;
   else if (!strcmp(CHAR(STRING_ELT(align, 0)), "left")) ialign = -1;
   else error("Internal error: invalid align argument in rolling function, should have been caught before. please report to data.table issue tracker.");
+
+  SEXP thisfill = PROTECT(coerceVector(fill, REALSXP)); protecti++;
+  double dfill = REAL(thisfill)[0];
 
   bool bexact = LOGICAL(exact)[0];
   
@@ -111,19 +113,17 @@ SEXP rollfun(SEXP fun, SEXP obj, SEXP k, SEXP fill, SEXP exact, SEXP align, SEXP
     LOGICAL(hasna)[0]==TRUE ? 1 :       // hasna TRUE, might be some NAs
     -1;                                 // hasna FALSE, there should be no NAs
 
-  
-
   if (nx==1 && nk==1) {                                                 // no need to init openmp for single thread call
     if (iverbose>0) Rprintf("rollfun: single window and single column, skipping parallel execution\n");
     this = AS_NUMERIC(VECTOR_ELT(x, 0));
     tmp = VECTOR_ELT(ans, 0);
     switch (sfun) {
       case MEAN :
-        if (!badaptive) rollmeanVector(REAL(this), xrows[0], REAL(tmp), INTEGER(k)[0], ialign, dfill, bexact, bnarm, ihasna, iverbose);
+        if (!badaptive) rollmeanVector(REAL(this), xrows[0], REAL(tmp), ik[0], ialign, dfill, bexact, bnarm, ihasna, iverbose);
         else rollmeanVectorAdaptive(REAL(this), xrows[0], REAL(tmp), INTEGER(VECTOR_ELT(kl, 0)), dfill, bexact, bnarm, ihasna, iverbose);
         break;
       case SUM :
-        if (!badaptive) rollsumVector(REAL(this), xrows[0], REAL(tmp), INTEGER(k)[0], ialign, dfill, bexact, bnarm, ihasna, iverbose);
+        if (!badaptive) rollsumVector(REAL(this), xrows[0], REAL(tmp), ik[0], ialign, dfill, bexact, bnarm, ihasna, iverbose);
         else rollsumVectorAdaptive(REAL(this), xrows[0], REAL(tmp), INTEGER(VECTOR_ELT(kl, 0)), dfill, bexact, bnarm, ihasna, iverbose);
         break;
     }
@@ -138,11 +138,11 @@ SEXP rollfun(SEXP fun, SEXP obj, SEXP k, SEXP fill, SEXP exact, SEXP align, SEXP
           tmp = VECTOR_ELT(ans, i*nk+j); // tmp is SEXP, this is probably not parallel safe
           switch (sfun) {
             case MEAN :
-              if (!badaptive) rollmeanVector(REAL(this), xrows[i], REAL(tmp), INTEGER(k)[j], ialign, dfill, bexact, bnarm, ihasna, iverbose);
+              if (!badaptive) rollmeanVector(REAL(this), xrows[i], REAL(tmp), ik[j], ialign, dfill, bexact, bnarm, ihasna, iverbose);
               else rollmeanVectorAdaptive(REAL(this), xrows[i], REAL(tmp), INTEGER(VECTOR_ELT(kl, j)), dfill, bexact, bnarm, ihasna, iverbose);
               break;
             case SUM :
-              if (!badaptive) rollsumVector(REAL(this), xrows[i], REAL(tmp), INTEGER(k)[j], ialign, dfill, bexact, bnarm, ihasna, iverbose);
+              if (!badaptive) rollsumVector(REAL(this), xrows[i], REAL(tmp), ik[j], ialign, dfill, bexact, bnarm, ihasna, iverbose);
               else rollsumVectorAdaptive(REAL(this), xrows[i], REAL(tmp), INTEGER(VECTOR_ELT(kl, j)), dfill, bexact, bnarm, ihasna, iverbose);
               break;
           }
