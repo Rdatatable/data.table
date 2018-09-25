f26b15ea79b8900863473a31b91356686f965879
MarkusBonsch <mbonsch@posteo.de>
2018-09-25 23:57:08 +0200

    Modified code to increase test coverage

--- src/assign.c
+++ src/assign.c
@@ -576,13 +576,8 @@ SEXP assign(SEXP dt, SEXP rows, SEXP cols, SEXP newcolnames, SEXP values, SEXP v
             // silence warning on singleton NAs
             if (INTEGER(RHS)[0] != NA_INTEGER) warning("Coerced '%s' RHS to 'integer' to match the factor column's underlying type. Character columns are now recommended (can be in keys), or coerce RHS to integer or character first.", type2char(TYPEOF(thisvalue)));
           } else {
-            // make sure to copy thisvalue if it is referenced. May be modified below. See #2984
-            if(MAYBE_REFERENCED(thisvalue)) {
-               RHS = duplicate(thisvalue);
-            } else {
-              RHS = thisvalue;
-            }
-            
+            // make sure to copy thisvalue. May be modified below. See #2984
+            RHS = duplicate(thisvalue);
           }
           for (j=0; j<length(RHS); j++) {
             if ( (INTEGER(RHS)[j]<1 || INTEGER(RHS)[j]>LENGTH(targetlevels))
