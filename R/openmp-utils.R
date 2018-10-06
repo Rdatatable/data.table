a1703b42f42e6e1e94380e2f9e13ced8d92771a9
MarkusBonsch <mbonsch@posteo.de>
2018-10-07 00:19:49 +0200

    Full purge.

--- R/openmp-utils.R
+++
@@ -1,8 +0,0 @@
-setDTthreads <- function(threads) {
-  invisible(.Call(CsetDTthreads, as.integer(threads)))
-}
-
-getDTthreads <- function(verbose=getOption("datatable.verbose", FALSE)) {
-  .Call(CgetDTthreads, verbose)
-}
-
