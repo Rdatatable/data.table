8a355cf653977752f8d39c23cf0ea6a7714851eb
mattdowle <mattjdowle@gmail.com>
2019-03-14 16:09:00 -0700

    removed unused isDatatable; test 1146 covers #74

--- src/data.table.h
+++ src/data.table.h
@@ -87,7 +87,6 @@ SEXP SelfRefSymbol;
 // assign.c
 SEXP allocNAVector(SEXPTYPE type, R_len_t n);
 void savetl_init(), savetl(SEXP s), savetl_end();
-Rboolean isDatatable(SEXP x);
 int checkOverAlloc(SEXP x);
 
 // forder.c
