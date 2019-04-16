66166b39895c603c70d15653a26be6aaaa7a11bf
Michael Chirico <michaelchirico4@gmail.com>
2019-04-17 05:18:56 +0800

    empty argument in j output caught early & stopped (#3510)

--- R/data.table.R
+++ R/data.table.R
@@ -982,8 +982,10 @@ replace_dot_alias <- function(e) {
           jvnames = names(jsubl)[-1L]   # check list(a=sum(v),v)
           if (is.null(jvnames)) jvnames = rep.int("", length(jsubl)-1L)
           for (jj in seq.int(2L,length(jsubl))) {
-            if (jvnames[jj-1L] == "" && mode(jsubl[[jj]])=="name")
-              jvnames[jj-1L] = gsub("^[.](N|I|GRP|BY)$","\\1",deparse(jsubl[[jj]]))
+            if (jvnames[jj-1L] == "" && mode(jsubl[[jj]])=="name") {
+              if (!nzchar(as.character(jsubl[[jj]]))) stop("Item ", jj-1L, " of the .() or list() passed to j is missing") #3507
+              jvnames[jj-1L] = gsub("^[.](N|I|GRP|BY)$", "\\1", deparse(jsubl[[jj]]))
+            }
             # TO DO: if call to a[1] for example, then call it 'a' too
           }
           setattr(jsubl, "names", NULL)  # drops the names from the list so it's faster to eval the j for each group. We'll put them back aftwards on the result.
