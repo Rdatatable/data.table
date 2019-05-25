d5b66d2b8d00c7ad134f7b529aa8da6fdcaff152
Michael Chirico <michael.chirico@grabtaxi.com>
2019-05-25 18:38:11 +0800

    successfully parallelized where possible

--- src/cj.c
+++ src/cj.c
@@ -7,6 +7,7 @@ SEXP cj(SEXP base_list) {
   for (int j = 0; j < JJ; j++) NN *= LENGTH(VECTOR_ELT(base_list, j));
   int div = NN, modulo;
   
+  //#pragma omp parallel for num_threads(getDTthreads())
   for (int j = 0; j < JJ; j++) {
     SEXP this_v = VECTOR_ELT(base_list, j);
     modulo = div;
@@ -14,17 +15,20 @@ SEXP cj(SEXP base_list) {
     switch(TYPEOF(this_v)) {
     case LGLSXP: {
       SEXP this_col = PROTECT(allocVector(LGLSXP, NN)); nprotect++;
-      int *this_v_ptr = LOGICAL(this_v);
+      int *restrict this_v_ptr = LOGICAL(this_v);
       int *this_col_ptr = LOGICAL(this_col);
       if (j == 0) {
+        #pragma omp parallel for num_threads(getDTthreads())
         for (int i = 0; i < NN; i++) {
           this_col_ptr[i] = this_v_ptr[i / div];
         }
       } else if (j == JJ - 1) {
+        #pragma omp parallel for num_threads(getDTthreads())
         for (int i = 0; i < NN; i++) {
           this_col_ptr[i] = this_v_ptr[i % modulo];
         }
       } else {
+        #pragma omp parallel for num_threads(getDTthreads())
         for (int i = 0; i < NN; i++) {
           this_col_ptr[i] = this_v_ptr[(i % modulo) / div];
         }
@@ -34,17 +38,20 @@ SEXP cj(SEXP base_list) {
       break;
     case INTSXP: {
       SEXP this_col = PROTECT(allocVector(INTSXP, NN)); nprotect++;
-      int *this_v_ptr = INTEGER(this_v);
+      int *restrict this_v_ptr = INTEGER(this_v);
       int *this_col_ptr = INTEGER(this_col);
       if (j == 0) {
+        #pragma omp parallel for num_threads(getDTthreads())
         for (int i = 0; i < NN; i++) {
           this_col_ptr[i] = this_v_ptr[i / div];
         }
       } else if (j == JJ - 1) {
+        #pragma omp parallel for num_threads(getDTthreads())
         for (int i = 0; i < NN; i++) {
           this_col_ptr[i] = this_v_ptr[i % modulo];
         }
       } else {
+        #pragma omp parallel for num_threads(getDTthreads())
         for (int i = 0; i < NN; i++) {
           this_col_ptr[i] = this_v_ptr[(i % modulo) / div];
         }
@@ -54,17 +61,20 @@ SEXP cj(SEXP base_list) {
       break;
     case REALSXP: {
       SEXP this_col = PROTECT(allocVector(REALSXP, NN)); nprotect++;
-      double *this_v_ptr = REAL(this_v);
+      double *restrict this_v_ptr = REAL(this_v);
       double *this_col_ptr = REAL(this_col);
       if (j == 0) {
+        #pragma omp parallel for num_threads(getDTthreads())
         for (int i = 0; i < NN; i++) {
           this_col_ptr[i] = this_v_ptr[i / div];
         }
       } else if (j == JJ - 1) {
+        #pragma omp parallel for num_threads(getDTthreads())
         for (int i = 0; i < NN; i++) {
           this_col_ptr[i] = this_v_ptr[i % modulo];
         }
       } else {
+        #pragma omp parallel for num_threads(getDTthreads())
         for (int i = 0; i < NN; i++) {
           this_col_ptr[i] = this_v_ptr[(i % modulo) / div];
         }
