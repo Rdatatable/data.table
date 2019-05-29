7cd97581c9ff70917f139da53b4ccf8eda3b0307
jangorecki <j.gorecki@wit.edu.pl>
2019-05-29 11:30:07 +0530

    resolving mmy feedback in PR

--- src/coalesce.c
+++ src/coalesce.c
@@ -1,51 +1,42 @@
 #include "data.table.h"
 
 SEXP coalesce(SEXP x, SEXP values) {
-  int JJ = LENGTH(values), nprotect = 0, NN = LENGTH(x);
-  
-  // pass over x in parallel to count NA
-  int na_count = 0;
-  switch(TYPEOF(x)) {
+  int JJ = LENGTH(values), protecti = 0;
+  uint64_t NN = LENGTH(x), na_count = 0;;
+
+  switch(TYPEOF(x)) { // pass over x in parallel to count NA
   case LGLSXP: {
     int *restrict x_ptr = LOGICAL(x);
     #pragma omp parallel for schedule(dynamic) reduction(+:na_count)
-    for (int i = 0; i < NN; i++) na_count += (int) (x_ptr[i] == NA_LOGICAL);
+    for (uint64_t i = 0; i < NN; i++) na_count += (int) (x_ptr[i] == NA_LOGICAL);
   } break;
   case INTSXP: {
     int *restrict x_ptr = INTEGER(x);
     #pragma omp parallel for schedule(dynamic) reduction(+:na_count)
-    for (int i = 0; i < NN; i++) na_count += (int) (x_ptr[i] == NA_INTEGER);
+    for (uint64_t i = 0; i < NN; i++) na_count += (int) (x_ptr[i] == NA_INTEGER);
   } break;
   case REALSXP: {
     double *restrict x_ptr = REAL(x);
     #pragma omp parallel for schedule(dynamic) reduction(+:na_count)
-    for (int i = 0; i < NN; i++) na_count += (int) (ISNA(x_ptr[i]));
+    for (uint64_t i = 0; i < NN; i++) na_count += (int) (ISNA(x_ptr[i]));
   } break;
   case STRSXP: {
-    for (int i = 0; i < NN; i++) na_count += (int) (STRING_ELT(x, i) == NA_STRING);
+    for (uint64_t i = 0; i < NN; i++) na_count += (int) (STRING_ELT(x, i) == NA_STRING);
   } break;
   default: error("Incompatible type");
   }
-  
+
   switch(TYPEOF(x)) {
   case LGLSXP: {
     int *x_ptr = LOGICAL(x);
-    
-    SEXP out = PROTECT(allocVector(LGLSXP, NN)); nprotect++;
+    SEXP out = PROTECT(allocVector(LGLSXP, NN)); protecti++;
     int *out_ptr = LOGICAL(out);
-    
-    for (int i = 0; i < NN; i++) {
+    for (uint64_t i = 0; i < NN; i++) {
       if (na_count > 0 && x_ptr[i] == NA_LOGICAL) {
         int j = 0;
         while (j < JJ) {
           SEXP this_j = VECTOR_ELT(values, j);
-          int take_idx;
-          if (LENGTH(this_j) == 1) {
-            take_idx = 0;
-          } else {
-            take_idx = i;
-          }
-          int this_replacement = LOGICAL(this_j)[take_idx];
+          int this_replacement = LOGICAL(this_j)[LENGTH(this_j)==1 ? 0 : i];
           if (this_replacement != NA_LOGICAL) {
             out_ptr[i] = this_replacement;
             na_count--;
@@ -56,27 +47,19 @@ SEXP coalesce(SEXP x, SEXP values) {
         if (j == JJ) out_ptr[i] = NA_LOGICAL;
       } else out_ptr[i] = x_ptr[i];
     }
-    UNPROTECT(nprotect);
+    UNPROTECT(protecti);
     return(out);
   }
   case INTSXP: {
     int *x_ptr = INTEGER(x);
-    
-    SEXP out = PROTECT(allocVector(INTSXP, NN)); nprotect++;
+    SEXP out = PROTECT(allocVector(INTSXP, NN)); protecti++;
     int *out_ptr = INTEGER(out);
-    
-    for (int i = 0; i < NN; i++) {
+    for (uint64_t i = 0; i < NN; i++) {
       if (na_count > 0 && x_ptr[i] == NA_INTEGER) {
         int j = 0;
         while (j < JJ) {
           SEXP this_j = VECTOR_ELT(values, j);
-          int take_idx;
-          if (LENGTH(this_j) == 1) {
-            take_idx = 0;
-          } else {
-            take_idx = i;
-          }
-          int this_replacement = INTEGER(this_j)[take_idx];
+          int this_replacement = INTEGER(this_j)[LENGTH(this_j)==1 ? 0 : i];
           if (this_replacement != NA_INTEGER) {
             out_ptr[i] = this_replacement;
             na_count--;
@@ -87,27 +70,19 @@ SEXP coalesce(SEXP x, SEXP values) {
         if (j == JJ) out_ptr[i] = NA_INTEGER;
       } else out_ptr[i] = x_ptr[i];
     }
-    UNPROTECT(nprotect);
+    UNPROTECT(protecti);
     return(out);
   }
   case REALSXP: {
     double *x_ptr = REAL(x);
-    
-    SEXP out = PROTECT(allocVector(REALSXP, NN)); nprotect++;
+    SEXP out = PROTECT(allocVector(REALSXP, NN)); protecti++;
     double *out_ptr = REAL(out);
-    
-    for (int i = 0; i < NN; i++) {
+    for (uint64_t i = 0; i < NN; i++) {
       if (na_count > 0 && ISNA(x_ptr[i])) {
         int j = 0;
         while (j < JJ) {
           SEXP this_j = VECTOR_ELT(values, j);
-          int take_idx;
-          if (LENGTH(this_j) == 1) {
-            take_idx = 0;
-          } else {
-            take_idx = i;
-          }
-          double this_replacement = REAL(this_j)[take_idx];
+          double this_replacement = REAL(this_j)[LENGTH(this_j)==1 ? 0 : i];
           if (!ISNA(this_replacement)) {
             out_ptr[i] = this_replacement;
             na_count--;
@@ -118,24 +93,17 @@ SEXP coalesce(SEXP x, SEXP values) {
         if (j == JJ) out_ptr[i] = NA_REAL;
       } else out_ptr[i] = x_ptr[i];
     }
-    UNPROTECT(nprotect);
+    UNPROTECT(protecti);
     return(out);
   }
   case STRSXP: {
-    SEXP out = PROTECT(allocVector(STRSXP, NN)); nprotect++;
-    
-    for (int i = 0; i < NN; i++) {
+    SEXP out = PROTECT(allocVector(STRSXP, NN)); protecti++;
+    for (uint64_t i = 0; i < NN; i++) {
       if (na_count > 0 && STRING_ELT(x, i) == NA_STRING) {
         int j = 0;
         while (j < JJ) {
           SEXP this_j = VECTOR_ELT(values, j);
-          int take_idx;
-          if (LENGTH(this_j) == 1) {
-            take_idx = 0;
-          } else {
-            take_idx = i;
-          }
-          SEXP this_replacement = STRING_ELT(this_j, take_idx);
+          SEXP this_replacement = STRING_ELT(this_j, LENGTH(this_j)==1 ? 0 : i);
           if (this_replacement != NA_STRING) {
             SET_STRING_ELT(out, i, this_replacement);
             na_count--;
@@ -146,7 +114,7 @@ SEXP coalesce(SEXP x, SEXP values) {
         if (j == JJ) SET_STRING_ELT(out, i, NA_STRING);
       } else SET_STRING_ELT(out, i, STRING_ELT(x, i));
     }
-    UNPROTECT(nprotect);
+    UNPROTECT(protecti);
     return(out);
   }
   default:
