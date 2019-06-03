55c6be2197fd154bc29e4e965ff86451be1b4b10
jangorecki <j.gorecki@wit.edu.pl>
2019-06-03 10:54:31 +0530

    early escape for  no input

--- R/coalesce.R
+++ R/coalesce.R
@@ -1,11 +1,13 @@
 coalesce = function(x, ..., .dots=NULL) {
-  if (is.null(.dots) && missing(..1)) return(x)
+  if (is.null(.dots) && missing(..1)) return(copy(x))
 
   values = if (!is.null(.dots)) {
     if (!missing(..1)) stop("provide either '...' or '.dots' argument, not both")
     if (!is.list(.dots)) stop("argument '.dots' requires a list input")
     .dots
   } else list(...)
+
+  if (!length(values)) return(copy(x))
 
   .Call(Ccoalesce, x, values, FALSE)
 }
@@ -17,6 +19,8 @@ setcoalesce = function(x, ..., .dots=NULL) {
     if (!is.list(.dots)) stop("argument '.dots' requires a list input")
     .dots
   } else list(...)
+
+  if (!length(values)) return(x)
 
   invisible(.Call(Ccoalesce, x, values, TRUE))
 }
