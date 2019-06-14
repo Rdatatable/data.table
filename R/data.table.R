47560887838a44beafad1eb52eeb747eb9d50e7a
Matt Dowle <mattjdowle@gmail.com>
2019-06-13 21:48:28 -0700

    is.list returns true for data.frame|table too

--- R/data.table.R
+++ R/data.table.R
@@ -2675,7 +2675,7 @@ chgroup = function(x) {
 
 rbindlist = function(l, use.names="check", fill=FALSE, idcol=NULL) {
   if (is.null(l)) return(null.data.table())
-  if (!is.list(l)) stop("Input is not a list")
+  if (class(l)[1L]!="list") stop("Input is ", class(l)[1L]," but should be a plain list of items to be stacked")
   if (isFALSE(idcol)) { idcol = NULL }
   else if (!is.null(idcol)) {
     if (isTRUE(idcol)) idcol = ".id"
