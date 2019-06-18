0acc6364aaaf3d985c8569cfc1db0efccfa2406a
mattdowle <mattjdowle@gmail.com>
2019-06-18 02:11:17 -0700

    news item benchmark and removed .dots

--- R/coalesce.R
+++ R/coalesce.R
@@ -1,28 +1,29 @@
-coalesce = function(x, ..., .dots=NULL) {
-  if (is.null(.dots) && missing(..1)) return(copy(x))
 
-  values = if (!is.null(.dots)) {
-    if (!missing(..1)) stop("provide either '...' or '.dots' argument, not both")
-    if (!is.list(.dots)) stop("argument '.dots' requires a list input")
-    .dots
-  } else list(...)
-
-  if (!length(values)) return(copy(x))
-
+coalesce = function(x, ...) {
+  if (missing(..1)) {
+    if (is.list(x)) {
+      if (length(x)<=1L) return(x[[1L]])
+      values = x[-1L]
+      x = x[[1L]]
+    } else return(x)
+  } else {
+    if (!missing(..2)) values = list(...)
+    else values = if (is.list(..1)) ..1 else list(..1)
+  }
   .Call(Ccoalesce, x, values, FALSE)
 }
 
-setcoalesce = function(x, ..., .dots=NULL) {
-  if (is.null(.dots) && missing(..1)) return(x)
-
-  values = if (!is.null(.dots)) {
-    if (!missing(..1)) stop("provide either '...' or '.dots' argument, not both")
-    if (!is.list(.dots)) stop("argument '.dots' requires a list input")
-    .dots
-  } else list(...)
-
-  if (!length(values)) return(x)
-
+setcoalesce = function(x, ...) {
+  if (missing(..1)) {
+    if (is.list(x)) {
+      if (length(x)<=1L) return(x[[1L]])
+      values = x[-1L]
+      x = x[[1L]]
+    } else return(x)
+  } else {
+    if (!missing(..2)) values = list(...)
+    else values = if (is.list(..1)) ..1 else list(..1)
+  }
   invisible(.Call(Ccoalesce, x, values, TRUE))
 }
 
