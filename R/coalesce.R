1f0bcc7c6262b2a364a163e6fb48dbfe30873205
mattdowle <mattjdowle@gmail.com>
2019-07-09 19:47:20 -0700

    Moved argument massage down to C to simplify R level and avoid any chance of copies

--- R/coalesce.R
+++ R/coalesce.R
@@ -1,27 +1,4 @@
-coalesce = function(x, ...) {
-  if (missing(..1)) {
-    if (is.list(x)) {
-      if (length(x)<=1L) return(x[[1L]])
-      values = if (is.data.table(x)) x[,-1L] else x[-1L]
-      x = x[[1L]]
-    } else return(x)
-  } else {
-    if (!missing(..2)) values = list(...)
-    else values = if (is.list(..1)) ..1 else list(..1)
-  }
-  .Call(Ccoalesce, x, values, FALSE)
-}
 
-setcoalesce = function(x, ...) {
-  if (missing(..1)) {
-    if (is.list(x)) {
-      if (length(x)<=1L) return(x[[1L]])
-      values = if (is.data.table(x)) x[,-1L] else x[-1L]
-      x = x[[1L]]
-    } else return(x)
-  } else {
-    if (!missing(..2)) values = list(...)
-    else values = if (is.list(..1)) ..1 else list(..1)
-  }
-  invisible(.Call(Ccoalesce, x, values, TRUE))
-}
+coalesce    = function(...) .Call(Ccoalesce, list(...), FALSE)
+setcoalesce = function(...) .Call(Ccoalesce, list(...), TRUE)
+
