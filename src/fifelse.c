5cae5c4d00a05f59773b770530b8500498e6aacb
mattdowle <mattjdowle@gmail.com>
2019-08-01 16:31:01 -0700

    499 lines of C reduced to 131; e.g. simplified length checks, removed adj cases, copyMostAttrib does everything the last part was doing. Reduced code (saved repeated logic) better than saving ternaries. Use of const and &mask gives compiler and branch prediction good chance. Need to demonstrate performance improvement if branches are to be put back; i.e. make a case for many branches (which I think will be hard in this example of ifelse).

--- src/fifelse.c
+++ src/fifelse.c
@@ -5,34 +5,30 @@ SEXP fifelseR(SEXP l, SEXP a, SEXP b)
   // l is the test
   // a is what to do in case l is TRUE
   // b is what to do in case l is FALSE
-  // maybe add something in case l is neither TRUE or FALSE ? for now it will default to NA
 
-  // Check if test is logical
-  if(!isLogical(l)) error("Argument 'test' must be logical.");
+  if (!isLogical(l)) error("Argument 'test' must be logical.");
 
   const int64_t len0 = xlength(l);
   const int64_t len1 = xlength(a);
   const int64_t len2 = xlength(b);
-
   SEXPTYPE ta = TYPEOF(a);
   SEXPTYPE tb = TYPEOF(b);
-
   int nprotect = 0;
 
-  // Check if same type and do en-listing of singleton
-  if(ta != tb)
+  // Check if same storage type and do en-listing of singleton
+  if (ta != tb)
   {
-    if(ta == VECSXP && (tb == INTSXP || tb == REALSXP || tb == LGLSXP || tb == CPLXSXP || tb == STRSXP))
+    if (ta == VECSXP && (tb == INTSXP || tb == REALSXP || tb == LGLSXP || tb == CPLXSXP || tb == STRSXP))
     {
-      if(len2 == 1)
+      if (len2 == 1)
       {
         SEXP tmp = PROTECT(allocVector(VECSXP,1)); nprotect++;
         SET_VECTOR_ELT(tmp, 0, b);
         b = tmp;
         tb = VECSXP;
       }
-    } else if(tb == VECSXP && (ta == INTSXP || ta == REALSXP || ta == LGLSXP || ta == CPLXSXP || ta == STRSXP)){
-      if(len1 == 1)
+    } else if (tb == VECSXP && (ta == INTSXP || ta == REALSXP || ta == LGLSXP || ta == CPLXSXP || ta == STRSXP)){
+      if (len1 == 1)
       {
         SEXP tmp = PROTECT(allocVector(VECSXP,1)); nprotect++;
         SET_VECTOR_ELT(tmp, 0, a);
@@ -44,444 +40,93 @@ SEXP fifelseR(SEXP l, SEXP a, SEXP b)
     }
   }
 
-  SEXP class_type = PROTECT(getAttrib(a,R_ClassSymbol)); nprotect++;
-
-  // Check if same class
-  if(!R_compute_identical(class_type,getAttrib(b,R_ClassSymbol),0))
+  if (!R_compute_identical(PROTECT(getAttrib(a,R_ClassSymbol)), PROTECT(getAttrib(b,R_ClassSymbol)), 0))
     error("'yes' has different class than 'no'. Please make sure that candidate replacements have the same class.");
+  UNPROTECT(2);
 
-  const int atest = isFactor(a);
-  SEXP alevels = R_NilValue, blevels = R_NilValue, result = R_NilValue;
-
-  // Check if factor
-  if(atest)
-  {
-    alevels = PROTECT(getAttrib(a,R_LevelsSymbol)); nprotect++;
-    blevels = PROTECT(getAttrib(b,R_LevelsSymbol)); nprotect++;
+  if (isFactor(a)) {
+    if (!R_compute_identical(PROTECT(getAttrib(a,R_LevelsSymbol)), PROTECT(getAttrib(b,R_LevelsSymbol)), 0))
+      error("'yes' and 'no' are both type factor but their levels are different.");
+    UNPROTECT(2);
   }
-
-  /*
-   Variable 'adj' is used to seperate case where l (test),
-   a (yes) and b (no) are of different length.
-   */
-  unsigned int adj = 0;
 
   // Check here the length of the different input variables.
-  if(len1 != len2)
-  {
-    if(len1 < len2)
-    {
-      if(len1 != 1)    error("Length of 'yes' must be 1 or equal to length of 'test' (%d).", len0);
-      if(len2 != len0) error("Length of 'no' must be 1 or equal to length of 'test' (%d).", len0);
-      adj = 1;
-    } else {
-      if(len2 != 1)    error("Length of 'no' must be 1 or equal to length of 'test' (%d).", len0);
-      if(len1 != len0) error("Length of 'yes' must be 1 or equal to length of 'test' (%d).", len0);
-      adj = 2;
-    }
-  } else {
-    if(len0 != len1)
-    {
-      if(len1 != 1) error("Length of 'yes' and 'no' must be 1 or equal to length of 'test' (%d).", len0);
-      adj = 3;
-    }
-  }
+  if (len1!=1 && len1!=len0) error("Length of 'yes' is %d but must be 1 or length of 'test' (%d).", len1, len0);
+  if (len2!=1 && len2!=len0) error("Length of 'no' is %d but must be 1 or length of 'test' (%d).", len2, len0);
+  const int64_t amask = len1>1 ? INT64_MAX : 0;
+  const int64_t bmask = len2>1 ? INT64_MAX : 0;
 
   int *pl = LOGICAL(l);
-  switch(ta)
-  {
-    /*
-     This part is for dealing with a (yes) and b (no)
-     in case they are both of type INTSXP (integer)
-     */
-  case INTSXP :
-    result = PROTECT(allocVector(INTSXP, len0)); nprotect++;
-    int *int_pres = INTEGER(result);
-    int *int_pa   = INTEGER(a);
-    int *int_pb   = INTEGER(b);
-    switch(adj)
-    {
-    case 0:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : int_pres[i] = int_pb[i];  break;
-          case 1  : int_pres[i] = int_pa[i];  break;
-          default : int_pres[i] = NA_INTEGER; break;
-        }
-      }
-      break;
+  SEXP ans = PROTECT(allocVector(TYPEOF(a), len0)); nprotect++;
+  copyMostAttrib(a, ans);
 
-    case 1:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : int_pres[i] = int_pb[i];  break;
-          case 1  : int_pres[i] = int_pa[0];  break;
-          default : int_pres[i] = NA_INTEGER; break;
-        }
-      }
-      break;
-
-    case 2:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : int_pres[i] = int_pb[0];  break;
-          case 1  : int_pres[i] = int_pa[i];  break;
-          default : int_pres[i] = NA_INTEGER; break;
-        }
-      }
-      break;
-
-    case 3:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : int_pres[i] = int_pb[0];  break;
-          case 1  : int_pres[i] = int_pa[0];  break;
-          default : int_pres[i] = NA_INTEGER; break;
-        }
-      }
-      break;
+  switch(TYPEOF(a)) {
+  case LGLSXP: {
+    int *pans = LOGICAL(ans);
+    int *pa   = LOGICAL(a);
+    int *pb   = LOGICAL(b);
+    for (int64_t i=0; i<len0; ++i) {
+      pans[i] = pl[i]==0 ? pb[i & bmask] : (pl[i]==1 ? pa[i & amask] : NA_LOGICAL);
     }
-    break;
-
-    /*
-     This part is for dealing with a (yes) and b (no)
-     in case they are both of type REALSXP (double)
-     */
-  case REALSXP :
-    result = PROTECT(allocVector(REALSXP, len0)); nprotect++;
-    double *double_pres = REAL(result);
-    double *double_pa   = REAL(a);
-    double *double_pb   = REAL(b);
-    switch(adj)
-    {
-    case 0:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : double_pres[i] = double_pb[i];  break;
-          case 1  : double_pres[i] = double_pa[i];  break;
-          default : double_pres[i] = NA_REAL; break;
-        }
-      }
-      break;
-
-    case 1:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : double_pres[i] = double_pb[i];  break;
-          case 1  : double_pres[i] = double_pa[0];  break;
-          default : double_pres[i] = NA_REAL; break;
-        }
-      }
-      break;
-
-    case 2:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : double_pres[i] = double_pb[0];  break;
-          case 1  : double_pres[i] = double_pa[i];  break;
-          default : double_pres[i] = NA_REAL; break;
-        }
-      }
-      break;
-
-    case 3:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : double_pres[i] = double_pb[0];  break;
-          case 1  : double_pres[i] = double_pa[0];  break;
-          default : double_pres[i] = NA_REAL; break;
-        }
-      }
-      break;
+  } break;
+  case INTSXP: {
+    int *pans = INTEGER(ans);
+    int *pa   = INTEGER(a);
+    int *pb   = INTEGER(b);
+    for (int64_t i=0; i<len0; ++i) {
+      pans[i] = pl[i]==0 ? pb[i & bmask] : (pl[i]==1 ? pa[i & amask] : NA_INTEGER);
     }
-    break;
-
-    /*
-     This part is for dealing with a (yes) and b (no)
-     in case they are both of type LGLSXP (logical)
-     */
-  case LGLSXP :
-    result = PROTECT(allocVector(LGLSXP, len0)); nprotect++;
-    int *lg_pres = LOGICAL(result);
-    int *lg_pa   = LOGICAL(a);
-    int *lg_pb   = LOGICAL(b);
-    switch(adj)
-    {
-    case 0:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : lg_pres[i] = lg_pb[i];  break;
-          case 1  : lg_pres[i] = lg_pa[i];  break;
-          default : lg_pres[i] = NA_LOGICAL; break;
-        }
-      }
-      break;
-
-    case 1:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : lg_pres[i] = lg_pb[i];  break;
-          case 1  : lg_pres[i] = lg_pa[0];  break;
-          default : lg_pres[i] = NA_LOGICAL; break;
-        }
-      }
-      break;
-
-    case 2:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : lg_pres[i] = lg_pb[0];  break;
-          case 1  : lg_pres[i] = lg_pa[i];  break;
-          default : lg_pres[i] = NA_LOGICAL; break;
-        }
-      }
-      break;
-
-    case 3:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : lg_pres[i] = lg_pb[0];  break;
-          case 1  : lg_pres[i] = lg_pa[0];  break;
-          default : lg_pres[i] = NA_LOGICAL; break;
-        }
-      }
-      break;
+  } break;
+  case REALSXP: {
+    double *pans = REAL(ans);
+    double *pa   = REAL(a);
+    double *pb   = REAL(b);
+    for (int64_t i=0; i<len0; ++i) {
+      pans[i] = pl[i]==0 ? pb[i & bmask] : (pl[i]==1 ? pa[i & amask] : NA_REAL);
     }
-    break;
-
-    /*
-     This part is for dealing with a (yes) and b (no)
-     in case they are both of type STRSXP (character)
-     */
-  case STRSXP :
-    result = PROTECT(allocVector(STRSXP, len0)); nprotect++;
+  } break;
+  case STRSXP : {
     const SEXP *pa = STRING_PTR(a);
     const SEXP *pb = STRING_PTR(b);
-    switch(adj)
-    {
-    case 0:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : SET_STRING_ELT(result, i, pb[i]);  break;
-          case 1  : SET_STRING_ELT(result, i, pa[i]);  break;
-          default : SET_STRING_ELT(result, i, NA_STRING); break;
-        }
-      }
-      break;
-
-    case 1:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : SET_STRING_ELT(result, i, pb[i]);  break;
-          case 1  : SET_STRING_ELT(result, i, pa[0]);  break;
-          default : SET_STRING_ELT(result, i, NA_STRING); break;
-        }
-      }
-      break;
-
-    case 2:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : SET_STRING_ELT(result, i, pb[0]);  break;
-          case 1  : SET_STRING_ELT(result, i, pa[i]);  break;
-          default : SET_STRING_ELT(result, i, NA_STRING); break;
-        }
-      }
-      break;
-
-    case 3:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : SET_STRING_ELT(result, i, pb[0]);  break;
-          case 1  : SET_STRING_ELT(result, i, pa[0]);  break;
-          default : SET_STRING_ELT(result, i, NA_STRING); break;
-        }
-      }
-      break;
+    for (int64_t i=0; i<len0; ++i) {
+      SET_STRING_ELT(ans, i, pl[i]==0 ? pb[i & bmask] : (pl[i]==1 ? pa[i & amask] : NA_STRING));
     }
-    break;
-
-    /*
-     This part is for dealing with a (yes) and b (no)
-     in case they are both of type CPLXSXP (complex)
-     */
-  case CPLXSXP :
-    result = PROTECT(allocVector(CPLXSXP, len0)); nprotect++;
-    Rcomplex *cp_pres = COMPLEX(result);
-    Rcomplex *cp_pa   = COMPLEX(a);
-    Rcomplex *cp_pb   = COMPLEX(b);
-    switch(adj)
-    {
-    case 0:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : cp_pres[i] = cp_pb[i]; break;
-          case 1  : cp_pres[i] = cp_pa[i]; break;
-          default : cp_pres[i] = NA_CPLX;  break;
-        }
-      }
-      break;
-
-    case 1:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : cp_pres[i] = cp_pb[i]; break;
-          case 1  : cp_pres[i] = cp_pa[0]; break;
-          default : cp_pres[i] = NA_CPLX;  break;
-        }
-      }
-      break;
-
-    case 2:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : cp_pres[i] = cp_pb[0]; break;
-          case 1  : cp_pres[i] = cp_pa[i]; break;
-          default : cp_pres[i] = NA_CPLX;  break;
-        }
-      }
-      break;
-
-    case 3:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : cp_pres[i] = cp_pb[0]; break;
-          case 1  : cp_pres[i] = cp_pa[0]; break;
-          default : cp_pres[i] = NA_CPLX;  break;
-        }
-      }
-      break;
+  } break;
+  case CPLXSXP : {
+    Rcomplex *pans = COMPLEX(ans);
+    Rcomplex *pa   = COMPLEX(a);
+    Rcomplex *pb   = COMPLEX(b);
+    for (int64_t i=0; i<len0; ++i) {
+      pans[i] = pl[i]==0 ? pb[i & bmask] : (pl[i]==1 ? pa[i & amask] : NA_CPLX);
     }
-    break;
-
-    /*
-     This part is for dealing with a (yes) and b (no)
-     in case they are both of type VECSXP (list)
-     */
-  case VECSXP :
-    result = PROTECT(allocVector(VECSXP, len0)); nprotect++;
-    SEXP nms = PROTECT(allocVector(STRSXP, len0)); nprotect++; // create list names
-    // Get list names from a and b
-    SEXP nmsa = PROTECT(getAttrib(a,R_NamesSymbol)); nprotect++;
-    SEXP nmsb = PROTECT(getAttrib(b,R_NamesSymbol)); nprotect++;
-    SEXP na_list = PROTECT(allocVector(INTSXP, 1)); nprotect++;
-    INTEGER(na_list)[0] = NA_INTEGER;
-    const int testa = !isNull(nmsa);
-    const int testb = !isNull(nmsb);
-    switch(adj)
-    {
-    case 0:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : SET_VECTOR_ELT(result, i, VECTOR_ELT(b, i)); if(testb)SET_STRING_ELT(nms, i, STRING_ELT(nmsb,i)); break;
-          case 1  : SET_VECTOR_ELT(result, i, VECTOR_ELT(a, i)); if(testa)SET_STRING_ELT(nms, i, STRING_ELT(nmsa,i)); break;
-          default : SET_VECTOR_ELT(result, i, na_list); break;
-        }
+  } break;
+  case VECSXP : {
+    const SEXP *pa = VECTOR_PTR(a);
+    const SEXP *pb = VECTOR_PTR(b);
+    SEXP a_names = PROTECT(getAttrib(a,R_NamesSymbol)); nprotect++;
+    SEXP b_names = PROTECT(getAttrib(b,R_NamesSymbol)); nprotect++;
+    SEXP *pa_names = isNull(a_names) ? NULL : STRING_PTR(a_names);
+    SEXP *pb_names = isNull(b_names) ? NULL : STRING_PTR(b_names);
+    if (!pa_names && !pb_names) {
+      for (int64_t i=0; i<len0; ++i) {
+        if (pl[i]==NA_INTEGER) continue;  // allocVector already initialized with R_NilValue
+        SET_VECTOR_ELT(ans, i, pl[i]==0 ? pb[i & bmask] : pa[i & amask]);
       }
-      break;
-
-    case 1:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : SET_VECTOR_ELT(result, i, VECTOR_ELT(b, i)); if(testb)SET_STRING_ELT(nms, i, STRING_ELT(nmsb,i)); break;
-          case 1  : SET_VECTOR_ELT(result, i, VECTOR_ELT(a, 0)); if(testa)SET_STRING_ELT(nms, i, STRING_ELT(nmsa,0)); break;
-          default : SET_VECTOR_ELT(result, i, na_list); break;
-        }
-      }
-      break;
-
-    case 2:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : SET_VECTOR_ELT(result, i, VECTOR_ELT(b, 0)); if(testb)SET_STRING_ELT(nms, i, STRING_ELT(nmsb,0)); break; //some speed up can be done here
-          case 1  : SET_VECTOR_ELT(result, i, VECTOR_ELT(a, i)); if(testa)SET_STRING_ELT(nms, i, STRING_ELT(nmsa,i)); break;
-          default : SET_VECTOR_ELT(result, i, na_list); break;
-        }
-      }
-      break;
-
-    case 3:
-      for(int64_t i=0; i<len0; ++i)
-      {
-        switch(pl[i])
-        {
-          case 0  : SET_VECTOR_ELT(result, i, VECTOR_ELT(b, 0)); if(testb)SET_STRING_ELT(nms, i, STRING_ELT(nmsb,0)); break;
-          case 1  : SET_VECTOR_ELT(result, i, VECTOR_ELT(a, 0)); if(testa)SET_STRING_ELT(nms, i, STRING_ELT(nmsa,0)); break;
-          default : SET_VECTOR_ELT(result, i, na_list); break;
-        }
-      }
-      break;
-    }
-    if(testa || testb) setAttrib(result, R_NamesSymbol, nms);
-    break;
-
-  default: error("Type %s is not supported.",type2char(ta)); break;
-  }
-
-  // Check class type and adjust (including factors)
-  if(!isNull(class_type))
-  {
-    if(atest)
-    {
-      if(!R_ExternalPtrAddr(alevels))
-      {
-        setAttrib(result, R_LevelsSymbol, blevels);
-      } else {
-        setAttrib(result, R_LevelsSymbol, alevels);
-      }
-      setAttrib(result, R_ClassSymbol, ScalarString(char_factor));
     } else {
-      copyMostAttrib(a, result);
+      SEXP ans_names = PROTECT(allocVector(STRSXP, len0)); nprotect++;
+      setAttrib(ans, R_NamesSymbol, ans_names);
+      for (int64_t i=0; i<len0; ++i) {
+        if (pl[i]==NA_INTEGER) continue;  // allocVector already initialized with R_NilValue, and names already initialized with ""
+        SET_VECTOR_ELT(ans, i, pl[i]==0 ? pb[i & bmask] : pa[i & amask]);
+        if (pa_names && pl[i]==1) SET_STRING_ELT(ans_names, i, pa_names[i & amask]);
+        if (pb_names && pl[i]==0) SET_STRING_ELT(ans_names, i, pb_names[i & bmask]);
+      }
     }
+  } break;
+  default:
+    error("Type %s is not supported.",type2char(TYPEOF(a)));
   }
-
   UNPROTECT(nprotect);
-  return result;
+  return ans;
 }
 
