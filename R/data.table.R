ca41b6be3856972a95ed9562cf8c081ac439b59e
Michael Chirico <michael.chirico@grabtaxi.com>
2019-08-18 22:59:36 +0800

    Closes #2109 -- restrict negation in !with to unary -/!

--- R/data.table.R
+++ R/data.table.R
@@ -657,7 +657,8 @@ replace_order = function(isub, verbose, env) {
 
     if (!with) {
       # missingby was already checked above before dealing with i
-      if (is.call(jsub) && deparse(jsub[[1L]], 500L, backtick=FALSE) %chin% c("!", "-")) {  # TODO is deparse avoidable here?
+      # 2109 we should only match unary -
+      if (is.call(jsub) && deparse(jsub[[1L]], 500L, backtick=FALSE) %chin% c("!", "-") && length(jsub) == 2L) {  # TODO is deparse avoidable here?
         notj = TRUE
         jsub = jsub[[2L]]
       } else notj = FALSE
