69522a58f2d99f021ded957352c5658dcee0ae37
mattdowle <mattjdowle@gmail.com>
2019-09-05 13:53:24 -0700

    renamed coalesce to fcoalesce

--- R/wrappers.R
+++ R/wrappers.R
@@ -2,7 +2,7 @@
 # Very small (e.g. one line) R functions that just call C.
 # One file wrappers.R to avoid creating lots of small .R files.
 
-coalesce    = function(...) .Call(Ccoalesce, list(...), FALSE)
+fcoalesce   = function(...) .Call(Ccoalesce, list(...), FALSE)
 setcoalesce = function(...) .Call(Ccoalesce, list(...), TRUE)
 
 fifelse = function(test, yes, no, na=NA) .Call(CfifelseR, test, yes, no, na)
