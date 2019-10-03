946d204c407a94ff109294a77e0a84f48149d823
Michael Chirico <michael.chirico@grabtaxi.com>
2019-10-04 00:51:45 +0800

    coerceFill puts Inf to NA_INTEGER for integer

--- src/utils.c
+++ src/utils.c
@@ -160,7 +160,7 @@ void coerceFill(SEXP fill, double *dfill, int32_t *ifill, int64_t *i64fill) {
         // NA -> NA, NaN -> NaN
         ifill[0] = NA_INTEGER; dfill[0] = rfill; i64fill[0] = NA_INTEGER64;
       } else {
-        ifill[0] = rfill>INT32_MAX ? NA_INTEGER : (int32_t)(rfill);
+        ifill[0] = rfill>INT32_MAX || !R_finite(rfill) ? NA_INTEGER : (int32_t)(rfill);
         dfill[0] = rfill;
         i64fill[0] = rfill>INT64_MAX ? NA_INTEGER64 : (int64_t)(rfill);
       }
