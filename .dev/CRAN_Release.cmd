f8556cbd99e09b0a6117300597dbe38ab03ee3bd
mattdowle <mattjdowle@gmail.com>
2019-10-16 21:41:29 +0000

    cran_release only: extra debian package to compile 2nd order revdep

--- .dev/CRAN_Release.cmd
+++ .dev/CRAN_Release.cmd
@@ -417,6 +417,7 @@ sudo apt-get -y install libssl-dev libsasl2-dev
 sudo apt-get -y install biber   # for ctsem
 sudo apt-get -y install libopenblas-dev  # for ivmte (+ local R build with default ./configure to pick up shared openblas)
 sudo apt-get -y install libhiredis-dev  # for redux used by nodbi
+sudo apt-get -y install libzmq3-dev   # for rzmq
 sudo R CMD javareconf
 # ENDIF
 
