8c5042ca9aa5c6217b460fa5e8bf01003c7be358
mattdowle <mattjdowle@gmail.com>
2019-10-18 16:46:27 -0700

    1.12.6 on CRAN. Bump to 1.12.7

--- src/init.c
+++ src/init.c
@@ -363,6 +363,6 @@ SEXP initLastUpdated(SEXP var) {
 
 SEXP dllVersion() {
   // .onLoad calls this and checks the same as packageVersion() to ensure no R/C version mismatch, #3056
-  return(ScalarString(mkChar("1.12.5")));
+  return(ScalarString(mkChar("1.12.7")));
 }
 
