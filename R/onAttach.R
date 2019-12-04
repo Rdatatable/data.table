781c4504aa245bad671cb70297669737afbe3593
jangorecki <j.gorecki@wit.edu.pl>
2019-12-04 11:21:58 +0530

    upgrade DT helpers works for custom library location

--- R/onAttach.R
+++ R/onAttach.R
@@ -29,10 +29,10 @@
   }
 }
 
-dcf.lib = function(pkg, field){
+dcf.lib = function(pkg, field, lib.loc){
   # get DESCRIPTION metadata field from local library
   stopifnot(is.character(pkg), is.character(field), length(pkg)==1L, length(field)==1L)
-  dcf = system.file("DESCRIPTION", package=pkg)
+  dcf = system.file("DESCRIPTION", package=pkg, lib.loc=lib.loc)
   if (nzchar(dcf)) read.dcf(dcf, fields=field)[1L] else NA_character_
 }
 
@@ -46,21 +46,22 @@ dcf.repo = function(pkg, repo, field, type){
   dcf[dcf[,"Package"]==pkg, field][[1L]]
 }
 
-update.dev.pkg = function(object="data.table", repo="https://Rdatatable.gitlab.io/data.table", field="Revision", type=getOption("pkgType"), ...){
+update.dev.pkg = function(object="data.table", repo="https://Rdatatable.gitlab.io/data.table", field="Revision", type=getOption("pkgType"), lib=NULL, ...){
   pkg = object
   # perform package upgrade when new Revision present
   stopifnot(is.character(pkg), length(pkg)==1L, !is.na(pkg),
             is.character(repo), length(repo)==1L, !is.na(repo),
-            is.character(field), length(field)==1L, !is.na(field))
+            is.character(field), length(field)==1L, !is.na(field),
+            is.null(lib) || (is.character(lib) && length(lib)==1L && !is.na(lib)))
   una = is.na(ups<-dcf.repo(pkg, repo, field, type))
-  upg = una | !identical(ups, dcf.lib(pkg, field))
-  if (upg) utils::install.packages(pkg, repos=repo, type=type, ...)
+  upg = una | !identical(ups, dcf.lib(pkg, field, lib.loc=lib))
+  if (upg) utils::install.packages(pkg, repos=repo, type=type, lib=lib, ...)
   if (una) cat(sprintf("No commit information found in DESCRIPTION file for %s package. Unsure '%s' is correct field name in PACKAGES file in your devel repository '%s'.\n", pkg, field, file.path(repo, "src","contrib","PACKAGES")))
   cat(sprintf("R %s package %s %s (%s)\n",
               pkg,
               c("is up-to-date at","has been updated to")[upg+1L],
-              dcf.lib(pkg, field),
-              utils::packageVersion(pkg)))
+              dcf.lib(pkg, field, lib.loc=lib),
+              utils::packageVersion(pkg, lib.loc=lib)))
 }
 
 # non-exported utility when using devel version #3272: data.table:::.git()
