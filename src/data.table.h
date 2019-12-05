eba8704457fac31582a973a0624260b2043145b2
Matt Dowle <mattjdowle@gmail.com>
2019-12-04 17:47:47 -0800

    mingw I64 warnings (#4073)

--- src/data.table.h
+++ src/data.table.h
@@ -1,10 +1,10 @@
+#include "dt_stdio.h"  // PRId64 and PRIu64
 #include <R.h>
 #define USE_RINTERNALS
 #include <Rinternals.h>
 // #include <signal.h> // the debugging machinery + breakpoint aidee
 // raise(SIGINT);
 #include <stdint.h>    // for uint64_t rather than unsigned long long
-#include <inttypes.h>  // for PRId64 and PRIu64
 #include <stdbool.h>
 #include "myomp.h"
 #include "types.h"
