ee4e6af5a16ab4da7ad73343234ac689b6cc084b
Scott Ritchie <sritchie73@gmail.com>
2019-12-30 18:37:04 +0000

    Added long vector support
    Matrices with more the 2^32-1 elements should now be creatable using as.matrix.data.table. Code should also be forwards compatible with any future changes to data.table that enable columns with > 2^31-1 elements.

--- src/asmatrix.c
+++ src/asmatrix.c
@@ -6,17 +6,17 @@
  * corresponding asmatrix_<type> function.
  */
 
-SEXP asmatrix_logical(SEXP dt, int matlen, int n, int rncolnum) {
+SEXP asmatrix_logical(SEXP dt, R_xlen_t matlen, R_xlen_t n, R_xlen_t rncolnum) {
   SEXP mat = PROTECT(allocVector(LGLSXP, matlen)); // output matrix
   int *pmat, *pcol; // pointers to casted R objects
-  int vecIdx = 0; // counter to track place in vector underlying matrix
+  R_xlen_t vecIdx = 0; // counter to track place in vector underlying matrix
 
   // Iterate through dt and copy into mat 
   pmat = LOGICAL(mat);
-  for (int jj = 0; jj < length(dt); jj++) {
+  for (R_xlen_t jj = 0; jj < xlength(dt); jj++) {
     if (jj == rncolnum) continue; // skip rownames. TODO: make sure this does slow down because of branch prediction
     pcol = LOGICAL(VECTOR_ELT(dt, jj));
-    for (int ii = 0; ii < n; ii++) {
+    for (R_xlen_t ii = 0; ii < n; ii++) {
       pmat[vecIdx] = pcol[ii];
       vecIdx++;
     }
@@ -26,17 +26,17 @@ SEXP asmatrix_logical(SEXP dt, int matlen, int n, int rncolnum) {
   return mat;
 }
 
-SEXP asmatrix_integer(SEXP dt, int matlen, int n, int rncolnum) {
+SEXP asmatrix_integer(SEXP dt, R_xlen_t matlen, R_xlen_t n, R_xlen_t rncolnum) {
   SEXP mat = PROTECT(allocVector(INTSXP, matlen)); // output matrix
   int *pmat, *pcol; // pointers to casted R objects
-  int vecIdx = 0; // counter to track place in vector underlying matrix
+  R_xlen_t vecIdx = 0; // counter to track place in vector underlying matrix
   
   // Iterate through dt and copy into mat 
   pmat = INTEGER(mat);
-  for (int jj = 0; jj < length(dt); jj++) {
+  for (R_xlen_t jj = 0; jj < xlength(dt); jj++) {
     if (jj == rncolnum) continue; // skip rownames. TODO: make sure this does slow down because of branch prediction
     pcol = INTEGER(VECTOR_ELT(dt, jj));
-    for (int ii = 0; ii < n; ii++) {
+    for (R_xlen_t ii = 0; ii < n; ii++) {
       pmat[vecIdx] = pcol[ii];
       vecIdx++;
     }
@@ -46,17 +46,17 @@ SEXP asmatrix_integer(SEXP dt, int matlen, int n, int rncolnum) {
   return mat;
 }
 
-SEXP asmatrix_numeric(SEXP dt, int matlen, int n, int rncolnum) {
+SEXP asmatrix_numeric(SEXP dt, R_xlen_t matlen, R_xlen_t n, R_xlen_t rncolnum) {
   SEXP mat = PROTECT(allocVector(REALSXP, matlen)); // output matrix
   double *pmat, *pcol; // pointers to casted R objects
-  int vecIdx = 0; // counter to track place in vector underlying matrix
+  R_xlen_t vecIdx = 0; // counter to track place in vector underlying matrix
   
   // Iterate through dt and copy into mat 
   pmat = REAL(mat);
-  for (int jj = 0; jj < length(dt); jj++) {
+  for (R_xlen_t jj = 0; jj < xlength(dt); jj++) {
     if (jj == rncolnum) continue; // skip rownames. TODO: make sure this does slow down because of branch prediction
     pcol = REAL(VECTOR_ELT(dt, jj));
-    for (int ii = 0; ii < n; ii++) {
+    for (R_xlen_t ii = 0; ii < n; ii++) {
       pmat[vecIdx] = pcol[ii];
       vecIdx++;
     }
@@ -66,17 +66,17 @@ SEXP asmatrix_numeric(SEXP dt, int matlen, int n, int rncolnum) {
   return mat;
 }
 
-SEXP asmatrix_complex(SEXP dt, int matlen, int n, int rncolnum) {
+SEXP asmatrix_complex(SEXP dt, R_xlen_t matlen, R_xlen_t n, R_xlen_t rncolnum) {
   SEXP mat = PROTECT(allocVector(CPLXSXP, matlen)); // output matrix
   Rcomplex *pmat, *pcol; // pointers to casted R objects
-  int vecIdx = 0; // counter to track place in vector underlying matrix
+  R_xlen_t vecIdx = 0; // counter to track place in vector underlying matrix
   
   // Iterate through dt and copy into mat 
   pmat = COMPLEX(mat);
-  for (int jj = 0; jj < length(dt); jj++) {
+  for (R_xlen_t jj = 0; jj < xlength(dt); jj++) {
     if (jj == rncolnum) continue; // skip rownames. TODO: make sure this does slow down because of branch prediction
     pcol = COMPLEX(VECTOR_ELT(dt, jj));
-    for (int ii = 0; ii < n; ii++) {
+    for (R_xlen_t ii = 0; ii < n; ii++) {
       pmat[vecIdx] = pcol[ii];
       vecIdx++;
     }
@@ -86,16 +86,16 @@ SEXP asmatrix_complex(SEXP dt, int matlen, int n, int rncolnum) {
   return mat;
 }
 
-SEXP asmatrix_character(SEXP dt, int matlen, int n, int rncolnum) {
+SEXP asmatrix_character(SEXP dt, R_xlen_t matlen, R_xlen_t n, R_xlen_t rncolnum) {
   SEXP mat = PROTECT(allocVector(STRSXP, matlen)); // output matrix
   SEXP pcol; // pointers to casted R objects
-  int vecIdx = 0; // counter to track place in vector underlying matrix
+  R_xlen_t vecIdx = 0; // counter to track place in vector underlying matrix
   
   // Iterate through dt and copy into mat 
-  for (int jj = 0; jj < length(dt); jj++) {
+  for (R_xlen_t jj = 0; jj < xlength(dt); jj++) {
     if (jj == rncolnum) continue; // skip rownames. TODO: make sure this does slow down because of branch prediction
     pcol = VECTOR_ELT(dt, jj);
-    for (int ii = 0; ii < n; ii++) {
+    for (R_xlen_t ii = 0; ii < n; ii++) {
       SET_STRING_ELT(mat, vecIdx, STRING_ELT(pcol, ii));
       vecIdx++;
     }
@@ -105,16 +105,16 @@ SEXP asmatrix_character(SEXP dt, int matlen, int n, int rncolnum) {
   return mat;
 }
 
-SEXP asmatrix_list(SEXP dt, int matlen, int n, int rncolnum) {
+SEXP asmatrix_list(SEXP dt, R_xlen_t matlen, R_xlen_t n, R_xlen_t rncolnum) {
   SEXP mat = PROTECT(allocVector(VECSXP, matlen)); // output matrix
   SEXP pcol; // pointers to casted R objects
-  int vecIdx = 0; // counter to track place in vector underlying matrix
+  R_xlen_t vecIdx = 0; // counter to track place in vector underlying matrix
   
   // Iterate through dt and copy into mat 
-  for (int jj = 0; jj < length(dt); jj++) {
+  for (R_xlen_t jj = 0; jj < xlength(dt); jj++) {
     if (jj == rncolnum) continue; // skip rownames. TODO: make sure this does slow down because of branch prediction
     pcol = VECTOR_ELT(dt, jj);
-    for (int ii = 0; ii < n; ii++) {
+    for (R_xlen_t ii = 0; ii < n; ii++) {
       SET_VECTOR_ELT(mat, vecIdx, VECTOR_ELT(pcol, ii));
       vecIdx++;
     }
@@ -127,15 +127,15 @@ SEXP asmatrix_list(SEXP dt, int matlen, int n, int rncolnum) {
 // Dispatch function for different atomic types
 SEXP asmatrix(SEXP dt, SEXP nrow, SEXP ncol, SEXP rownames) {
   // Determine the length of the matrix vector given its dimensions
-  int n = asInteger(nrow);
-  int p = asInteger(ncol);
-  int matlen = n * p;
+  R_xlen_t n = asInteger(nrow);
+  R_xlen_t p = asInteger(ncol);
+  R_xlen_t matlen = n * p;
   
   /* Determine the column number in which the rownames is stored 
    * to skip when filling in the matrix. If there are no rownames, this
    * number will be larger than the number of columns in the data.table.
    */
-  int rncolnum = asInteger(rownames) - 1; // -1 to convert from 1-based to 0-based indexing
+  R_xlen_t rncolnum = asInteger(rownames) - 1; // -1 to convert from 1-based to 0-based indexing
   
   /* Conversion to a common atomic type is handled in R. We detect the
    * atomic type from the first column; unless that column is the 
