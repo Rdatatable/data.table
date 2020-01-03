d4c2465fa90525d3e08b1352fd5628a928af3ace
Scott Ritchie <sritchie73@gmail.com>
2020-01-03 17:00:06 +0000

    Explicit integer L not needed when using `:`

--- R/data.table.R
+++ R/data.table.R
@@ -1887,7 +1887,7 @@ as.matrix.data.table = function(x, rownames=NULL, rownames.value=NULL, ...) {
   if (!is.null(rownames)) {
     # extract that column.
     rownames.value = x[[rownames]]
-    dm = dim(x) - 0L:1L
+    dm = dim(x) - 0:1
     cn = names(x)[-rownames]
   } else {
     dm = dim(x)
