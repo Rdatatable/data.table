2ab6122b537b87b750a437a5f5787d80e00b5c96
Scott Ritchie <sritchie73@gmail.com>
2020-01-29 00:20:15 +0000

    No need for memcpy64, bug was elsewhere
    size_t is architecture dependent, not bound at INT_MAX, so no need to chunk memcpy.

--- src/data.table.h
+++ src/data.table.h
@@ -160,7 +160,6 @@ SEXP dt_na(SEXP x, SEXP cols);
 
 // assign.c
 SEXP alloccol(SEXP dt, R_len_t n, Rboolean verbose);
-void *memcpy64(void *str1, const void *str2, uint64_t n); // memcpy but for long vectors
 const char *memrecycle(const SEXP target, const SEXP where, const int64_t r, const int64_t len, SEXP source, const int64_t sourceStart, const int64_t sourceLen, const int64_t coln, const char *colname);
 SEXP shallowwrapper(SEXP dt, SEXP cols);
 
