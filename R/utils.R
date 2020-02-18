a2f2833f219ebcd24d31ed5666925f68fa9fb8c5
Cole Miller <57992489+ColeMiller1@users.noreply.github.com>
2020-02-17 20:11:44 -0500

    col_helper replicates .SDcols functionality

--- R/utils.R
+++ R/utils.R
@@ -140,3 +140,107 @@ edit.data.table = function(name, ...) {
   setDT(NextMethod('edit', name))[]
 }
 # nocov end
+
+col_helper = function(x, colsub, origin_call = NULL) {
+  ## helper that could replace some common code used for by, .SDcols, j, duplicated, unique, dcast, 
+  ## or anything that looks for columns
+  
+  #' @param x required; a data.frame, data.table, tibble, or list.
+  #' @param colsub takes the same arguments as .SDcols such as
+  #' `patterns()`, `var1:var2`, a variable, or a vector of characters.
+  #' When TRUE, will return seq_len(length(x))
+  #' @param origin_call takes character vector of length 1. In the short-term
+  #' there will be different modes based on the call. In the long-term, it's just
+  #' to help the end-user what caused the error
+  #' @return Integer vector with an attribute of whether negation was in the call
+  
+  # see @jangorecki #4174 for how to get the vast majority of this into C
+  
+  x_len = length(x)
+  
+  if (length(colsub) == 1L && isTRUE(colsub)){
+    ansvals = seq_len(x_len)
+    attr(ansvals, "negate") = FALSE
+    return(ansvals)
+  }
+  
+  # FR #4979 - negative numeric and character indices for SDcols
+  # fix for #5190. colsub[[1L]] gave error when it's a symbol.
+  negate_cols = is.call(colsub) && deparse(colsub[[1L]], 500L, backtick=FALSE) %in% c("!", "-")
+  if (negate_cols) colsub = colsub[[2L]]
+  
+  if (is.call(colsub)){
+    # fix for #1216, make sure the parentheses are peeled from expr of the form (((1:4)))
+    while(colsub %iscall% "(") colsub = as.list(colsub)[[-1L]]
+    if (length(colsub) == 3L && colsub[[1L]] == ":") {
+      if (is.name(colsub[[2L]])){
+        # cols is of the format a:c
+        rnge = chmatch(c(as.character(colsub[[2L]]), as.character(colsub[[3L]])), names(x))
+        cols = rnge[1L]:rnge[2L]
+      } else {
+        # cols is of the format 1:3
+        cols = eval(colsub)
+      }
+    } else if (colsub[[1L]] == "patterns") {
+      # each pattern gives a new filter condition, intersect the end result
+      colsub = as.list(colsub)[-1L]
+      if (length(colsub) == 1L) { #e.g. patterns("only_one_pattern")
+        cols = grep(eval(colsub[[1L]], parent.frame(2L)), names(x))
+      } else { #e.g. patterns("one", "or", "more", "patterns") 
+        x_names = names(x)
+        cols = Reduce(intersect, lapply(colsub, function(col) grep(eval(col, parent.frame(2L)), x_names)))
+      }
+    } else {
+      cols = eval(colsub, parent.frame(2L))
+    }
+  } else {
+    # if colsub is also a name in the list, we prioritize it over anything in the environment
+    if (exists(as.character(colsub), where = x)) 
+      cols = as.character(colsub) 
+    else 
+      cols = eval(colsub, parent.frame(2L), parent.frame(2L))
+  }
+                                        
+  # allow filtering via function in .SDcols, #3950
+  if (is.function(cols)) {
+    cols = lapply(x, cols)
+    if (any(idx <- vapply_1i(cols, length) > 1L | vapply_1c(cols, typeof) != 'logical' | vapply_1b(cols, anyNA)))
+      stop("When .SDcols is a function, it is applied to each column; the output of this function must be a non-missing boolean scalar signalling inclusion/exclusion of the column. However, these conditions were not met for: ", brackify(names(x)[idx]))
+    cols = unlist(cols, use.names = FALSE)
+  }
+  
+  if (origin_call == "by") {
+    #check for weird comma delimited "V1,V2,V3" and give warning message
+    #in the by eval we'll check if by = list(...) otherwise we can come here.
+  }
+  
+  if (anyNA(cols))
+    stop(origin_call, " missing at the following indices: ", brackify(which(is.na(cols))))
+  
+  if (is.logical(cols)) {
+    if (length(cols) != x_len) {
+      ## TODO change to error in 2022
+      warning("When ", origin_call, " is a logical vector, each column should have a TRUE or FALSE entry. Vector is repeated. Warning will change to error in 2022.")
+      cols = rep(cols, length.out = x_len)
+    }
+    cols = which_(cols)
+  }
+                                        
+  if (is.numeric(cols)) {
+    cols = as.integer(cols)
+    if (length(cols) == 1L && cols < 0L && abs(cols) <= x_len) cols = setdiff(seq_len(x), abs(cols))
+    if (length(unique(sign(cols))) > 1L) stop(origin_call, " is numeric but has both +ve and -ve indices")
+    if (any(idx <- abs(cols)>x_len | abs(cols)<1L))
+      stop(".SDcols is numeric but out of bounds [1, ", x_len, "] at: ", brackify(which(idx)))
+    ansvals = if (negate_cols) setdiff(seq_len(x_len), cols) else cols
+  } else {
+    if (!is.character(cols)) stop(origin_call, " should be column numbers or names")
+    x_names = names(x)
+    if (!all(idx <- cols %in% x_names))
+      stop("Some items of ", origin_call, " are not column names: ", brackify(cols[!idx]))
+    ansvars = if (negate_cols) setdiff(x_names, cols) else cols
+    ansvals = chmatch(ansvars, x_names)
+  }
+  attr(ansvals, "negate") = negate_cols
+  return(ansvals)
+}
