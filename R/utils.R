36d7bf630493347014ded451840fa9d745ce46f6
Cole Miller <57992489+ColeMiller1@users.noreply.github.com>
2020-02-22 09:52:16 -0500

    update based on comments
    include gettextf
    change from origin_call to mode
    repeat logical vectors of length 1

--- R/utils.R
+++ R/utils.R
@@ -141,7 +141,7 @@ edit.data.table = function(name, ...) {
 }
 # nocov end
 
-col_helper = function(x, colsub, origin_call = NULL) {
+col_helper = function(x, colsub, mode = NA_character_) {
   ## helper that could replace some common code used for by, .SDcols, j, duplicated, unique, dcast, 
   ## or anything that looks for columns
   
@@ -149,95 +149,89 @@ col_helper = function(x, colsub, origin_call = NULL) {
   #' @param colsub takes the same arguments as .SDcols such as
   #' `patterns()`, `var1:var2`, a variable, or a vector of characters.
   #' When TRUE, will return seq_len(length(x))
-  #' @param origin_call takes character vector of length 1. In the short-term
+  #' @param mode takes character vector of length 1. In the short-term
   #' there will be different modes based on the call. In the long-term, it's just
   #' to help the end-user what caused the error
-  #' @return Integer vector with an attribute of whether negation was in the call
+  #' @return Integer vector with a boolean attribute, `"negate"` which is TRUE when
+  #' `colsub[[1L]]` is `-` or `!`.
   
   # see @jangorecki #4174 for how to get the vast majority of this into C
   
   x_len = length(x)
-  
-  if (length(colsub) == 1L && isTRUE(colsub)){
-    ansvals = seq_len(x_len)
-    attr(ansvals, "negate") = FALSE
-    return(ansvals)
-  }
+  origsub = colsub
   
   # FR #4979 - negative numeric and character indices for SDcols
   # fix for #5190. colsub[[1L]] gave error when it's a symbol.
-  negate_cols = is.call(colsub) && deparse(colsub[[1L]], 500L, backtick=FALSE) %in% c("!", "-")
+  negate_cols = is.call(colsub) && (colsub[[1L]] == "!" || (colsub[[1L]] == "-" && length(colsub) == 2L))
   if (negate_cols) colsub = colsub[[2L]]
-  
+
   if (is.call(colsub)){
     # fix for #1216, make sure the parentheses are peeled from expr of the form (((1:4)))
-    while(colsub %iscall% "(") colsub = as.list(colsub)[[-1L]]
+    if (colsub[[1L]] == "(") {
+      colsub = as.list(colsub)[[-1L]]
+      while(length(colsub) > 1L && colsub[[1L]] == "(") colsub = colsub[[-1L]]
+      # give users a second chance with negation for (-1)
+      if (length(colsub) == 2L && colsub[[1L]] == "-" && is.numeric(colsub[[2L]])) {
+        negate_cols = TRUE ##what about -(-1)???
+        colsub = colsub[[2L]]
+      }
+    }
     if (length(colsub) == 3L && colsub[[1L]] == ":") {
-      if (is.name(colsub[[2L]])){
+      if (is.name(colsub[[2L]]) && is.name(colsub[[3L]])){
         # cols is of the format a:c
         rnge = chmatch(c(as.character(colsub[[2L]]), as.character(colsub[[3L]])), names(x))
-        cols = rnge[1L]:rnge[2L]
+        if (anyNA(rnge)) stop(gettextf("Not all columns were found in the %s column select statement: [%s]", mode, deparse(origsub), domain = "R-data.table"))
+        cols = rnge[1L]:rnge[2L] 
+      } else if (is.character(colsub[[2L]]) || is.character(colsub[[3L]])){
+        stop(gettextf("When selecting a range of columns in the var_1:var_2 format, quotation marks should not be used. Instead of %s try %s:%s", deparse(origsub), deparse(as.name(colsub[[2L]])), deparse(as.name(colsub[[3L]])), domain = "R-data.table"))
       } else {
-        # cols is of the format 1:3
-        cols = eval(colsub)
+        # cols is of the format 1:3 or would also allow min(var):max(var)
+        cols = eval(colsub, parent.frame(2L), parent.frame(2L))
       }
-    } else if (colsub[[1L]] == "patterns") {
+    } else if (length(colsub) > 1 && colsub[[1L]] == "patterns") {
       # each pattern gives a new filter condition, intersect the end result
-      colsub = as.list(colsub)[-1L]
-      if (length(colsub) == 1L) { #e.g. patterns("only_one_pattern")
-        cols = grep(eval(colsub[[1L]], parent.frame(2L)), names(x))
-      } else { #e.g. patterns("one", "or", "more", "patterns") 
-        x_names = names(x)
-        cols = Reduce(intersect, lapply(colsub, function(col) grep(eval(col, parent.frame(2L)), x_names)))
-      }
+      x_names = names(x)
+      cols = Reduce(intersect, lapply(as.list(colsub)[-1L], function(col) grep(eval(col, parent.frame(2L)), x_names)))
     } else {
-      cols = eval(colsub, parent.frame(2L))
+      cols = eval(colsub, parent.frame(2L), parent.frame(2L))
     }
   } else {
     # if colsub is also a name in the list, we prioritize it over anything in the environment
-    if (exists(as.character(colsub), where = x)) 
-      cols = as.character(colsub) 
-    else 
+    if (!exists(cols <- as.character(colsub), where = x)) 
       cols = eval(colsub, parent.frame(2L), parent.frame(2L))
   }
-                                        
+  
   # allow filtering via function in .SDcols, #3950
   if (is.function(cols)) {
     cols = lapply(x, cols)
     if (any(idx <- vapply_1i(cols, length) > 1L | vapply_1c(cols, typeof) != 'logical' | vapply_1b(cols, anyNA)))
-      stop("When .SDcols is a function, it is applied to each column; the output of this function must be a non-missing boolean scalar signalling inclusion/exclusion of the column. However, these conditions were not met for: ", brackify(names(x)[idx]))
+      stop(gettextf("When %s is a function, it is applied to each column; the output of this function must be a non-missing boolean scalar signalling inclusion/exclusion of the column. However, these conditions were not met for: %s", mode, brackify(names(x)[idx]), domain = "R-data.table"))
     cols = unlist(cols, use.names = FALSE)
   }
   
-  if (origin_call == "by") {
-    #check for weird comma delimited "V1,V2,V3" and give warning message
-    #in the by eval we'll check if by = list(...) otherwise we can come here.
-  }
-  
   if (anyNA(cols))
-    stop(origin_call, " missing at the following indices: ", brackify(which(is.na(cols))))
+    stop(gettextf("%s missing at the following indices: %s", mode, brackify(which(is.na(cols)))))
   
   if (is.logical(cols)) {
-    if (length(cols) != x_len) {
-      ## TODO change to error in 2022
-      warning("When ", origin_call, " is a logical vector, each column should have a TRUE or FALSE entry. Vector will be repeated to length of data.table. Warning will change to error in year 2022.")
+    if ((col_len <- length(cols)) == 1L) {
       cols = rep(cols, length.out = x_len)
-    }
-    cols = which_(cols)
-  }
-                                        
-  if (is.numeric(cols)) {
+    } else if (col_len != x_len) {
+      ## TODO change to error in 2022
+      warning(gettextf("When %s is a logical vector, each column should have a TRUE or FALSE entry. The current logical vector of length %d will be repeated to length of data.table. Warning will change to error in the next verion.", mode, col_len, domain = "R-data.table"))
+      cols = rep(cols, length.out = x_len)
+    } 
+    ansvals = which_(cols, !negate_cols)
+  } else if (is.numeric(cols)) {
     cols = as.integer(cols)
-    if (length(cols) == 1L && cols < 0L && abs(cols) <= x_len) cols = setdiff(seq_len(x), abs(cols))
-    if (length(unique(sign(cols))) > 1L) stop(origin_call, " is numeric but has both +ve and -ve indices")
-    if (any(idx <- abs(cols)>x_len | abs(cols)<1L))
-      stop(".SDcols is numeric but out of bounds [1, ", x_len, "] at: ", brackify(which(idx)))
+    if (any(idx <- abs(cols)>x_len | cols == 0L))
+      stop(gettextf("%s is numeric but out of bounds [1, %d] at: %s", mode, x_len, brackify(which(idx)), domain = "R-data.table"))
+    if (length(unique(sign(cols))) > 1L) stop(gettextf("%s is numeric but has both +ve and -ve indices", mode, domain = "R-data.table"))
     ansvals = if (negate_cols) setdiff(seq_len(x_len), cols) else cols
   } else {
-    if (!is.character(cols)) stop(origin_call, " should be column numbers or names")
+    if (!is.character(cols)) stop(gettextf("%s should be column numbers or names", mode, domain = "R-data.table"))
     x_names = names(x)
-    if (!all(idx <- cols %in% x_names))
-      stop("Some items of ", origin_call, " are not column names: ", brackify(cols[!idx]))
+    if (!all(idx <- cols %chin% x_names))
+      stop(gettextf("Some items of %s are not column names: %s", mode, brackify(cols[!idx]), domain = "R-data.table"))
     ansvars = if (negate_cols) setdiff(x_names, cols) else cols
     ansvals = chmatch(ansvars, x_names)
   }
