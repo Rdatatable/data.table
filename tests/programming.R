a2b876b2e6729c1f89c0b7bf60da6d332ba1a9f5
jangorecki <j.gorecki@wit.edu.pl>
2020-03-14 12:44:06 +0530

    add example from #2655

--- tests/programming.R
+++ tests/programming.R
@@ -21,3 +21,16 @@ substitute2(.(), env=list(a="b", fun="const1", nm="int1"), char.as.name=TRUE)
 substitute2(.("TRUE" = 1L, "FALSE" = 2L, "1" = 3L, "2" = 4L),
             env=list("FALSE"="col2", "2"="col4"),
             char.as.name=TRUE)
+
+substitute2(
+  .(out_col_name = fun(in_col_name, fun_arg1=fun_arg1val)),
+  env = list(
+    in_col_name = "x",
+    fun = "sum",
+    fun_arg1 = "na.rm",
+    fun_arg1val = TRUE,
+    out_col_name = "sum_x"
+  ),
+  char.as.name = TRUE
+)
+#.(sum_x = sum(x, na.rm = TRUE))
