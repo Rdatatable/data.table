9736c0da1d3d84f1970211be571c90aff5abd636
Michael Chirico <michael.chirico@grabtaxi.com>
2020-05-15 01:49:14 +0800

    copy-paste to pprod

--- src/psum.c
+++ src/psum.c
@@ -127,6 +127,12 @@ SEXP pprod(SEXP x, SEXP narmArg) {
   if (!isLogical(narmArg) || LENGTH(narmArg)!=1 || LOGICAL(narmArg)[0]==NA_LOGICAL) error(_("na.rm must be TRUE or FALSE"));
 
   int J=LENGTH(x);
+  if (J == 0) {
+    error(_("Empty input"));
+  } else if (J == 1) {
+    // na.rm doesn't matter -- input --> output
+    return duplicate(VECTOR_ELT(x, 0));
+  }
   SEXPTYPE outtype = INTSXP;
   int n = -1;
   for (int j=0; j<J; j++) {
@@ -138,7 +144,7 @@ SEXP pprod(SEXP x, SEXP narmArg) {
       outtype = REALSXP;
       break;
     default:
-      error(_("Only numeric inputs are supported for psum"));
+      error(_("Only numeric inputs are supported for pprod"));
     }
     if (n >= 0) {
       if (n != LENGTH(xj)) {
@@ -165,14 +171,29 @@ SEXP pprod(SEXP x, SEXP narmArg) {
           } // else remain as NA
         }
       }
-    } else { // REALSXP; cut-and-paste except for NA
+    } else { // REALSXP; special handling depending on whether each input is int/numeric
       double *outp = REAL(out);
       for (int j=0; j<J; j++) {
-        double *xjp = REAL(VECTOR_ELT(x, j));
-        for (int i=0; i<n; i++) {
-          if (!ISNAN(xjp[i])) {
-            outp[i] = ISNAN(outp[i]) ? xjp[i] : outp[i] * xjp[i];
+        SEXP xj = VECTOR_ELT(x, j);
+        switch (TYPEOF(xj)) {
+        case INTSXP: {
+          int *xjp = INTEGER(xj);
+          for (int i=0; i<n; i++) {
+            if (xjp[i] != NA_INTEGER) {
+              outp[i] = ISNAN(outp[i]) ? (double)xjp[i] : outp[i] * (double)xjp[i];
+            }
           }
+        } break;
+        case REALSXP: {
+          double *xjp = REAL(xj);
+          for (int i=0; i<n; i++) {
+            if (!ISNAN(xjp[i])) {
+              outp[i] = ISNAN(outp[i]) ? xjp[i] : outp[i] * xjp[i];
+            }
+          }
+        } break;
+        default:
+          error(_("Internal error: should have caught non-INTSXP/REALSXP input by now"));
         }
       }
     }
@@ -181,11 +202,7 @@ SEXP pprod(SEXP x, SEXP narmArg) {
       int *outp = INTEGER(out);
       int *xj0p = INTEGER(VECTOR_ELT(x, 0));
       for (int i=0; i<n; i++) outp[i] = xj0p[i];
-      if (J == 1) {
-        UNPROTECT(1);
-        return out;
-      }
-      for (int j=1; j<J; j++) {
+      for (int j=1; j<J; j++) { // J>=2 by now
         int *xjp = INTEGER(VECTOR_ELT(x, j));
         for (int i=0; i<n; i++) {
           outp[i] = outp[i] == NA_INTEGER || xjp[i] == NA_INTEGER ? NA_INTEGER : outp[i] * xjp[i];
@@ -193,16 +210,30 @@ SEXP pprod(SEXP x, SEXP narmArg) {
       }
     } else { // REALSXP
       double *outp = REAL(out);
-      double *xj0p = REAL(VECTOR_ELT(x, 0));
-      for (int i=0; i<n; i++) outp[i] = xj0p[i];
-      if (J == 1) {
-        UNPROTECT(1);
-        return out;
+      if (TYPEOF(VECTOR_ELT(x, 0)) == INTSXP) {
+        int *xj0p = INTEGER(VECTOR_ELT(x, 0));
+        for (int i=0; i<n; i++) outp[i] = (double)xj0p[i];
+      } else { // x[[1]] must be REALSXP in current logic
+        double *xj0p = REAL(VECTOR_ELT(x, 0));
+        for (int i=0; i<n; i++) outp[i] = xj0p[i];
       }
       for (int j=1; j<J; j++) {
-        double *xjp = REAL(VECTOR_ELT(x, j));
-        for (int i=0; i<n; i++) {
-          outp[i] = outp[i] == NA_REAL || xjp[i] == NA_REAL ? NA_REAL : outp[i] * xjp[i];
+        SEXP xj=VECTOR_ELT(x, j);
+        switch (TYPEOF(xj)) {
+        case INTSXP: {
+          int *xjp = INTEGER(xj);
+          for (int i=0; i<n; i++) {
+            outp[i] = outp[i] == NA_REAL || xjp[i] == NA_INTEGER ? NA_REAL : outp[i] * (double)xjp[i];
+          }
+        } break;
+        case REALSXP: {
+          double *xjp = REAL(xj);
+          for (int i=0; i<n; i++) {
+            outp[i] = outp[i] == NA_REAL || xjp[i] == NA_REAL ? NA_REAL : outp[i] * xjp[i];
+          }
+        } break;
+        default:
+          error(_("Internal error: should have caught non-INTSXP/REALSXP input by now"));
         }
       }
     }
