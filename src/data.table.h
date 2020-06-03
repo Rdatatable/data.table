9a688b21ec1121f65c3bf47365b744ea402a55cb
Matt Dowle <mattjdowle@gmail.com>
2020-06-03 15:37:29 -0600

    3rd attempt. Took opportunity to outlaw sprintf and require snprintf internally always.

--- src/data.table.h
+++ src/data.table.h
@@ -15,8 +15,9 @@
 #include "po.h"
 #ifdef WIN32  // positional specifiers (%n$) used in translations; #4402
 #  define snprintf _sprintf_p  // the non-n one in Windows takes n anyway so there's no separate _snprintf_f
-#  define sprintf(dest, format, ...)  _sprintf_p(dest, INT_MAX, format, ...)
 #endif
+#define sprintf USE_SNPRINTF_NOT_SPRINTF  // prevent use of sprintf in data.table source; force us to use n always
+
 // #include <signal.h> // the debugging machinery + breakpoint aidee
 // raise(SIGINT);
 
