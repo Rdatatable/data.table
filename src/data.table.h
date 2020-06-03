be60eea9602119f9637b1012f77b358af0eea345
Matt Dowle <mattjdowle@gmail.com>
2020-06-03 16:49:48 -0600

    long shot: some references online to the leading underscore may not be needed

--- src/data.table.h
+++ src/data.table.h
@@ -14,7 +14,7 @@
 #include "types.h"
 #include "po.h"
 #ifdef WIN32  // positional specifiers (%n$) used in translations; #4402
-#  define snprintf _sprintf_p  // the non-n one in Windows takes n anyway so there's no separate _snprintf_f
+#  define snprintf sprintf_p  // the non-n one in Windows takes n anyway so there's no separate _snprintf_f
 #endif
 #define sprintf USE_SNPRINTF_NOT_SPRINTF  // prevent use of sprintf in data.table source; force us to use n always
 
