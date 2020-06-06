ffb521aec9079ae12e4ce9621f9ccc336a5ed9d6
jangorecki <j.gorecki@wit.edu.pl>
2020-06-06 19:02:11 +0100

    finally parallel

--- src/fjoin.c
+++ src/fjoin.c
@@ -6,24 +6,23 @@
  * split whole 1:nx_starts into numbers of buckets equal to threads*2
  * each thread do binary merge x of first and last element on y, and then sort-merge to a subset of y defined by those matches
  */
-static int max_i_match(int *y, int ny, int *y_o, bool y_ord, int val) { // those need to be binary search obviously
-  for (int i=0; i<ny; ++i) {
-    //Rprintf("i=%d: y[y_o[i]] > val: %d > %d\n", i, y[y_o[i]-1], val);
-    if (y[y_o[i]-1]>val) {
-      if (i==0) error("i=0 max_i_match, unhandled");
-      return(i-1);
-    }
+static int max_i_match(int *y, int *y_starts, int ny_starts, int val) { // those need to be binary search obviously
+  int i;
+  for (i=0; i<ny_starts; ++i) {
+    //Rprintf("max_i_match: y[y_starts[%d]-1] >= val: %d >= %d\n", i, y[y_starts[i]-1], val);
+    if (y[y_starts[i]-1]==val) return(i);
+    else if (y[y_starts[i]-1]>val) return(i-1);
   }
-  error("not yet handled in max_i_match");
+  return(i-1);
 }
-static int min_i_match(int *y, int ny, int *y_o, bool y_ord, int val) {
-  for (int i=ny-1; i<ny; --i) {
-    if (y[y_o[i]-1]<val) {
-      if (i==ny-1) error("i=ny-1 min_i_match, unhandled");
-      return(i+1);
-    }
+static int min_i_match(int *y, int *y_starts, int ny_starts, int val) {
+  int i;
+  for (i=ny_starts-1; i>=0; --i) {
+    //Rprintf("min_i_match: y[y_starts[%d]-1] <= val: %d <= %d\n", i, y[y_starts[i]-1], val);
+    if (y[y_starts[i]-1]==val) return(i);
+    else if (y[y_starts[i]-1]<val) return(i+1);
   }
-  error("not yet handled in min_i_match");
+  return(i+1);
 }
 
 // helper to count how many threads were used
@@ -37,10 +36,32 @@ int unqN(int *x, int nx) { // x have 0:(nx-1) values
   return ans;
 }
 
+// materialize counts of duplicated entries
 void grpLens(int *starts, int n_starts, int n, int *lens) { // #4395
   for (int i=0; i<n_starts-1; ++i)
     lens[i] = starts[i+1]-starts[i];
   lens[n_starts-1] = n - starts[n_starts-1]+1;
+  return;
+}
+
+// workhorse join that runs in parallel
+void fjoin1(int *xs, int *xl, int nxs, int *ys, int *yl, int nys, int *x, int *y, int *starts_y, int *lens_y) {
+  int i=0, j=0, is=0, js=0, x_i, y_j;
+  while (is<nxs && js<nys) {
+    //Rprintf("============= is=%d\n", is);
+    i = xs[is]-1, j = ys[js]-1;
+    x_i = x[i], y_j = y[j];
+    if (x_i == y_j) {
+      //Rprintf("x[%d]==y[%d]: %d\n", i, j, x_i);
+      int j1 = j+1;
+      //Rprintf("x_lens[is]=x_lens[%d]=%d\n",is,xl[is]);
+      for (int ii=0; ii<xl[is]; ++ii) {
+        //Rprintf("starts_y[%d]=%d\n", i+ii, j1);
+        starts_y[i+ii] = j1; lens_y[i+ii] = yl[js];
+      }
+      is++;
+    } else if (x_i < y_j) is++; else if (x_i > y_j) js++;
+  }
   return;
 }
 
@@ -50,13 +71,10 @@ void fjoinC(int *x, int nx, int *x_starts, int nx_starts,
            bool *match, int *matchn,
            int *starts_x, int *lens_x,
            int *starts_y, int *lens_y) {
-  bool x_ord=true, y_ord=true;
-  int *x_o=0, *y_o=0;
-  
   bool unq_x = nx_starts==nx, unq_y = ny_starts==ny;
   int *x_lens=0, *y_lens=0;
   double t_lens = omp_get_wtime();
-  if (!unq_x || true) {
+  if (!unq_x || true) { // else true as we dont yet have branches for unq cases
     x_lens = (int *)R_alloc(nx_starts, sizeof(int)); // #4395
     grpLens(x_starts, nx_starts, nx, x_lens);
     //for (int i=0; i<nx_starts; ++i) Rprintf("x_lens[%d]=%d\n", i, x_lens[i]);
@@ -68,132 +86,62 @@ void fjoinC(int *x, int nx, int *x_starts, int nx_starts,
   t_lens = omp_get_wtime() - t_lens;
   
   double t_batch = omp_get_wtime();
-  //int min_i = 0, max_i = x_starts[nx_starts-1]-1;
-  //Rprintf("x[i]=[%d:%d]\n", min_i, max_i);
-  //int nth = getDTthreads();
+  /*
+  int min_i = 0, max_i = x_starts[nx_starts-1]-1;
+  Rprintf("x[i]=[%d:%d]\n", min_i, max_i);
+  int b1_min_i = min_i, b1_max_i = max_i;
+  int b1_min_i = min_i, b1_max_i = x_starts[nx_starts/2]-1;
+  int b2_min_i = x_starts[nx_starts/2+1]-1, b2_max_i = max_i;
+  Rprintf("x: 1[%d-%d]\n", b1_min_i, b1_max_i);
+  Rprintf("x: 2[%d-%d]\n", b2_min_i, b2_max_i);
+  */
+  int nth = 2;//getDTthreads();
   //int nBatch=nth*2;
-  int nBatch=1;
-  //int b1_min_i = min_i, b1_max_i = max_i;
-  //int b1_min_i = min_i, b1_max_i = x_starts[nx_starts/2]-1;
-  //int b2_min_i = x_starts[nx_starts/2+1]-1, b2_max_i = max_i;
-  //Rprintf("x: 1[%d-%d]\n", b1_min_i, b1_max_i);
-  //Rprintf("x: 2[%d-%d]\n", b2_min_i, b2_max_i);
+  int nBatch=2;
   
-  
-  int *xs, *ys, nxs, nys;
+  int *xs, *ys, nxs, nys, *xl, *yl;
+  /*
   xs = x_starts + 0*sizeof(int);
   nxs = nx_starts;
   ys = y_starts + 0*sizeof(int);
   nys = ny_starts;
-  
-  int *th = (int *)R_alloc(nBatch, sizeof(int)); // report number of used threads
+   */
+  int b1_nxs = nx_starts/2, b2_nxs = nx_starts-b1_nxs;
+  int *b1_xs = x_starts, *b2_xs = x_starts + b1_nxs;
+  int *b1_xl = x_lens, *b2_xl = x_lens + b1_nxs;
+  //Rprintf("batch sizes nx: b1=%d; b2=%d\nidx of first in batch: x_starts[0]: b1=%d; b2=%d\nfirst value in batch: x[x_starts[0]-1]: b1=%d; b2=%d\n\n",
+  //                      b1_nxs, b2_nxs,                                 b1_xs[0], b2_xs[0],                         x[b1_xs[0]-1], x[b2_xs[0]-1]);
+  //Rprintf("finding bounds of b1 y starts indices\n\n");
+  int b1_ylow = min_i_match(y, y_starts, ny_starts, x[b1_xs[0]-1]);
+  int b1_yupp = max_i_match(y, y_starts, ny_starts, x[b1_xs[b1_nxs-1]-1]);
+  //Rprintf("\nb1_ylow=%d; b1_yupp=%d\ny[y_starts[b1_ylow]-1]=%d; y[ny_starts[b1_yupp]-1]=%d\n", b1_ylow, b1_yupp, y[y_starts[b1_ylow]-1], y[y_starts[b1_yupp]-1]);
+  int b2_ylow = min_i_match(y, y_starts, ny_starts, x[b2_xs[0]-1]);
+  int b2_yupp = max_i_match(y, y_starts, ny_starts, x[b2_xs[b2_nxs-1]-1]);
+  //Rprintf("\nb2_ylow=%d; b2_yupp=%d\ny[y_starts[b2_ylow]-1]=%d; y[ny_starts[b2_yupp]-1]=%d\n", b2_ylow, b2_yupp, y[y_starts[b2_ylow]-1], y[y_starts[b2_yupp]-1]);
+  int b1_nys = b1_yupp-b1_ylow+1, b2_nys = b2_yupp-b2_ylow+1;
+  int *b1_ys = y_starts, *b2_ys = y_starts + b1_nys;
+  int *b1_yl = y_lens, *b2_yl = y_lens + b1_nys;
+  //Rprintf("batch sizes ny: b1=%d; b2=%d\nidx of first in batch: y_starts[0]: b1=%d; b2=%d\nfirst value in batch: y[y_starts[0]-1]: b1=%d; b2=%d\n\n",
+  //        b1_nys, b2_nys,                                 b1_ys[0], b2_ys[0],                         y[b1_ys[0]-1], y[b2_ys[0]-1]);
+  int *th = (int *)R_alloc(nBatch, sizeof(int)); // report threads used
   t_batch = omp_get_wtime() - t_batch;
-  
   double t_join = omp_get_wtime();
-  //#pragma omp parallel for schedule(dynamic) num_threads(nth)
+  #pragma omp parallel for schedule(dynamic) num_threads(nth)
   for (int b=0; b<nBatch; ++b) {
-    // fjoin1(/*local in*/xs, nxs, ys, nys, /*global in*/x, x_lens, y, y_lens, /*out*/start_y, lens_y);
-    int i=0, j=0, is=0, js=0, x_i, y_j;
-    while (is<nxs && js<nys) {
-      //Rprintf("============= is=%d\n", is);
-      i = xs[is]-1, j = ys[js]-1;
-      x_i = x[i], y_j = y[j];
-      if (x_i == y_j) {
-        //Rprintf("x[%d]==y[%d]: %d\n", i, j, x_i);
-        int j1 = j+1;
-        for (int ii=0; ii<x_lens[is]; ++ii) {
-          //Rprintf("starts_y[%d]=%d\n", i+ii, j1);
-          starts_y[i+ii] = j1; lens_y[i+ii] = y_lens[js];
-        }
-        is++;
-      } else if (x_i < y_j) is++; else if (x_i > y_j) js++;
+    if (b==0) {
+      xs = b1_xs; nxs = b1_nxs; ys = b1_ys; nys = b1_nys, xl = b1_xl, yl = b1_yl;
+    } else if (b==1) {
+      xs = b2_xs; nxs = b2_nxs; ys = b2_ys; nys = b2_nys, xl = b2_xl, yl = b2_yl;
     }
-    /*while (is<nx_starts && js<ny_starts) {
-      //Rprintf("============= is=%d\n", is);
-      i = x_starts[is]-1, j = y_starts[js]-1;
-      x_i = x[i], y_j = y[j];
-      if (x_i == y_j) {
-        //Rprintf("x[%d]==y[%d]: %d\n", i, j, x_i);
-        int j1 = j+1;
-        for (int ii=0; ii<x_lens[is]; ++ii) {
-          //Rprintf("starts_y[%d]=%d\n", i+ii, j1);
-          starts_y[i+ii] = j1; lens_y[i+ii] = y_lens[js];
-        }
-        is++;
-      } else if (x_i < y_j) is++; else if (x_i > y_j) js++;
-    }*/
+    fjoin1(xs, xl, nxs, ys, yl, nys, /*batch specific input: shifted pointers and size*/
+           x, y,                     /*common input*/
+           starts_y, lens_y);        /*common output*/
     th[b] = omp_get_thread_num();
   }
+  for (int b=0; b<nBatch; ++b) Rprintf("th[%d]=%d\n", th[b]);
   t_join = omp_get_wtime() - t_join;
-  //for (int b=0; b<nBatch; ++b) Rprintf("th(batch[%d])=%d\n", b, th[b]);
-  
-  if (false) {
-    error("input must be ordered!");
-    if (false) {
-      // find bucket ranges
-      int min_x_i = x_o[0]-1, max_x_i = x_o[nx-1]-1;
-      int min_y_i = y_o[0]-1, max_y_i = y_o[ny-1]-1;
-      //int min_x = x[min_x_i], max_x = x[max_x_i];
-      //int min_y = y[min_y_i], max_y = y[max_y_i];
-      
-      //bool trim_min_x = min_x < min_y;
-      //bool trim_min_y = min_x > min_y;
-      //bool trim_max_x = max_x > max_y;
-      //bool trim_max_y = max_x < max_y;
-      // todo trim buckets to join-able subsets of x and y
-      
-      //Rprintf("min_x=%d; max_x=%d; min_y=%d, max_y=%d\n", min_x, max_x, min_y, max_y);
-      //int min_xy = imin(min_x, min_y), max_xy = imin(max_x, max_y);
-      //if (max_xy - min_xy < 2) error("not handled yet");
-      
-      int n_buckets = 2;
-      if (n_buckets != 2) error("not handled yet");
-      // this needs to be done for every bucket
-      if (y_ord) error("y must not be sorted");
-      
-      int b1_max_x_o_i = max_i_match(x, nx, x_o, x_ord, x[x_o[nx/2]-1]);
-      Rprintf("b1_max_x_o_i=%d; x[x_o[b1_max_x_o_i]-1]=%d\n", b1_max_x_o_i, x[x_o[b1_max_x_o_i]-1]);
-      int b1_min_x_i = min_x_i, b1_max_x_i = x_o[b1_max_x_o_i]-1; // this need to ensure that duplicate goes into same bucket!! fjoin(c(1:3,3L,3L,5L,4L), y), 3 goes into both buckets!
-      int b2_min_x_i = x_o[b1_max_x_o_i+1]-1, b2_max_x_i = max_x_i;
-      if (x[b1_max_x_i]==x[b2_min_x_i]) error("value of x=%d fits into multiple buckets", x[b2_min_x_i]);
-      Rprintf("min(x)=%d; max(x)=%d; len(x)=%d\n", x[min_x_i], x[max_x_i], nx);
-      Rprintf("b1(min(x))=%d; b1(max(x))=%d; b1(len(x))=%d\n", x[b1_min_x_i], x[b1_max_x_i], b1_max_x_o_i-0);
-      Rprintf("b2(min(x))=%d; b2(max(x))=%d; b2(len(x))=%d\n", x[b2_min_x_i], x[b2_max_x_i], nx-b1_max_x_o_i+1);
-      error("dev");
-      int b1_max_y_o_i = max_i_match(y, ny, y_o, y_ord, /*value to match to*/x[b1_max_x_i]);
-      int b1_min_y_i = min_y_i, b1_max_y_i = y_o[b1_max_y_o_i]-1;
-      int b2_min_y_o_i = min_i_match(y, ny, y_o, y_ord, x[b2_min_x_i]);
-      int b2_min_y_i = y_o[b2_min_y_o_i]-1, b2_max_y_i = max_y_i;
-      Rprintf("b1_min_x_i=%d, b1_max_x_i=%d, b2_min_x_i=%d, b2_max_x_i=%d\n", b1_min_x_i, b1_max_x_i, b2_min_x_i, b2_max_x_i);
-      Rprintf("buckets: 1(x:%d-%d, y:%d-%d); 2(x:%d-%d, y:%d-%d)\n", x[b1_min_x_i], x[b1_max_x_i], y[b1_min_y_i], y[b1_max_y_i], x[b2_min_x_i], x[b2_max_x_i], y[b2_min_y_i], y[b2_max_y_i]);
-      // openmp here!!
-      for (int b=0; b<n_buckets; ++b) {
-        int *x_o_, nx_, *y_o_, ny_;
-        if (b==0) {
-          x_o_ = x_o;
-          nx_ = nx/2;
-          y_o_ = y_o;
-          ny_ = b1_max_y_o_i;
-        } else {
-          x_o_ = x_o + (nx/2+1)*sizeof(int);
-          nx_ = nx-nx/2;
-          y_o_ = y_o + b2_min_y_o_i*sizeof(int);
-          ny_ = ny-b2_min_y_o_i+1;
-        }
-        Rprintf("--- bucket %d content ---", b);
-        Rprintf("\nx (len %d):", nx_);
-        for (int i=0; i<nx_; ++i) Rprintf(" %d", x[x_o_[i]-1]);
-        Rprintf("\ny (len %d):", ny_);
-        for (int i=0; i<ny_; ++i) Rprintf(" %d", y[y_o_[i]-1]);
-        Rprintf("\n");
-        //bucketjoin(x, y, bucket_len_x, bucket_start_x, x_o, x_min, y
-      }
-      error("dev");
-      //error("dev: !x_ord && !y_ord && (!unq_x || !unq_y)");
-    }
-    //Rprintf("x_i=%d; y[y_o[j]-1]= y[y_o[%d]-1]= y[%d-1]= y[%d]= %d\n", x[i], j, y_o[j], y_o[j]-1, y[y_o[j]-1]);
-  }
-  Rprintf("fjoinC: lens %.3fs; batch %.3fs; join %.3fs; used %d th\n", t_lens, t_batch, t_join, unqN(th, nBatch));
+
+  Rprintf("fjoinC: lens %.3fs; batch %.3fs; join %.3fs; used %dth\n", t_lens, t_batch, t_join, unqN(th, nBatch));
   matchn[0] = nx;
 }
 
