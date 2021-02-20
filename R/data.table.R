db69ee91d9dd103e03a78c360635e51497b60861
Michael Young <myoung3@deohs-mesa03.deohs.washington.edu>
2021-02-19 20:03:08 -0800

    fix breaking tests related to inconsistent naming of blank columnames

--- R/data.table.R
+++ R/data.table.R
@@ -1851,7 +1851,10 @@ replace_dot_alias = function(e) {
     if (any(ww)) jvnames[ww] = paste0("V",ww)
     setattr(ans, "names", c(bynames, jvnames))
   } else {
-    setnames(ans,seq_along(bynames),bynames)   # TO DO: reinvestigate bynames flowing from dogroups here and simplify
+    nonbynames <- names(ans)[-seq_along(bynames)] #related to 2311. make naming of empty columns names more consistent
+    ww = which(nonbynames=="")
+    if (any(ww)) nonbynames[ww] = paste0("V",ww)
+    setattr(ans, "names", c(bynames, nonbynames))   # TO DO: reinvestigate bynames flowing from dogroups here and simplify
   }
   if (byjoin && keyby && !bysameorder) {
     if (verbose) {last.started.at=proc.time();cat("setkey() afterwards for keyby=.EACHI ... ");flush.console()}
