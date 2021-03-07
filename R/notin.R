212b0b2bc2fbfeefe13f2dcbdb757c605635cbf2
mczekanski1 <mczekanski1@gmail.com>
2021-03-07 09:25:15 -0500

    include branching for is.character in response to PR comments

--- R/notin.R
+++ R/notin.R
@@ -1,6 +1,10 @@
 # Intended to be used to create %notin% operator
 notin = function(example, elements) {
-  return(!match(example, elements, nomatch = 0))
+  if (is.character(example)) {
+    return(!chmatch(example, elements, nomatch = 0))
+  } else {
+    return(!match(example, elements, nomatch = 0))
+  }
 }
 
 "%notin%" = function(example, elements) notin(example, elements)
