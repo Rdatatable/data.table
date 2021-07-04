e874bea79b5071a5cb59ba6d2dd4ff9f959c24d5
Václav Tlapák <55213630+tlapak@users.noreply.github.com>
2021-07-04 20:43:06 +0200

    reuse parent

--- R/utils.R
+++ R/utils.R
@@ -131,7 +131,7 @@ eval_with_cols = function(orig_call, all_cols) {
     if ("cols" %in% names(formals(fun)) && !"cols" %in% names(named_call)) {
       named_call[["cols"]] = all_cols
     } else if (is.call(named_call[["cols"]])) {
-      named_call[["cols"]] = eval(named_call[["cols"]], parent.frame(2L))
+      named_call[["cols"]] = eval(named_call[["cols"]], parent)
     }
     named_call[[1L]] = fun
     eval(named_call, parent)
