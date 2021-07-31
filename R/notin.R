84f47606ee89ad6ba363b420b01ed196f335e72a
mczekanski1 <mczekanski1@gmail.com>
2021-07-31 12:34:38 -0400

    bug fix, match variable names

--- R/notin.R
+++ R/notin.R
@@ -1,5 +1,5 @@
 "%notin%" = function(x, table) {
-  if (is.character(example)) {
+  if (is.character(x) && is.character((table))) {
     return(.Call(Cchin, x, table, TRUE))
   } else {
     return(!match(x, table, nomatch = 0))
