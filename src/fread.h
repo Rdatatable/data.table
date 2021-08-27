d1206196a64e9fc0367a93498406fa80d796bff6
Matt Dowle <mattjdowle@gmail.com>
2021-08-27 08:16:14 -0600

    drop lower half of lookup table as @QuLogic suggested

--- src/fread.h
+++ src/fread.h
@@ -37,7 +37,7 @@ typedef enum {
 
 extern int8_t typeSize[NUMTYPE];
 extern const char typeName[NUMTYPE][10];
-extern const long double pow10lookup[601];
+extern const long double pow10lookup[301];
 extern const uint8_t hexdigits[256];
 
 
