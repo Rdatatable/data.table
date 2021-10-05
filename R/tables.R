75cc7380c14cd2ab21051158f38179664195b024
Michael Chirico <chiricom@google.com>
2021-10-05 20:50:03 +0000

    small tweaks to avoid double-sorting

--- R/tables.R
+++ R/tables.R
@@ -6,9 +6,9 @@ tables = function(mb=TRUE, order.col="NAME", width=80,
 {
   # Prints name, size and colnames of all data.tables in the calling environment by default
   # include "hidden" objects (starting with .) via all.names=TRUE, but exclude ... specifically, #5197
-  all_obj = grep("...", objects(envir=env, all.names=TRUE), invert=TRUE, fixed=TRUE, value=TRUE)
-  is_DT = which(vapply_1b(all_obj, function(x) is.data.table(get(x, envir=env))))
-  if (!length(is_DT)) {
+  all_obj = grep("...", objects(envir=env, all.names=TRUE, sorted=order.col == "NAME"), invert=TRUE, fixed=TRUE, value=TRUE)
+  is_DT = vapply_1b(all_obj, function(x) is.data.table(get(x, envir=env)))
+  if (!any(is_DT)) {
     if (!silent) catf("No objects of class data.table exist in %s\n", if (identical(env, .GlobalEnv)) ".GlobalEnv" else format(env))
     return(invisible(data.table(NULL)))
   }
@@ -24,8 +24,11 @@ tables = function(mb=TRUE, order.col="NAME", width=80,
       KEY = list(key(DT)),
       INDICES = if (index) list(indices(DT)))
   }))
-  if (!order.col %chin% names(info)) stopf("order.col='%s' not a column name of info", order.col)
-  info = info[base::order(info[[order.col]])]  # base::order to maintain locale ordering of table names
+  # objects() above handled the sorting for order.col=="NAME"
+  if (order.col != "NAME") {
+    if (!order.col %chin% names(info)) stopf("order.col='%s' not a column name of info", order.col)
+    info = info[base::order(info[[order.col]])]  # base::order to maintain locale ordering of table names
+  }
   if (!silent) {
     # prettier printing on console
     pretty_format = function(x, width) {
