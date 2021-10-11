0ef4c72da4f5ab5d58bb2729c7962719ca3dc442
mattdowle <mattjdowle@gmail.com>
2021-10-11 14:41:42 -0600

    add PR number now that's known

--- R/data.table.R
+++ R/data.table.R
@@ -222,7 +222,7 @@ replace_dot_alias = function(e) {
   .unsafe.opt() #3585
   missingnomatch = missing(nomatch)
   nomatch0 = identical(nomatch,0) || identical(nomatch,0L) || identical(nomatch, FALSE)  # for warning with row-numbers in i; #4353
-  if (nomatch0) nomatch=NULL  # retain nomatch=0|FALSE backwards compatibility, #857 #XXXX
+  if (nomatch0) nomatch=NULL  # retain nomatch=0|FALSE backwards compatibility, #857 #5214
   if (!(is.null(nomatch) || (length(nomatch)==1L && is.na(nomatch)))) stopf("nomatch= must be either NA or NULL (or 0 for backwards compatibility which is the same as NULL but please use NULL)")
   if (!is.logical(which) || length(which)>1L) stopf("which= must be a logical vector length 1. Either FALSE, TRUE or NA.")
   if ((isTRUE(which)||is.na(which)) && !missing(j)) stopf("which==%s (meaning return row numbers) but j is also supplied. Either you need row numbers or the result of j, but only one type of result can be returned.", which)
