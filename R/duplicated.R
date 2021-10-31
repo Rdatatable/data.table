065890d421ae9b69d12c0493d7e85282f437e5dd
Michael Chirico <michaelchirico4@gmail.com>
2021-10-31 00:00:54 -0700

    need to retain keys

--- R/duplicated.R
+++ R/duplicated.R
@@ -32,7 +32,7 @@ unique.data.table = function(x, incomparables=FALSE, fromLast=FALSE, by=seq_alon
   if (!length(by)) by = NULL  #4594
   o = forderv(x, by=by, sort=FALSE, retGrp=TRUE)
   if (is.null(cols)) cols = names(x) else cols = c(by, cols)
-  x = .shallow(x, cols)
+  x = .shallow(x, cols, retain.key=TRUE)
   # if by=key(x), forderv tests for orderedness within it quickly and will short-circuit
   # there isn't any need in unique() to call uniqlist like duplicated does; uniqlist returns a new nrow(x) vector anyway and isn't
   # as efficient as forderv returning empty o when input is already ordered
