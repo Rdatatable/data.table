79e363c8f49cd0674866e9e73453468c244bdf52
mattdowle <mattjdowle@gmail.com>
2021-11-24 17:34:05 -0700

    man and news tweak

--- R/print.data.table.R
+++ R/print.data.table.R
@@ -189,7 +189,7 @@ format_col.default = function(x, ...) {
   if (!is.null(dim(x)))
     "<multi-column>"
   else if (has_format_method(x) && length(formatted<-format(x, ...))==length(x))
-    formatted  #PR5224 motivated by package sf where column class is c("sfc_MULTIPOLYGON","sfc")
+    formatted  #PR5224 motivated by package sf where column class is c("sfc_MULTIPOLYGON","sfc") and sf:::format.sfc exists
   else if (is.list(x))
     vapply_1c(x, format_list_item, ...)
   else
