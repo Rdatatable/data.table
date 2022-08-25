cab28b53760fb2882a0ea2c94ccb228ee36e2a80
jangorecki <j.gorecki@wit.edu.pl>
2022-08-25 16:36:38 +0200

    unit tests for partial corner cases

--- R/froll.R
+++ R/froll.R
@@ -1,3 +1,28 @@
+partial2adaptive = function(x, n, align) {
+  if (align=="center")
+    stopf("'partial' cannot be used together with align='center'")
+  if (is.list(x) && length(unique(vapply(x, length, 0L)))!=1L)
+    stopf("'partial' does not support variable length of columns in 'x'")
+  if (!(is.numeric(n) || (is.list(n) && all(vapply(n, is.numeric, FALSE)))))
+    stopf("n must be integer vector or list of integer vectors")
+  len = if (is.list(x)) length(x[[1L]]) else length(x)
+  verbose = getOption("datatable.verbose")
+  if (verbose)
+    cat("partial2adaptive: froll partial=TRUE trimming 'n' and redirecting to adaptive=TRUE\n")
+  trimn = function(n, len, align) {
+    n = min(n, len)
+    if (align=="right")
+      c(seq.int(n), rep(n, len-n))
+    else
+      c(rep(n, len-n), rev(seq.int(n)))
+  }
+  if (is.list(n)) {
+    sapply(n, len, align, FUN=trimn)
+  } else {
+    trimn(n, len, align)
+  }
+}
+
 froll = function(fun, x, n, fill=NA, algo=c("fast", "exact"), align=c("right", "left", "center"), na.rm=FALSE, hasNA=NA, adaptive=FALSE, partial=FALSE) {
   stopifnot(!missing(fun), is.character(fun), length(fun)==1L, !is.na(fun))
   algo = match.arg(algo)
@@ -5,28 +30,7 @@ froll = function(fun, x, n, fill=NA, algo=c("fast", "exact"), align=c("right", "
   if (isTRUE(partial)) {
     if (isTRUE(adaptive))
       stopf("'partial' argument cannot be used together with 'adaptive'")
-    if (align=="center")
-      stopf("'partial' cannot be used together with align='center'")
-    if (is.list(x) && length(unique(vapply(x, length, 0L)))!=1L)
-      stopf("'partial' does not support variable length of columns in 'x'")
-    if (!(is.numeric(n) || (is.list(n) && all(vapply(n, is.numeric, FALSE)))))
-      stopf("n must be integer vector or list of integer vectors")
-    len = if (is.list(x)) length(x[[1L]]) else length(x)
-    verbose = getOption("datatable.verbose")
-    if (verbose)
-      cat("froll: partial=TRUE trimming 'n' and redirecting to adaptive=TRUE\n")
-    trimn = function(n, len, align) {
-      n = min(n, len)
-      if (align=="right")
-        c(seq.int(n), rep(n, len-n))
-      else
-        c(rep(n, len-n), rev(seq.int(n)))
-    }
-    n = if (is.list(n)) {
-      sapply(n, len, align, FUN=trimn)
-    } else {
-      trimn(n, len, align)
-    }
+    n = partial2adaptive(x, n, align)
     adaptive = TRUE
   } ## support for partial added in #5441
   leftadaptive = isTRUE(adaptive) && align=="left"
@@ -58,11 +62,16 @@ frollsum = function(x, n, fill=NA, algo=c("fast","exact"), align=c("right", "lef
 frollmax = function(x, n, fill=NA, algo=c("fast", "exact"), align=c("right", "left", "center"), na.rm=FALSE, hasNA=NA, adaptive=FALSE, partial=FALSE) {
   froll(fun="max", x=x, n=n, fill=fill, algo=algo, align=align, na.rm=na.rm, hasNA=hasNA, adaptive=adaptive, partial=partial)
 }
+
 frollapply = function(x, n, FUN, ..., fill=NA, align=c("right", "left", "center"), adaptive=FALSE, partial=FALSE) {
   FUN = match.fun(FUN)
   align = match.arg(align)
-  if (!missing(partial))
-    stopf("frollapply does not support 'partial' argument yet")
+  if (isTRUE(partial)) {
+    if (isTRUE(adaptive))
+      stopf("'partial' argument cannot be used together with 'adaptive'")
+    n = partial2adaptive(x, n, align)
+    adaptive = TRUE
+  }
   if (!missing(adaptive))
     stopf("frollapply does not support 'adaptive' argument yet")
   rho = new.env()
