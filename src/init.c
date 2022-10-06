f2964c12f717a876ec293c01c8ab6c7917eb9d6d
mattdowle <mattjdowle@gmail.com>
2022-10-05 21:48:03 -0600

    1.14.3 => 1.14.5; 1.14.4 will be backported patch to resolve current CRAN issues

--- src/init.c
+++ src/init.c
@@ -433,6 +433,6 @@ SEXP initLastUpdated(SEXP var) {
 
 SEXP dllVersion() {
   // .onLoad calls this and checks the same as packageVersion() to ensure no R/C version mismatch, #3056
-  return(ScalarString(mkChar("1.14.3")));
+  return(ScalarString(mkChar("1.14.5")));
 }
 
