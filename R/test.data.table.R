a5dea77ff323b98bdef9eb5a8039c37634429ee3
mattdowle <mattjdowle@gmail.com>
2022-11-07 15:19:47 -0700

    memtest now an argument to test.data.table; simpler than having to use the env variable

--- R/test.data.table.R
+++ R/test.data.table.R
@@ -1,4 +1,5 @@
-test.data.table = function(script="tests.Rraw", verbose=FALSE, pkg=".", silent=FALSE, showProgress=interactive()&&!silent) {
+test.data.table = function(script="tests.Rraw", verbose=FALSE, pkg=".", silent=FALSE, showProgress=interactive()&&!silent,
+                           memtest=as.logical(Sys.getenv("TEST_DATA_TABLE_MEMTEST", "FALSE"))) {
   stopifnot(isTRUEorFALSE(verbose), isTRUEorFALSE(silent), isTRUEorFALSE(showProgress))
   if (exists("test.data.table", .GlobalEnv, inherits=FALSE)) {
     # package developer
@@ -112,10 +113,9 @@ test.data.table = function(script="tests.Rraw", verbose=FALSE, pkg=".", silent=F
   assign("whichfail", NULL, envir=env)
   assign("started.at", proc.time(), envir=env)
   assign("lasttime", proc.time()[3L], envir=env)  # used by test() to attribute time inbetween tests to the next test
-  assign("timings", data.table( ID = seq_len(9999L), time=0.0, nTest=0L ), envir=env)   # test timings aggregated to integer id
-  assign("memtest", TRUE, envir=env)  # as.logical(Sys.getenv("TEST_DATA_TABLE_MEMTEST", "FALSE")), envir=env)
+  assign("timings", data.table( ID = seq_len(9999L), time=0.0, nTest=0L, RSS=0.0 ), envir=env)   # test timings aggregated to integer id
+  assign("memtest", memtest, envir=env)
   assign("filename", fn, envir=env)
-  assign("inittime", as.integer(Sys.time()), envir=env) # keep measures from various test.data.table runs
   assign("showProgress", showProgress, envir=env)
 
   owd = setwd(tempdir()) # ensure writeable directory; e.g. tests that plot may write .pdf here depending on device option and/or batch mode; #5190
@@ -175,19 +175,19 @@ test.data.table = function(script="tests.Rraw", verbose=FALSE, pkg=".", silent=F
 
   # There aren't any errors, so we can use up 11 lines for the timings table
   timings = env$timings[nTest>0]
-  if (!env$memtest) {
-    ans = head(timings[-1L][order(-time)], 10L)   # exclude id 1 as in dev that includes JIT
+  if (!memtest) {
+    ans = head(timings[-1L][order(-time)], 10L)[,RSS:=NULL]   # exclude id 1 as in dev that includes JIT
     if ((x<-sum(timings[["nTest"]])) != ntest) {
       warningf("Timings count mismatch: %d vs %d", x, ntest)  # nocov
     }
-    catf("10 longest running tests took %ds (%d%% of %ds)\n", as.integer(tt<-DT[, sum(time)]), as.integer(100*tt/(ss<-timings[,sum(time)])), as.integer(ss))
+    catf("10 longest running tests took %ds (%d%% of %ds)\n", as.integer(tt<-ans[, sum(time)]), as.integer(100*tt/(ss<-timings[,sum(time)])), as.integer(ss))
     print(ans, class=FALSE)
   } else {
     y = head(order(-diff(timings$RSS)), 10L)
     ans = timings[, diff:=c(NA,round(diff(RSS),1))][y+1L][,time:=NULL]  # time is distracting and influenced by gc() calls; just focus on RAM usage here
-    catf("10 largest RAM increases (MB); see plot for cummulative effect (if any)\n")
+    catf("10 largest RAM increases (MB); see plot for cumulative effect (if any)\n")
     print(ans, class=FALSE)
-    plot(timings$RSS)
+    plot(timings$RSS, main=fn, ylab="RSS (MB)")
   }
 
   catf("All %d tests (last %.8g) in %s completed ok in %s\n", ntest, env$prevtest, names(fn), timetaken(env$started.at))
@@ -288,20 +288,18 @@ test = function(num,x,y=TRUE,error=NULL,warning=NULL,message=NULL,output=NULL,no
     lasttime = get("lasttime", parent.frame())
     timings = get("timings", parent.frame())
     memtest = get("memtest", parent.frame())
-    # inittime = get("inittime", parent.frame())
     filename = get("filename", parent.frame())
     foreign = get("foreign", parent.frame())
     showProgress = get("showProgress", parent.frame())
     time = nTest = NULL  # to avoid 'no visible binding' note
     if (num>0) on.exit( {
        took = proc.time()[3L]-lasttime  # so that prep time between tests is attributed to the following test
-       # mem = as.list(c(inittime=inittime, filename=basename(filename), timestamp=timestamp, test=num, ps_mem(), gc_mem())) # nocov
        timings[as.integer(num), `:=`(time=time+took, nTest=nTest+1L), verbose=FALSE]
        if (memtest) {
-         gc() # force gc so we can find tests that use relatively larger amounts of RAM and don't rm() them
-         timings[as.integer(num), RSS:=max(ps_mem(),RSS,na.rm=TRUE), verbose=FALSE]
+         gc() # force gc so we can find tests that use relatively larger amounts of RAM
+         timings[as.integer(num), RSS:=max(ps_mem(),RSS), verbose=FALSE]
        }
-       assign("lasttime", proc.time()[3L], parent.frame(), inherits=TRUE)  # proc.time() after gc() to exclude gc() time when memtest
+       assign("lasttime", proc.time()[3L], parent.frame(), inherits=TRUE)  # after gc() to exclude gc() time from next test when memtest
     } )
     if (showProgress)
       # \r can't be in gettextf msg
@@ -344,9 +342,6 @@ test = function(num,x,y=TRUE,error=NULL,warning=NULL,message=NULL,output=NULL,no
     actual$message <<- c(actual$message, conditionMessage(m))
     m
   }
-  if (memtest) {
-    timestamp = as.numeric(Sys.time())   # nocov
-  }
   if (is.null(output) && is.null(notOutput)) {
     x = suppressMessages(withCallingHandlers(tryCatch(x, error=eHandler), warning=wHandler, message=mHandler))
     # save the overhead of capture.output() since there are a lot of tests, often called in loops
@@ -354,11 +349,6 @@ test = function(num,x,y=TRUE,error=NULL,warning=NULL,message=NULL,output=NULL,no
   } else {
     out = capture.output(print(x <- suppressMessages(withCallingHandlers(tryCatch(x, error=eHandler), warning=wHandler, message=mHandler))))
   }
-  #if (memtest) {
-  #  mem = as.list(c(inittime=inittime, filename=basename(filename), timestamp=timestamp, test=num, ps_mem(), gc_mem())) # nocov
-  #  
-  #  fwrite(mem, "memtest.csv", append=TRUE, verbose=FALSE)                                                                             # nocov
-  #}
   fail = FALSE
   if (.test.data.table && num>0) {
     if (num<prevtest+0.0000005) {
