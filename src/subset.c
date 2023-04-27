fd0942ab7da9237c891b90f7d631335e30d5e5cc
Benjamin Schwendinger <52290390+ben-schwen@users.noreply.github.com>
2023-04-27 19:57:41 +0200

    add subset and dogroups support for columns of type expression

--- src/subset.c
+++ src/subset.c
@@ -79,7 +79,7 @@ void subsetVectorRaw(SEXP ans, SEXP source, SEXP idx, const bool anyNA)
       for (int i=0; i<n; i++) {                     SET_STRING_ELT(ans, i, sp[idxp[i]-1]); }
     }
   } break;
-  case VECSXP : {
+  case VECSXP: case EXPRSXP: {
     const SEXP *sp = SEXPPTR_RO(source);
     if (anyNA) {
       for (int i=0; i<n; i++) { int elem = idxp[i]; SET_VECTOR_ELT(ans, i, elem==NA_INTEGER ? R_NilValue : sp[elem-1]); }
