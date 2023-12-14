463b974414c54a576953929fef88ca9434a138c5
Michael Chirico <chiricom@google.com>
2023-12-14 15:48:56 +0000

    delint tests

--- tests/autoprint.R
+++ tests/autoprint.R
@@ -14,24 +14,24 @@ print(DT[2,a:=4L])                    # no
 print(DT)                             # yes
 if (TRUE) DT[2,a:=5L]                 # no. used to print before v1.9.5
 if (TRUE) if (TRUE) DT[2,a:=6L]       # no. used to print before v1.9.5
-(function(){DT[2,a:=5L];NULL})()      # print NULL
+(function(){ DT[2,a:=5L]; NULL })()   # print NULL
 DT                                    # no (from v1.9.5+). := suppresses next auto print (can't distinguish just "DT" symbol alone at the prompt)
 DT                                    # yes. 2nd time needed, or solutions below
-(function(){DT[2,a:=5L];NULL})()      # print NULL
+(function(){ DT[2,a:=5L]; NULL })()   # print NULL
 DT[]                                  # yes. guaranteed print
-(function(){DT[2,a:=5L];NULL})()      # print NULL
+(function(){ DT[2,a:=5L]; NULL })()   # print NULL
 print(DT)                             # no. only DT[] is guaranteed print from v1.9.6 and R 3.2.0
-(function(){DT[2,a:=5L][];NULL})()    # print NULL
+(function(){ DT[2,a:=5L][]; NULL })() # print NULL
 DT                                    # yes. i) function needs to add [] after last one, so that "DT" alone is guaranteed anyway
-(function(){DT[2,a:=5L];DT[];NULL})() # print NULL
+(function(){ DT[2,a:=5L]; DT[]; NULL })() # print NULL
 DT                                    # yes. ii) or as a separate DT[] after the last := inside the function
 DT2 = data.table(b=3:4)               # no
-(function(){DT[2,a:=6L];DT2[1,b:=7L];NULL})()
+(function(){ DT[2,a:=6L]; DT2[1,b:=7L]; NULL })()
 DT                                    # yes. last := was on DT2 not DT
 {DT[2,a:=6L];invisible()}             # no
 print(DT)                             # no
-(function(){print(DT[2,a:=7L]);print(DT);invisible()})()    # yes*2
-{print(DT[2,a:=8L]);print(DT);invisible()}                  # yes*1  Not within function so as at prompt
+(function(){ print(DT[2,a:=7L]); print(DT); invisible() })()    # yes*2
+{ print(DT[2,a:=8L]); print(DT); invisible() }                  # yes*1  Not within function so as at prompt
 DT[1][,a:=9L]      # no (was too tricky to detect that DT[1] is a new object). Simple rule is that := always doesn't print
 DT[2,a:=10L][1]                       # yes
 DT[1,a:=10L][1,a:=10L]                # no
@@ -43,4 +43,3 @@ DT[1,a:=10L][]                        # yes. ...[] == oops, forgot print(...)
 tryCatch(DT[,foo:=ColumnNameTypo], error=function(e) e$message)         # error: not found.
 DT                                    # yes
 DT                                    # yes
-
