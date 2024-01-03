dd532452ee8e8339576d3fa9fe730e684d50845e
Michael Chirico <chiricom@google.com>
2024-01-04 00:05:12 +0800

    Compile translations (#5879)
    * fix errors around po syntax/newline mismatch
    
    * update .mo compiled translations
    
    * CRAN_release note

--- .dev/CRAN_Release.cmd
+++ .dev/CRAN_Release.cmd
@@ -16,12 +16,14 @@ xgettext -o /dev/stdout ./*.c \
 Rscript -e "tools::update_pkg_po('.')"
 
 # 2) Open a PR with the new templates & contact the translators
-#   * zh_CN:
+#   * zh_CN: @hongyuanjia
 ## Translators to submit commits with translations to this PR
 ##   [or perhaps, if we get several languages, each to open
 ##    its own PR and merge to main translation PR]
 
 ## 3) Check validity with tools::checkPoFiles("zh_CN")
+
+## 4) Compile the new .mo binary files with potools::po_compile()
 
 ###############################################
 #  Basic checks
