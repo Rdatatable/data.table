8e7a6aa91a7af63b49325252b35d8d1f3087bd6c
Michael Chirico <michaelchirico4@gmail.com>
2024-01-13 01:10:04 +0800

    Friendlier error in assignment with trailing comma (#5467)
    * friendlier error in assignment with trailing comma
    
    e.g. `DT[, `:=`(a = 1, b = 2,)`.
    
    WIP. Need to add tests and such, but editing from browser before I forget.
    
    * Another pass
    
    * include unnamed indices on RHS too
    
    * tests
    
    * NEWS
    
    * test numbering
    
    * explicit example in NEWS

--- R/data.table.R
+++ R/data.table.R
@@ -1128,7 +1128,16 @@ replace_dot_alias = function(e) {
         } else {
           # `:=`(c2=1L,c3=2L,...)
           lhs = names(jsub)[-1L]
-          if (any(lhs=="")) stopf("In %s(col1=val1, col2=val2, ...) form, all arguments must be named.", if (root == "let") "let" else "`:=`")
+          if (!all(named_idx <- nzchar(lhs))) {
+            # friendly error for common case: trailing terminal comma
+            n_lhs = length(lhs)
+            root_name <- if (root == "let") "let" else "`:=`"
+            if (!named_idx[n_lhs] && all(named_idx[-n_lhs])) {
+              stopf("In %s(col1=val1, col2=val2, ...) form, all arguments must be named, but the last argument has no name. Did you forget a trailing comma?", root_name)
+            } else {
+              stopf("In %s(col1=val1, col2=val2, ...) form, all arguments must be named, but these arguments lack names: %s.", root_name, brackify(which(!named_idx)))
+            }
+          }
           names(jsub)=""
           jsub[[1L]]=as.name("list")
         }
