b548ab2b7489f54e7a9851076b703c7b3fd1f85e
Michael Chirico <chiricom@google.com>
2024-01-15 09:00:33 -0800

    Unify logic with helper

--- R/translation.R
+++ R/translation.R
@@ -4,48 +4,35 @@ catf = function(fmt, ..., sep=" ", domain="R-data.table") {
   cat(gettextf(fmt, ..., domain=domain), sep=sep)
 }
 
+raise_condition = function(signal, message, call, classes, immediate=FALSE, appendLF=FALSE) {
+  obj = list(message=message, call=call)
+  # NB: append _after_ translation
+  if (appendLF) obj$message = paste0(obj$message, "\n")
+  setattr(obj, "class", classes)
+  # cannot set immediate.=TRUE through warning(), so use the description in ?warning to replicate this behavior ourselves
+  if (immediate) {
+    old = options(warn=1)
+    on.exit(options(old))
+  }
+  signal(obj)
+}
+
 stopf = function(fmt, ..., class=NULL, domain="R-data.table") {
-  obj = list(
-    message=gettextf(fmt, ..., domain=domain),
-    call=sys.call()
-  )
-  setattr(obj, "class", c(class, "simpleError", "error", "condition"))
-  stop(obj)
+  call = sys.call()
+  raise_condition(stop, gettextf(fmt, ..., domain=domain), call, c(class, "simpleError", "error", "condition"))
 }
 
 warningf = function(fmt, ..., immediate.=FALSE, class=NULL, domain="R-data.table") {
-  # cannot set immediate.=TRUE through warning(), so use the description in ?warning to replicate this behavior ourselves
-  if (immediate.) {
-    old = options(warn = 1)
-    on.exit(options(old))
-  }
-  obj = list(
-    message=gettextf(fmt, ..., domain=domain),
-    call=sys.call()
-  )
-  setattr(obj, "class", c(class, "simpleWarning", "warning", "condition"))
-  warning(obj)
+  call = sys.call()
+  raise_condition(warning, gettextf(fmt, ..., domain=domain), call, c(class, "simpleWarning", "warning", "condition"), immediate=immediate.)
 }
 
 messagef = function(fmt, ..., appendLF=TRUE, class=NULL, domain="R-data.table") {
-  obj = list(
-    message=gettextf(fmt, ..., domain=domain),
-    call=sys.call()
-  )
-  # NB: append _after_ translation
-  if (appendLF) obj$message <- paste0(obj$message, "\n")
-  setattr(obj, "class", c(class, "simpleMessage", "message", "condition"))
-  message(obj)
+  call = sys.call()
+  raise_condition(message, gettextf(fmt, ..., domain=domain), call, c(class, "simpleMessage", "message", "condition"), appendLF=appendLF)
 }
 
 packageStartupMessagef = function(fmt, ..., appendLF=TRUE, class=NULL, domain="R-data.table") {
-  obj = list(
-    message=gettextf(fmt, ..., domain=domain),
-    call=sys.call()
-  )
-  # NB: append _after_ translation
-  if (appendLF) obj$message <- paste0(obj$message, "\n")
-  setattr(obj, "class", c(class, "packageStartupMessage", "simpleMessage", "message", "condition"))
   # NB: packageStartupMessage() itself calls message(.packageStartupMessage(...))
-  message(obj)
+  messagef(fmt, ..., appendLF=appendLF, class=c(class, "packageStartupMessage"), domain=domain)
 }
