56228c9cd8b06d1c55d13e51676c19b66854de56
Michael Chirico <michaelchirico4@gmail.com>
2024-03-14 11:12:41 -0700

    revert methods:: change, not necessary

--- R/AllS4.R
+++ R/AllS4.R
@@ -3,21 +3,21 @@ if ("package:data.table" %in% search()) stopf("data.table package loaded. When d
 
 ## Allows data.table to be defined as an object of an S4 class,
 ## or even have data.table be a super class of an S4 class.
-methods::setOldClass(c('data.frame'))
-methods::setOldClass(c('data.table', 'data.frame'))
+setOldClass(c('data.frame'))
+setOldClass(c('data.table', 'data.frame'))
 
 ## as(some.data.frame, "data.table")
-methods::setAs("data.frame", "data.table", function(from) {
+setAs("data.frame", "data.table", function(from) {
   as.data.table(from)
 })
 
 ## as(some.data.table, "data.frame")
-methods::setAs("data.table", "data.frame", function(from) {
+setAs("data.table", "data.frame", function(from) {
   as.data.frame(from)
 })
 
-methods::setOldClass("IDate")
-methods::setOldClass("ITime")
+setOldClass("IDate")
+setOldClass("ITime")
 
-methods::setAs("character", "IDate", function(from) as.IDate(from))
-methods::setAs("character", "ITime", function(from) as.ITime(from))
+setAs("character", "IDate", function(from) as.IDate(from))
+setAs("character", "ITime", function(from) as.ITime(from))
