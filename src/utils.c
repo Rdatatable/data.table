5a4e230667d0b1149080ede380641186f55083d8
Nitish Jha <151559388+Nj221102@users.noreply.github.com>
2024-04-10 22:14:08 +0530

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -141,7 +141,7 @@ SEXP colnamesInt(SEXP x, SEXP cols, SEXP check_dups, SEXP skip_absent) {
     if(!Skip_absent){
       for (int i=0; i<nc; i++) {
         if ((icols[i]>nx) || (icols[i]<1))
-          error(_("argument specifying columns received non-existing column(s): cols[%d]=%d"), i+1, icols[i]); // handles NAs also
+         error(_("argument specifying columns received non-existing column(s): cols[%d]='%s'"), i+1, CHAR(STRING_ELT(cols, i)));
       }
     }
   } else {
