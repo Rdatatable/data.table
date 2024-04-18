28483e8f8f184ab04dbf3804ef0d70c8ebf0143a
Ani <bloodraven166@gmail.com>
2024-04-18 14:01:15 -0700

    Avoid repetition in test case titles

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -73,7 +73,7 @@ pkg.edit.fun = function(old.Package, new.Package, sha, new.pkg.path) {
 test.list <- list(
   # Performance regression discussed in: https://github.com/Rdatatable/data.table/issues/4311 
   # Fixed in: https://github.com/Rdatatable/data.table/pull/4440
-  "Test performance regression fixed in #4440" = list(
+  "Regression fixed in #4440" = list(
     pkg.edit.fun = pkg.edit.fun,
     N = 10^seq(3,8),
     setup = quote({
@@ -89,7 +89,7 @@ test.list <- list(
   # Test based on: https://github.com/Rdatatable/data.table/issues/5424
   # Performance regression introduced from a commit in: https://github.com/Rdatatable/data.table/pull/4491
   # Fixed in: https://github.com/Rdatatable/data.table/pull/5463
-  "Test performance regression fixed in #5463" = list(
+  "Regression fixed in #5463" = list(
     pkg.edit.fun = pkg.edit.fun,
     N = 10^seq(3, 8),
     setup = quote({
@@ -108,7 +108,7 @@ test.list <- list(
 
   # Issue reported in: https://github.com/Rdatatable/data.table/issues/5426
   # To be fixed in: https://github.com/Rdatatable/data.table/pull/5427
-  "Test performance improvement implemented in #5427" = list(
+  "Improvement implemented in #5427" = list(
   pkg.edit.fun = pkg.edit.fun,
   N = 10^seq(1, 7),
   setup = quote({
