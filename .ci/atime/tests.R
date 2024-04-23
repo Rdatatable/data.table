91b5de314e9674eaa5e23ca77e3408019fe75365
Ani <bloodraven166@gmail.com>
2024-04-23 15:12:03 -0700

    Titles

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -74,7 +74,7 @@ pkg.edit.fun = function(old.Package, new.Package, sha, new.pkg.path) {
 test.list <- list(
   # Performance regression discussed in: https://github.com/Rdatatable/data.table/issues/4311
   # Fixed in: https://github.com/Rdatatable/data.table/pull/4440
-  "Regression fixed in #4440" = list(
+  "shallow regression fixed in #4440" = list(
     pkg.edit.fun = pkg.edit.fun,
     N = 10^seq(3,8),
     setup = quote({
@@ -90,7 +90,7 @@ test.list <- list(
   # Test based on: https://github.com/Rdatatable/data.table/issues/5424
   # Performance regression introduced from a commit in: https://github.com/Rdatatable/data.table/pull/4491
   # Fixed in: https://github.com/Rdatatable/data.table/pull/5463
-  "Regression fixed in #5463" = list(
+  "memrecycle regression fixed in #5463" = list(
     pkg.edit.fun = pkg.edit.fun,
     N = 10^seq(3, 8),
     setup = quote({
@@ -109,7 +109,7 @@ test.list <- list(
 
   # Issue reported in: https://github.com/Rdatatable/data.table/issues/5426
   # To be fixed in: https://github.com/Rdatatable/data.table/pull/5427
-  "Test performance improvement implemented in #5427" = list(
+  "setDT improvement implemented in #5427" = list(
   pkg.edit.fun = pkg.edit.fun,
   N = 10^seq(1, 7),
   setup = quote({
