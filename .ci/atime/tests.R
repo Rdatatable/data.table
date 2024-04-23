f23abda7f11bb35ca078a35e55cd4237f2372172
Ani <bloodraven166@gmail.com>
2024-04-23 15:07:29 -0700

    Fixed the third test

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -109,14 +109,18 @@ test.list <- list(
 
   # Issue reported in: https://github.com/Rdatatable/data.table/issues/5426
   # To be fixed in: https://github.com/Rdatatable/data.table/pull/5427
-  "Improvement implemented in #5427" = list(
-    pkg.edit.fun = pkg.edit.fun,
-    N = 10^seq(1, 7),
-    setup = quote({
-      DT = replicate(N, 1, simplify = FALSE)
-    }),
-    expr = quote(data.table:::setDT(DT)),
-    Slow = "c4a2085e35689a108d67dacb2f8261e4964d7e12", # Parent of the first commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/commit/7cc4da4c1c8e568f655ab5167922dcdb75953801)
-    Fast = "1872f473b20fdcddc5c1b35d79fe9229cd9a1d15") # Last commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/pull/5427/commits)
+  "Test performance improvement implemented in #5427" = list(
+  pkg.edit.fun = pkg.edit.fun,
+  N = 10^seq(1, 7),
+  setup = quote({
+    L <- replicate(N, 1, simplify = FALSE)
+    setDT(L)
+  }),
+  expr = quote({
+    data.table:::setattr(L, "class", NULL)
+    data.table:::setDT(L)
+  }),
+  Slow = "c4a2085e35689a108d67dacb2f8261e4964d7e12", # Parent of the first commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/commit/7cc4da4c1c8e568f655ab5167922dcdb75953801)
+  Fast = "1872f473b20fdcddc5c1b35d79fe9229cd9a1d15") # Last commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/pull/5427/commits)
 )
 # nolint end: undesirable_operator_linter.
