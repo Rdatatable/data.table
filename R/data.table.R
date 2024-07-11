89a22b3f8286e8211e8058a9630840a117f3e249
Cole Miller <cole.miller42@gmail.com>
2024-07-10 21:44:54 -0400

    fix names_sd rev dev issue 6033

--- R/data.table.R
+++ R/data.table.R
@@ -1122,6 +1122,11 @@ replace_dot_alias = function(e) {
           if (is.name(lhs)) {
             lhs = as.character(lhs)
           } else {
+            #6033 revdev. Slowly deprecate in 1.17. Caller has given us `dt[, substitute(names(.SD))]` which means 
+            # jsub is actually substitute(names(.SD)) instead of just names(.SD)
+            if (lhs %iscall% 'substitute') 
+              lhs = eval(lhs, parent.frame(), parent.frame())
+
             # lhs is e.g. (MyVar) or get("MyVar") or names(.SD) || setdiff(names(.SD), cols)
             lhs = eval(lhs, list(.SD = setNames(logical(length(sdvars)), sdvars)), parent.frame())
           }
