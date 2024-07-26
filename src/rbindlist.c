1784104caa7c6bbe7291b50035714ced8b07815b
Michael Chirico <michaelchirico4@gmail.com>
2024-07-25 21:37:42 -0700

    a clean a tempt at PROTECT() inside if

--- src/rbindlist.c
+++ src/rbindlist.c
@@ -536,16 +536,11 @@ SEXP rbindlist(SEXP l, SEXP usenamesArg, SEXP fillArg, SEXP idcolArg, SEXP ignor
         } else {
           bool listprotect = (TYPEOF(target)==VECSXP || TYPEOF(target)==EXPRSXP) && TYPEOF(thisCol)!=TYPEOF(target);
           // do an as.list() on the atomic column; #3528
-          if (listprotect) {
-            thisCol = PROTECT(coerceVector(thisCol, TYPEOF(target)));
-            // else coerces if needed within memrecycle; with a no-alloc direct coerce from 1.12.4 (PR #3909)
-            const char *ret = memrecycle(target, R_NilValue, ansloc, thisnrow, thisCol, 0, -1, idcol+j+1, foundName);
-            UNPROTECT(1); // earlier unprotect rbindlist calls with lots of lists #4536
-            if (ret) warning(_("Column %d of item %d: %s"), w+1, i+1, ret);
-          } else {
-            const char *ret = memrecycle(target, R_NilValue, ansloc, thisnrow, thisCol, 0, -1, idcol+j+1, foundName);
-            if (ret) warning(_("Column %d of item %d: %s"), w+1, i+1, ret);
-          }
+          if (listprotect) thisCol = PROTECT(coerceVector(thisCol, TYPEOF(target)));
+          // else coerces if needed within memrecycle; with a no-alloc direct coerce from 1.12.4 (PR #3909)
+          const char *ret = memrecycle(target, R_NilValue, ansloc, thisnrow, thisCol, 0, -1, idcol+j+1, foundName);
+          if (listprotect) UNPROTECT(1); // earlier unprotect rbindlist calls with lots of lists #4536
+          if (ret) warning(_("Column %d of item %d: %s"), w+1, i+1, ret);
           // e.g. when precision is lost like assigning 3.4 to integer64; test 2007.2
           // TODO: but maxType should handle that and this should never warn
         }
