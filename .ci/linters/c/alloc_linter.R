6ed7ab9689b1ce907a2a7be590aed3651ef9f386
Michael Chirico <michaelchirico4@gmail.com>
2024-08-02 22:27:36 -0700

    refactor

--- .ci/linters/c/alloc_linter.R
+++ .ci/linters/c/alloc_linter.R
@@ -2,8 +2,8 @@
 # More specifically, given that this is an AST-ignorant checker,
 #   1. Find groups of malloc()/calloc() calls
 #   2. Check the next line for a check like 'if (!x || !y)'
-alloc_linter = function(c_file) {
-  lines <- readLines(c_file)
+alloc_linter = function(c_obj) {
+  lines <- c_obj$lines
   # Be a bit more precise to avoid mentions in comments
   alloc_lines <- grep(R"{=\s*([(]\w+\s*[*][)])?[mc]alloc[(]}", lines)
   if (!length(alloc_lines)) return()
@@ -27,7 +27,7 @@ alloc_linter = function(c_file) {
     #   but not immediately, so use 'NOCHECK' to escape.
     if (!grepl(check_regex, check_line) && !grepl("NOCHECK", check_line, fixed=TRUE)) {
       bad_lines_idx <- seq(alloc_lines[grp_idx[1L]], length.out=length(grp_idx)+1L)
-      cat("FILE: ", c_file, "; LINES: ", head(bad_lines_idx, 1L), "-", tail(bad_lines_idx, 1L), "\n", sep="")
+      cat("FILE: ", c_obj$path, "; LINES: ", head(bad_lines_idx, 1L), "-", tail(bad_lines_idx, 1L), "\n", sep="")
       writeLines(lines[bad_lines_idx])
       cat(strrep("-", max(nchar(lines[bad_lines_idx]))), "\n", sep="")
       stop("Expected the malloc()/calloc() usage above to be followed immediately by error checking.", call.=FALSE)
