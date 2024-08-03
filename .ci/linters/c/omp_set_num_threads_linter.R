40d4fc59501a51aaf4730a1ba9a52cbcf52982f4
Michael Chirico <michaelchirico4@gmail.com>
2024-08-03 05:01:19 +0000

    Add a linter for calls to omp_set_num_threads

---
+++ .ci/linters/c/omp_set_num_threads_linter.R
@@ -0,0 +1,11 @@
+# Ensure no calls to omp_set_num_threads() [to avoid affecting other packages and base R]
+# Only comments referring to it should be in openmp-utils.c
+omp_set_num_threads_linter = function(c_file) {
+  processed_lines = system2("gcc", c("-fpreprocessed", "-E", "-xc", "-"), stdin=c_file, stdout=TRUE, stderr=FALSE)
+  idx = grep("omp_set_num_threads", processed_lines, fixed = TRUE)
+  if (!length(idx)) return()
+  stop(sprintf(
+    "In %s, found 'omp_set_num_threads() usage, which could affect other packages and base R:\n%s",
+    c_file, paste0("  ", format(idx), ":", processed_lines[idx], collapse = "\n")
+  ))
+}
