0bc874cc728c0c650acbf3932d793dae60c7537e
Michael Chirico <chiricom@google.com>
2024-08-05 14:04:07 -0700

    Add a NEWS linter for checking GitHub link consistency (#6343)
    * New md linter for GitHub link consistency
    
    * copy change over
    
    * fixed bad refactor
    
    Co-authored-by: Benjamin Schwendinger <52290390+ben-schwen@users.noreply.github.com>
    
    * fix cont'd
    
    Co-authored-by: Benjamin Schwendinger <52290390+ben-schwen@users.noreply.github.com>
    
    * helpful comment
    
    * better programming -> obviate need for names(); better naming
    
    ---------
    
    Co-authored-by: Benjamin Schwendinger <52290390+ben-schwen@users.noreply.github.com>

--- .ci/linters/md/news_linter.R
+++ .ci/linters/md/news_linter.R
@@ -25,10 +25,37 @@ check_section_numbering = function(news) {
   return(any_mismatch)
 }
 
+# ensure that GitHub link text & URL actually agree
+check_gh_links = function(news) {
+  gh_links_info = gregexpr(
+    "\\[#(?<md_number>[0-9]+)\\]\\(https://github.com/Rdatatable/data.table/(?<link_type>[^/]+)/(?<link_number>[0-9]+)\\)",
+    news,
+    perl=TRUE # required for within-group indices
+  )
+  gh_link_metadata = do.call(rbind, lapply(seq_along(gh_links_info), function(idx) {
+    x = gh_links_info[[idx]]
+    if (x[1L] <= 0L) return(NULL)
+    match_mat = attr(x, "capture.start") # matrix seeded with the correct dimensions
+    match_mat[] = substring(news[idx], match_mat, match_mat + attr(x, "capture.length") - 1L)
+    match_df = data.frame(match_mat)
+    match_df$line_number = idx
+    match_df
+  }))
+  matched = gh_link_metadata$md_number == gh_link_metadata$link_number
+  if (all(matched)) return(FALSE)
+
+  cat(sep = "", with(gh_link_metadata[!matched, ], sprintf(
+    "In line %d, link pointing to %s %s is written #%s\n",
+    line_number, link_type, link_number, md_number
+  )))
+  return(TRUE)
+}
+
 any_error = FALSE
 for (news in list.files(pattern = "NEWS")) {
   cat(sprintf("Checking NEWS file %s...\n", news))
   news_lines = readLines("NEWS.md")
   any_error = any_error || check_section_numbering(news_lines)
+  any_error = any_error || check_gh_links(news_lines)
 }
 if (any_error) stop("Please fix the NEWS issues above.")
