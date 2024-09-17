3b376dab6092c67dead7197c858f450109cb519b
aitap <krylov.r00t@gmail.com>
2024-09-17 15:39:53 +0000

    Use internal_error() instead of manual error handling
    Co-authored-by: Michael Chirico <chiricom@google.com>

--- src/fmelt.c
+++ src/fmelt.c
@@ -804,7 +804,7 @@ SEXP fmelt(SEXP DT, SEXP id, SEXP measure, SEXP varfactor, SEXP valfactor, SEXP 
   int protecti=0;
   dtnames = PROTECT(getAttrib(DT, R_NamesSymbol)); protecti++;
   if (isNull(dtnames))
-    error(_("names(data) is NULL. Please report to the data.table issue tracker"));
+    internal_error("names(data) is NULL");
   if (LOGICAL(narmArg)[0] == TRUE) narm = TRUE;
   if (LOGICAL(verboseArg)[0] == TRUE) verbose = TRUE;
   struct processData data;
