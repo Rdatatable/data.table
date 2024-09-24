7bb4862a8cf042c7f285e1a67d7882a153d4ad8b
Toby Dylan Hocking <tdhock5@gmail.com>
2024-09-23 22:28:06 -0400

    patterns() throws new informative error when cols is non-character or has NA (#6511)
    Co-authored-by: Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
    Co-authored-by: Michael Chirico <chiricom@google.com>

--- R/fmelt.R
+++ R/fmelt.R
@@ -22,6 +22,9 @@ melt.default = function(data, ..., na.rm = FALSE, value.name = "value") {
 patterns = function(..., cols=character(0L), ignore.case=FALSE, perl=FALSE, fixed=FALSE, useBytes=FALSE) {
   # if ... has no names, names(list(...)) will be "";
   #   this assures they'll be NULL instead
+  if (!is.character(cols) || anyNA(cols)) {
+    stopf("cols must be a character vector of column names")
+  }
   L = list(...)
   p = unlist(L, use.names = any(nzchar(names(L))))
   if (!is.character(p))
