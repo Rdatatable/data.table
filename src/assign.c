71ac34a429870f18d583cff4ace193e2d8c90612
Michael Chirico <chiricom@google.com>
2024-10-31 16:54:41 +0000

    Revert "Don't warn about numeric coercion in set(j=)"
    This reverts commit f6c138575f5157bebde0ff0c22601cd1ec983bd4.

--- src/assign.c
+++ src/assign.c
@@ -425,8 +425,7 @@ SEXP assign(SEXP dt, SEXP rows, SEXP cols, SEXP newcolnames, SEXP values)
   } else {
     if (isReal(cols)) {
       cols = PROTECT(coerceVector(cols, INTSXP)); protecti++;
-      if (verbose)
-        Rprintf(_("Coerced j from numeric to integer. Please pass integer for efficiency; e.g., 2L rather than 2"));
+      warning(_("Coerced j from numeric to integer. Please pass integer for efficiency; e.g., 2L rather than 2"));
     }
     if (!isInteger(cols))
       error(_("j is type '%s'. Must be integer, character, or numeric is coerced with warning."), type2char(TYPEOF(cols)));
