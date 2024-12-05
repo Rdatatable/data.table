7c217f8a2d1d223d1aad95d0ac9d01d070f3a1d8
Benjamin Schwendinger <benjaminschwe@gmail.com>
2024-12-05 23:54:58 +0100

    remove unused function

--- src/utils.c
+++ src/utils.c
@@ -449,12 +449,3 @@ void internal_error(const char *call_name, const char *format, ...) {
 
   error("%s %s: %s. %s", _("Internal error in"), call_name, buff, _("Please report to the data.table issues tracker."));
 }
-
-SEXP setDT(SEXP x) {
-  // used by gsumm.c to return a barebones DT (e.g. no names, not over-allocated) to gforce when n is a vector of shifts
-  SEXP class;
-  setAttrib(x, R_ClassSymbol, class=allocVector(STRSXP, 2));
-  SET_STRING_ELT(class,0,char_datatable);
-  SET_STRING_ELT(class,1,char_dataframe);
-  return x;
-}
