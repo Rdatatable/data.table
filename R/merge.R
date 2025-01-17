771fbc0b45ba6670c0cf08daa37d5416ada6531a
venom1204 <venomplays1204@gmail.com>
2025-01-17 12:31:30 +0530

    introduced teh latest changes

--- R/merge.R
+++ R/merge.R
@@ -11,42 +11,44 @@ merge.data.table = function(x, y, by = NULL, by.x = NULL, by.y = NULL, all = FAL
       by = key(x)
     }
   }
-  x0 = length(x)==0L
-  y0 = length(y)==0L
-  if (x0 || y0) {
-    if (x0 && y0)
-      warningf("Neither of the input data.tables to join have columns.")
-    else if (x0)
-      warningf("Input data.table x has no columns.")
-    else
-      warningf("Input data.table y has no columns.")
-  }
-  check_duplicate_names(x)
-  check_duplicate_names(y)
+  x0 = length(x) == 0L
+  y0 = length(y) == 0L
 
-  nm_x = names(x)
-  nm_y = names(y)
+  if (x0 || y0) { 
+    if (x0 && y0) {
+      warningf("Neither of the input data.tables to join have columns.")
+    } else if (x0) {
+      warningf("Input data.table '%s' has no columns.", "x")
+    } else if (y0) {
+      warningf("Input data.table '%s' has no columns.", "y")
+    }
+  }
+
+check_duplicate_names(x)
+check_duplicate_names(y)
+
+nm_x = names(x)
+nm_y = names(y)
+
 
   ## set up 'by'/'by.x'/'by.y'
   if ( (!is.null(by.x) || !is.null(by.y)) && length(by.x)!=length(by.y) )
-    stopf("by.x and by.y must be of same length.")
+    stopf("'by.x' and 'by.y' must be of same length.")
   if (!missing(by) && !missing(by.x))
-    warningf("Supplied both by and by.x/by.y. by argument will be ignored.")
+    warningf("Supplied both by and 'by.x/by.y.' by argument will be ignored.")
   if (!is.null(by.x)) {
     if (length(by.x)==0L || !is.character(by.x) || !is.character(by.y))
-      stopf("A non-empty vector of column names is required for by.x and by.y.")
+      stopf("A non-empty vector of column names is required for 'by.x' and 'by.y'.")
     if (!all(by.x %chin% nm_x)) {
-      missing_in_x <- setdiff(by.x, nm_x)
-      stopf("The following columns listed in by.x are missing from x: %s",
-            toString(missing_in_x))
-    }
-    if (!all(by.y %chin% nm_y)) {
-      missing_in_y <- setdiff(by.y, nm_y)
-      stopf("The following columns listed in by.y are missing from y: %s",
-            toString(missing_in_y))
-    }
-    by = by.x
-    names(by) = by.y
+  missing_in_x <- setdiff(by.x, nm_x)
+  stopf("The following columns listed in 'by.x' are missing from x: %s", brackify(missing_in_x))
+  }
+  if (!all(by.y %chin% nm_y)) {
+     missing_in_y <- setdiff(by.y, nm_y)
+     stopf("The following columns listed in 'by.y' are missing from y: %s", brackify(missing_in_y))
+  }
+by = by.x
+names(by) = by.y
   } else {
     if (is.null(by))
       by = intersect(key(x), key(y))
@@ -59,14 +61,13 @@ merge.data.table = function(x, y, by = NULL, by.x = NULL, by.y = NULL, all = FAL
     missing_in_x <- setdiff(by, nm_x)
     missing_in_y <- setdiff(by, nm_y)
     if (length(missing_in_x) > 0 || length(missing_in_y) > 0) {
-      stopf("The following columns are missing:%s%s",
-            if (length(missing_in_x) > 0) sprintf(" - From x: %s", toString(missing_in_x)) else "",
-            if (length(missing_in_y) > 0) sprintf(" - From y: %s", toString(missing_in_y)) else "")
+      stopf(gettextf("The following columns are missing:\n%s%s",
+                     if (length(missing_in_x) > 0) gettextf(" - From x: %s\n", brackify(missing_in_x)) else "",
+                     if (length(missing_in_y) > 0) gettextf(" - From y: %s\n", brackify(missing_in_y)) else ""))
     }
     by = unname(by)
     by.x = by.y = by
-  }
-
+   }
   # warn about unused arguments #2587
   if (length(list(...))) {
     ell = as.list(substitute(list(...)))[-1L]
