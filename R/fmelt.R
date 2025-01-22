e12f726d4bd761302f38b11c70c60412c6b41fb5
nipun-gupta-3108 <nipungupta082006@gmail.com>
2025-01-23 01:22:20 +0530

    Refactored duplicated logic in setDT and [,:=] to use a helper function

--- R/fmelt.R
+++ R/fmelt.R
@@ -182,6 +182,12 @@ melt.data.table = function(data, id.vars, measure.vars, variable.name = "variabl
        value.name = "value", ..., na.rm = FALSE, variable.factor = TRUE, value.factor = FALSE,
        verbose = getOption("datatable.verbose")) {
   if (!is.data.table(data)) stopf("'data' must be a data.table")
+
+  # Validate id.vars
+  if (any(is.na(id.vars) | id.vars == "")) {
+    stopf("One or more values in 'id.vars' is invalid.")
+  }
+
   if (missing(id.vars)) id.vars=NULL
   if (missing(measure.vars)) measure.vars = NULL
   measure.sub = substitute(measure.vars)
