f4516b478ace6d9d7b8d749b56064247a1e0c487
Michael Chirico <michaelchirico4@gmail.com>
2025-01-29 14:10:06 -0800

    Revert "fully normalize useFancyQuotes"
    This reverts commit 8d50c3ada1c0cddb2af0c7966974332cdae84d6c.

--- R/test.data.table.R
+++ R/test.data.table.R
@@ -101,7 +101,6 @@ test.data.table = function(script="tests.Rraw", verbose=FALSE, pkg=".", silent=F
     datatable.rbindlist.check = NULL,
     datatable.integer64 = "integer64",
     digits = 7L, # ensure printing rounds to the expected number of digits in all sessions, #5285
-    useFancyQuotes = FALSE, # ensure ASCII ' and " are always produced from base
     warn = 0L,   # ensure signals are emitted as they are in the code, #5285
     warnPartialMatchArgs = base::getRversion()>="3.6.0", # ensure we don't rely on partial argument matching in internal code, #3664; >=3.6.0 for #3865
     warnPartialMatchAttr = TRUE,
