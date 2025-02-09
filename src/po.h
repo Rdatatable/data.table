39317bb38086195961684437fd544898bb6a9be9
pradeep dhane <pradeepdhane3@gmail.com>
2025-02-09 23:40:41 +0530

    solving issue The Pl_() translation macro was not recognized by
    issue no-6794. solving this issue

--- src/po.h
+++ src/po.h
@@ -1,9 +1,10 @@
 #ifdef ENABLE_NLS
 #include <libintl.h>
 #define _(String) dgettext("data.table", String)
-// NB: flip argument order to match that of R's ngettext()
 #define Pl_(n, String1, StringPlural) dngettext("data.table", String1, StringPlural, n)
+#define Pl_Extract(String1, StringPlural) (String1 "\0" StringPlural) // Helps extract both strings
 #else
 #define _(String) (String)
 #define Pl_(n, String1, StringPlural) ((n) == 1 ? (String1) : (StringPlural))
+#define Pl_Extract(String1, StringPlural) (String1 "\0" StringPlural) // Consistent behavior
 #endif
