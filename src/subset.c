fe159550cb13fccaf35c4c530dc7abd6a47b02f2
Ivan K <krylov.r00t@gmail.com>
2025-03-01 21:28:50 +0300

    Replace GetOption with GetOption1
    GetOption1() has been available since R-2.13, and GetOption() has been
    ignoring its second argument for a long time, so there's no portability
    cost.

--- src/subset.c
+++ src/subset.c
@@ -296,7 +296,7 @@ SEXP subsetDT(SEXP x, SEXP rows, SEXP cols) { // API change needs update NEWS.md
     if (this<1 || this>LENGTH(x)) error(_("Item %d of cols is %d which is outside the range [1,ncol(x)=%d]"), i+1, this, LENGTH(x));
   }
 
-  int overAlloc = checkOverAlloc(GetOption(install("datatable.alloccol"), R_NilValue));
+  int overAlloc = checkOverAlloc(GetOption1(install("datatable.alloccol")));
   SEXP ans = PROTECT(allocVector(VECSXP, LENGTH(cols)+overAlloc)); nprotect++;  // doing alloc.col directly here; eventually alloc.col can be deprecated.
 
   // user-defined and superclass attributes get copied as from v1.12.0
