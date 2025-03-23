337a0c2d508a31c59885416d7929ff6d6a4b0bda
Benjamin Schwendinger <benjaminschwe@gmail.com>
2025-03-23 19:44:41 +0100

    really use lookup or insert

--- src/chmatch.c
+++ src/chmatch.c
@@ -59,7 +59,7 @@ static SEXP chmatchMain(SEXP x, SEXP table, int nomatch, bool chin, bool chmatch
   int nuniq=0;
   for (int i=0; i<tablelen; ++i) {
     const SEXP s = td[i];
-    hash_set(marks, s, chmatchdup ? -(++nuniq) : -i-1); // first time seen this string in table
+    hash_lookup_or_insert(marks, s, chmatchdup ? -(++nuniq) : -i-1); // first time seen this string in table
   }
   // in future if we need NAs in x not to be matched to NAs in table ...
   if (chmatchdup) {
