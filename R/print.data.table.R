baa55efa1b7054f06093629a7e1758a61ce7fe7f
Mukul Kumar <mukulrao6480@gmail.com>
2025-04-12 19:47:42 +0530

    add column class at bottom if row > 20

--- R/print.data.table.R
+++ R/print.data.table.R
@@ -142,7 +142,15 @@ print.data.table = function(x, topn=getOption("datatable.print.topn"),
   if (nrow(toprint)>20L && col.names == "auto")
     # repeat colnames at the bottom if over 20 rows so you don't have to scroll up to see them
     #   option to shut this off per request of Oleg Bondar on SO, #1482
-    toprint = rbind(toprint, matrix(if (quote) old else colnames(toprint), nrow=1L)) # fixes bug #97
+    if (isTRUE(class)) {
+      # Add both column names and classes when classes are enabled
+      mat_abbs <- matrix(abbs, nrow = 1L)
+      rownames(mat_abbs) <- ""
+      toprint <- rbind(toprint, matrix(if (quote) old else colnames(toprint), nrow=1L), matrix(abbs, nrow=1L))
+    } else {
+      # Original behavior (just column names at bottom)
+      toprint <- rbind(toprint, matrix(if (quote) old else colnames(toprint), nrow=1L))
+    }
   print_default(toprint)
   invisible(x)
 }
