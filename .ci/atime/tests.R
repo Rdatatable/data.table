73efaf39e543310c229ebbda2404a7ba2ea038b0
Michael Chirico <chiricom@google.com>
2025-07-16 15:05:54 -0700

    missing 'data.table::'

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -316,7 +316,7 @@ test.list <- atime::atime_test_list(
         set.seed(34893)
         DT = data.table(i=N:1)
       },
-      expr = for (ii in seq_len(nrow(DT))) set(DT, ii, "i", ii),
+      expr = for (ii in seq_len(nrow(DT))) data.table::set(DT, ii, "i", ii),
       `Status quo` = "8f5ffa8bb8f3f5861020a6e32f897c30e42eeab0"), # Parent of the first commit in the PR (https://github.com/Rdatatable/data.table/commit/8f5ffa8bb8f3f5861020a6e32f897c30e42eeab0)
 
     tests=extra.test.list)
