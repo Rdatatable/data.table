e2e0173aa8e99282c3fad7a2b2f7095a6b1820cf
Benjamin Schwendinger <52290390+ben-schwen@users.noreply.github.com>
2025-08-06 17:45:41 +0200

    Add forceDecimal parameter to fwrite (#7212)
    * add new feature
    
    * add more tests
    
    * add const qualifier
    
    * Use local for clearer test
    
    * dec= test
    
    * Update man/fwrite.Rd
    
    Co-authored-by: Michael Chirico <chiricom@google.com>
    
    * nocov
    
    * oops
    
    * test #ing
    
    ---------
    
    Co-authored-by: Michael Chirico <chiricom@google.com>

--- R/fwrite.R
+++ R/fwrite.R
@@ -13,7 +13,8 @@ fwrite = function(x, file="", append=FALSE, quote="auto",
            yaml = FALSE,
            bom = FALSE,
            verbose=getOption("datatable.verbose", FALSE),
-           encoding = "") {
+           encoding = "",
+           forceDecimal = FALSE) {
   na = as.character(na[1L]) # fix for #1725
   if (length(encoding) != 1L || !encoding %chin% c("", "UTF-8", "native")) {
     stopf("Argument 'encoding' must be '', 'UTF-8' or 'native'.")
@@ -51,7 +52,7 @@ fwrite = function(x, file="", append=FALSE, quote="auto",
     length(compressLevel) == 1L && 0L <= compressLevel && compressLevel <= 9L,
     isTRUEorFALSE(col.names), isTRUEorFALSE(append), isTRUEorFALSE(row.names),
     isTRUEorFALSE(verbose), isTRUEorFALSE(showProgress), isTRUEorFALSE(logical01),
-    isTRUEorFALSE(bom),
+    isTRUEorFALSE(bom), isTRUEorFALSE(forceDecimal),
     length(na) == 1L, #1725, handles NULL or character(0) input
     is.character(file) && length(file)==1L && !is.na(file),
     length(buffMB)==1L && !is.na(buffMB) && 1L<=buffMB && buffMB<=1024L,
@@ -122,7 +123,7 @@ fwrite = function(x, file="", append=FALSE, quote="auto",
   }
   .Call(CfwriteR, x, file, sep, sep2, eol, na, dec, quote, qmethod=="escape", append,
         row.names, col.names, logical01, scipen, dateTimeAs, buffMB, nThread,
-        showProgress, is_gzip, compressLevel, bom, yaml, verbose, encoding)
+        showProgress, is_gzip, compressLevel, bom, yaml, verbose, encoding, forceDecimal)
   invisible()
 }
 
