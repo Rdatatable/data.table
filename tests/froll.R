00bc3b98963bef41c117eac98ac17ba77cf5071f
Benjamin Schwendinger <benjaminschwe@gmail.com>
2025-11-18 12:29:44 +0100

    add optional parameter and move batch tests

--- tests/froll.R
+++ tests/froll.R
@@ -1,3 +1,2 @@
 require(data.table)
-minor_version = as.integer(strsplit(packageDescription("data.table")$Version, "\\.")[[1]][[3]])
-if (minor_version == 99L || Sys.getenv("RunAllDataTableTests") == "yes") test.data.table(script="froll.Rraw")
+test.data.table(script="froll.Rraw", optional=TRUE)
