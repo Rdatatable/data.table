6f8020f805548e36610ec1130781ca1d20a4c83a
Benjamin Schwendinger <benjaminschwe@gmail.com>
2025-12-23 13:44:30 +0100

    add assign and tests

--- R/data.table.R
+++ R/data.table.R
@@ -2849,12 +2849,11 @@ setcolorder = function(x, neworder=key(x), before=NULL, after=NULL, skip_absent=
 set = function(x,i=NULL,j,value)  # low overhead, loopable
 {
   # If removing columns from a table that's not selfrefok, need to call setalloccol first, #7488
-  if (is.null(value) || (is.list(value) && any(vapply_1b(value, is.null)))) {
-    # Removing at least one column
-    ok = selfrefok(x, verbose=FALSE)
-    if (ok < 1L) {
-      # Table is not safe to resize (either fresh from disk or shallow copy)
-      setalloccol(x, n=eval(getOption("datatable.alloccol")), verbose=FALSE)
+  if ((is.null(value) || (is.list(value) && any(vapply_1b(value, is.null)))) && selfrefok(x, verbose=FALSE) < 1L) {
+    name = substitute(x)
+    setalloccol(x, verbose=FALSE)
+    if (is.name(name)) {
+      assign(as.character(name), x, parent.frame(), inherits=TRUE)
     }
   }
   .Call(Cassign,x,i,j,NULL,value)
