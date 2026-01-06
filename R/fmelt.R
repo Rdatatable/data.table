5bb9ff4a5d06d9456fe721f18d114e19c2752b5a
Ayush Singh <rawatsinghayush9720@gmail.com>
2026-01-06 15:34:10 +0000

    Final Fix: Remove extra brackets and clean test syntax

--- R/fmelt.R
+++ R/fmelt.R
@@ -197,16 +197,16 @@ melt.data.table = function(data, id.vars, measure.vars, variable.name = "variabl
   }
 
 # Fix for #6512: check for invalid measure.vars
-  check.vars = if (is.list(measure.vars)) unlist(measure.vars) else measure.vars
+check.vars = if (is.list(measure.vars)) unlist(measure.vars) else measure.vars
 
-  if (is.character(check.vars)) {
-    check.vars = check.vars[!is.na(check.vars)]
-    invalid_vars = setdiff(check.vars, names(data))
-    if (length(invalid_vars)) {
-      stopf("One or more values in 'measure.vars' is invalid; please fix by removing %s",
-            brackify(invalid_vars))
-    }
+if (is.character(check.vars)) {
+  check.vars = check.vars[!is.na(check.vars)]
+  invalid_vars = setdiff(check.vars, names(data))
+  if (length(invalid_vars)) {
+    stopf("One or more values in 'measure.vars' is invalid; please fix by removing %s",
+          brackify(invalid_vars))
   }
+}
 
   if (is.list(measure.vars)) {
     meas.nm = names(measure.vars)
