72d2b9799c11d55ea4ec077bdc0733b99065508e
Ivan K <krylov.r00t@gmail.com>
2026-01-15 00:18:32 +0300

    Add <link> tags for favicon

--- site/generate_reference.R
+++ site/generate_reference.R
@@ -72,6 +72,7 @@ for (rd_file in rd_files) {
     'table { border-collapse: collapse; }',
     'table td, table th { border: 1px solid #dee2e6; padding: 0.5rem; }',
     '</style>',
+    '<link rel="icon" type="image/png" sizes="16x16" href="../assets/favicon/favicon-16x16.png"><link rel="icon" type="image/png" sizes="32x32" href="../assets/favicon/favicon-32x32.png"><link rel="apple-touch-icon" sizes="60x60" href="../assets/favicon/apple-touch-icon-60x60.png"><link rel="apple-touch-icon" sizes="76x76" href="../assets/favicon/apple-touch-icon-76x76.png"><link rel="apple-touch-icon" sizes="120x120" href="../assets/favicon/apple-touch-icon-120x120.png"><link rel="apple-touch-icon" sizes="152x152" href="../assets/favicon/apple-touch-icon-152x152.png"><link rel="apple-touch-icon" sizes="180x180" href="../assets/favicon/apple-touch-icon-180x180.png"><link rel="apple-touch-icon" sizes="180x180" href="../assets/favicon/apple-touch-icon.png"><link rel="icon" sizes="any" href="../assets/favicon/favicon.ico">',
     '</head>',
     '<body>',
     navbar_html,
