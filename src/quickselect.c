6d94f8cd3a2efdea056d6798952743cc7955888b
Benjamin Schwendinger <benjaminschwe@gmail.com>
2026-01-17 17:14:17 +0100

    add missed recursive call

--- src/quickselect.c
+++ src/quickselect.c
@@ -75,52 +75,63 @@ double i64quickselect(int64_t *x, int n)
 // Floyd-Rivest selection algorithm
 // https://en.wikipedia.org/wiki/Floyd%E2%80%93Rivest_algorithm
 
+#undef DEFINE_FLOYD_RIVEST_SELECT_FN
+#define DEFINE_FLOYD_RIVEST_SELECT_FN(SELECT_FN, TYPE, SWAP)               \
+  static void SELECT_FN(TYPE *x, unsigned long left, unsigned long right, unsigned long k) { \
+    while (right > left) {                                                 \
+      if (right - left > 600) {                                            \
+        unsigned long n = right - left + 1;                                \
+        unsigned long i = k - left + 1;                                    \
+        double z = log((double)n);                                         \
+        double s = 0.5 * exp(2.0 * z / 3.0);                               \
+        double sd = 0.5 * sqrt(z * s * (n - s) / n);                       \
+        long delta = (long)i - (long)(n / 2);                              \
+        if (delta < 0) sd = -sd;                                           \
+        else if (delta == 0) sd = 0.0;                                     \
+        double new_left = (double)k - (double)i * s / n + sd;              \
+        double new_right = (double)k + (double)(n - i) * s / n + sd;       \
+        unsigned long newL = (unsigned long)MAX((long)left, (long)new_left); \
+        unsigned long newR = (unsigned long)MIN((long)right, (long)new_right); \
+        SELECT_FN(x, newL, newR, k);                                       \
+      }                                                                    \
+      TYPE pivot = x[k];                                                   \
+      unsigned long i = left, j = right;                                   \
+      SWAP(x + left, x + k);                                               \
+      if (x[right] > pivot) {                                              \
+        SWAP(x + right, x + left);                                         \
+      }                                                                    \
+      while (i < j) {                                                      \
+        SWAP(x + i, x + j);                                                \
+        i++; j--;                                                          \
+        while (x[i] < pivot) i++;                                          \
+        while (x[j] > pivot) j--;                                          \
+      }                                                                    \
+      if (x[left] == pivot) {                                              \
+        SWAP(x + left, x + j);                                             \
+      } else {                                                             \
+        j++;                                                               \
+        SWAP(x + j, x + right);                                            \
+      }                                                                    \
+      if (j <= k) left = j + 1;                                            \
+      if (k <= j) right = j - 1;                                           \
+    }                                                                      \
+  }
+
+DEFINE_FLOYD_RIVEST_SELECT_FN(dfloyd_rivest_select, double, dswap)
+DEFINE_FLOYD_RIVEST_SELECT_FN(ifloyd_rivest_select, int, iswap)
+DEFINE_FLOYD_RIVEST_SELECT_FN(i64floyd_rivest_select, int64_t, i64swap)
+
 #undef FLOYD_RIVEST_BODY
-#define FLOYD_RIVEST_BODY(SWAP, TYPE)                                      \
+#define FLOYD_RIVEST_BODY(TYPE, SELECTFN)                                  \
   if (n == 0) return NA_REAL;                                              \
   unsigned long med = n / 2 - (n % 2 == 0);                                \
-  unsigned long l = 0, ir = n - 1;                                         \
-  while (ir > l) {                                                         \
-    if (ir - l > 600) {                                                    \
-      unsigned long nn = ir - l + 1;                                       \
-      unsigned long i = med - l + 1;                                       \
-      double z = log((double)nn);                                          \
-      double s = 0.5 * exp(2.0*z/3.0);                                     \
-      double sd = 0.5 * sqrt(z*s*(nn-s)/nn);                               \
-      if (i < nn/2) sd = -sd;                                              \
-      unsigned long newL = (unsigned long)(MAX((long)l, (long)(med - i*s/nn + sd))); \
-      unsigned long newR = (unsigned long)(MIN((long)ir, (long)(med + (nn-i)*s/nn + sd))); \
-      l = newL;                                                            \
-      ir = newR;                                                           \
-    }                                                                      \
-    TYPE pivot = x[med];                                                   \
-    unsigned long i = l, j = ir;                                           \
-    SWAP(x + l, x + med);                                                  \
-    if (x[ir] > pivot) {                                                   \
-      SWAP(x + ir, x + l);                                                 \
-      pivot = x[l];                                                        \
-    }                                                                      \
-    while (i < j) {                                                        \
-      SWAP(x + i, x + j);                                                  \
-      i++; j--;                                                            \
-      while (x[i] < pivot) i++;                                            \
-      while (x[j] > pivot) j--;                                            \
-    }                                                                      \
-    if (x[l] == pivot) {                                                   \
-      SWAP(x + l, x + j);                                                  \
-    } else {                                                               \
-      j++;                                                                 \
-      SWAP(x + j, x + ir);                                                 \
-    }                                                                      \
-    if (j <= med) l = j + 1;                                               \
-    if (med <= j) ir = j - 1;                                              \
-  }                                                                        \
+  SELECTFN(x, 0, n - 1, med);                                              \
   a = x[med];                                                              \
   if (n % 2 == 1) {                                                        \
     return (double)a;                                                      \
   } else {                                                                 \
     b = x[med + 1];                                                        \
-    for (unsigned long i = med + 2; i < n; i++) {                          \
+    for (unsigned long i = med + 2; i < (unsigned long)n; i++) {           \
       if (x[i] < b) b = x[i];                                              \
     }                                                                      \
     return ((double)a + (double)b) / 2.0;                                  \
@@ -129,17 +140,17 @@ double i64quickselect(int64_t *x, int n)
 double dfloyd_rivest(double *x, int n)
 {
   double a, b;
-  FLOYD_RIVEST_BODY(dswap, double);
+  FLOYD_RIVEST_BODY(double, dfloyd_rivest_select);
 }
 
 double ifloyd_rivest(int *x, int n)
 {
   int a, b;
-  FLOYD_RIVEST_BODY(iswap, int);
+  FLOYD_RIVEST_BODY(int, ifloyd_rivest_select);
 }
 
 double i64floyd_rivest(int64_t *x, int n)
 {
   int64_t a, b;
-  FLOYD_RIVEST_BODY(i64swap, int64_t);
+  FLOYD_RIVEST_BODY(int64_t, i64floyd_rivest_select);
 }
