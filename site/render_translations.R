165d2000a119f749429331ad250412fe2b8c73f7
Benjamin Schwendinger <benjaminschwe@gmail.com>
2026-01-22 13:18:22 +0100

    add translations render

---
+++ site/render_translations.R
@@ -0,0 +1,18 @@
+files <- list.files("vignettes", "\\.Rmd$", recursive = TRUE, full.names = TRUE)
+translated <- files[dirname(files) != "vignettes"]
+
+if (length(translated) == 0L) {
+  cat("No translated vignettes found\n")
+  quit("no")
+}
+
+cat(sprintf("Found %d translated vignettes\n", length(translated)))
+
+for (f in translated) {
+  out <- file.path("website/articles", sub("\\.Rmd$", ".html", sub("^vignettes/", "", f)))
+  dir.create(dirname(out), recursive = TRUE, showWarnings = FALSE)
+  cat(sprintf("  Rendering: %s -> %s\n", f, out))
+  litedown::fuse(f, output = out)
+}
+
+cat("Done\n")
