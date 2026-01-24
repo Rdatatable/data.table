0a8609d542bebccb8fbcbaf72ba7a2e223a5b07e
Ivan K <krylov.r00t@gmail.com>
2026-01-24 17:50:31 +0300

    Be even stricter under Valgrind

--- tests/froll.R
+++ tests/froll.R
@@ -2,9 +2,9 @@
 # exec() is problematic for Valgrind. frollapply() uses
 # parallel::mcparallel(), which causes Valgrind to run
 # Rstd_ReadConsole() incorrectly.
-Sys.setenv(OMP_THREAD_LIMIT = Sys.getenv("OMP_THREAD_LIMIT",
-  if (grepl("valgrind", Sys.getenv("LD_PRELOAD"))) "1" else "2"
-))
+Sys.setenv(OMP_THREAD_LIMIT =
+  if (grepl("valgrind", Sys.getenv("LD_PRELOAD"))) "1" else Sys.getenv("OMP_THREAD_LIMIT", "2")
+)
 require(data.table)
 test.data.table(script="froll.Rraw")
 test.data.table(script="frollBatch.Rraw", optional=TRUE)
