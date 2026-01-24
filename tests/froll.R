1f3b53d84a0780cef35d6c515073b75693957852
Ivan K <krylov.r00t@gmail.com>
2026-01-24 14:26:30 +0300

    tests/froll.R: disable mcparallel under Valgrind

--- tests/froll.R
+++ tests/froll.R
@@ -1,4 +1,10 @@
-Sys.setenv(OMP_THREAD_LIMIT = Sys.getenv("OMP_THREAD_LIMIT", "2"))
+# Valgrind developers say that performing work after fork() without
+# exec() is problematic for Valgrind. frollapply() uses
+# parallel::mcparallel(), which causes Valgrind to run
+# Rstd_ReadConsole() incorrectly.
+Sys.setenv(OMP_THREAD_LIMIT = Sys.getenv("OMP_THREAD_LIMIT",
+  if (grepl("valgrind", Sys.getenv("LD_PRELOAD"))) "1" else "2"
+))
 require(data.table)
 test.data.table(script="froll.Rraw")
 test.data.table(script="frollBatch.Rraw", optional=TRUE)
