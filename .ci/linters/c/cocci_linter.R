5121ef57fbe71e18f4b20a5c9c7290cca705fde3
Ivan K <krylov.r00t@gmail.com>
2026-01-25 11:06:26 +0300

    Revert "cocci_linter.R: fix zero-length error reports"
    This reverts commit 70d37da5a8548d67337ebade6e8a25fc64bdf9df.

--- .ci/linters/c/cocci_linter.R
+++ .ci/linters/c/cocci_linter.R
@@ -10,12 +10,12 @@ cocci_linter = if (!nzchar(Sys.which("spatch"))) function(...) {} else function(
       stdout = TRUE, stderr = FALSE
     )
     if (length(out) > 0) {
-      cat(sprintf("In file '%s', Coccinelle linter '%s' located the following problems:\n", c_obj$c_obj, spfile))
+      cat(sprintf("In file '%s', Coccinelle linter '%s' located the following problems:\n", c_obj$path, spfile))
       writeLines(out)
       bad = TRUE
     }
     if (!is.null(status <- attr(out, 'status'))) {
-      cat(sprintf("While working on file '%s', Coccinelle linter '%s' failed with exit code %d:\n", c_obj$c_obj, spfile, status))
+      cat(sprintf("While working on file '%s', Coccinelle linter '%s' failed with exit code %d:\n", c_obj$path, spfile, status))
       bad = TRUE
     }
   }
