a6b70c7628bec8699d35160679b01e4c9547408a
Benjamin Schwendinger <benjaminschwe@gmail.com>
2026-01-26 11:59:39 +0100

    adjust paths

--- vignettes/_translation_links.R
+++ vignettes/_translation_links.R
@@ -2,17 +2,16 @@
 # idea is to look like 'Other languages: en | fr | de'
 .write.translation.links = function(fmt) {
     url = "https://rdatatable.gitlab.io/data.table/articles"
-    path = dirname(litedown::get_context("input"))
+    context = litedown::get_context("input")
+    path = dirname(context)
     if (basename(path) == "vignettes") {
       lang = "en"
+      path = "."
     } else {
       lang = basename(path)
-      path = dirname(path)
+      path = ".."
     }
-    translation = dir(path,
-      recursive = TRUE,
-      pattern = glob2rx(litedown::get_context("input"))
-    )
+    translation = dir(path = path, recursive = TRUE, pattern = basename(context))
     transl_lang = ifelse(dirname(translation) == ".", "en", dirname(translation))
     block = if (!all(transl_lang == lang)) {
       linked_transl = sprintf("[%s](%s)", transl_lang, file.path(url, sub("(?i)\\.Rmd$", ".html", translation)))
