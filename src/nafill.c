6cce6fdfe52607d1f66445388426ec6d6748a5a8
Benjamin Schwendinger <benjaminschwe@gmail.com>
2026-05-14 15:51:52 +0200

    add comment about any_char parallel guard

--- src/nafill.c
+++ src/nafill.c
@@ -246,6 +246,7 @@ SEXP nafillR(SEXP obj, SEXP type, SEXP fill, SEXP nan_is_na_arg, SEXP inplace, S
       fillp[i] = SEXPPTR_RO(VECTOR_ELT(fill, i)); // do like this so we can use in parallel region
     }
   }
+  // need any_char guard to avoid parallelizing on character columns
   #pragma omp parallel for if (nx>1 && !any_char) num_threads(getDTthreads(nx, true))
   for (R_len_t i=0; i<nx; i++) {
     switch (TYPEOF(VECTOR_ELT(x, i))) {
