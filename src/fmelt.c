1b998b10e8d6cef62909a1276686ac16c98f5c10
Benjamin Schwendinger <52290390+ben-schwen@users.noreply.github.com>
2026-05-28 20:57:58 +0200

    add setallocrow (#7644)
    * add setallocrow
    
    * fix copyAsPlain args
    
    * change allocrow(dt, n) n parameter to specify number of nrows and not number of additional rows
    
    * remove repetition in docs, note address change

--- src/fmelt.c
+++ src/fmelt.c
@@ -811,7 +811,7 @@ SEXP fmelt(SEXP DT, SEXP id, SEXP measure, SEXP varfactor, SEXP valfactor, SEXP 
   // edge case no measure.vars
   if (!data.lmax) {
     SEXP tt = PROTECT(shallowwrapper(DT, data.idcols)); protecti++;
-    ans = PROTECT(copyAsPlain(tt)); protecti++;
+    ans = PROTECT(copyAsPlain(tt, -1)); protecti++;
   } else {
     ansvals = PROTECT(getvaluecols(DT, dtnames, LOGICAL(valfactor)[0], verbose, &data)); protecti++;
     ansvars = PROTECT(getvarcols(DT, dtnames, LOGICAL(varfactor)[0], verbose, &data)); protecti++;
