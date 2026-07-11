d428bd94f2f8d2e7a3a068229d555f8b10ea2116
Jan Gorecki <j.gorecki@wit.edu.pl>
2026-07-11 11:22:29 +0200

    check for non-negative, tests

--- src/frollapply.c
+++ src/frollapply.c
@@ -90,8 +90,8 @@ SEXP copyAsGrowable(SEXP x) {
 
 SEXP resizeVector(SEXP x, SEXP size) {
   if (!(
-    isInteger(size) && LENGTH(size) == 1 && INTEGER_RO(size)[0] != NA_INTEGER
-  )) error(_("'size' must be length 1 non-NA integer value."));
+    isInteger(size) && LENGTH(size) == 1 && INTEGER_RO(size)[0] >= 0
+  )) error(_("'size' must be length 1 non-NA non-negative integer value."));
   if (!isVector(x))
     error(_("'x' must be a vector."));
   R_xlen_t newlen = INTEGER_RO(size)[0];
