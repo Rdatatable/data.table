14af4f478ca58657b97c848413a254d0d826f359
venom1204 <venomplays1204@gmail.com>
2026-07-25 14:48:07 +0000

    ..

--- R/shift.R
+++ R/shift.R
@@ -31,7 +31,7 @@ nafill = function(x, type=c("const", "locf", "nocb"), fill=NA, nan=NA, limit=Inf
   if (!is.numeric(limit) || length(limit) != 1L || is.na(limit) || limit < 0)
      stopf("limit must be a non-negative scalar numeric")
 
-  .Call(CnafillR, x, type, fill, nan_is_na(nan), FALSE, NULL, as.double(limit))
+  .Call(CnafillR, x, type, fill, nan_is_na(nan), FALSE, NULL, as.double(floor(limit)))
 }
 
 setnafill = function(x, type=c("const", "locf", "nocb"), fill=NA, nan=NA, cols=seq_along(x), limit=Inf) {
@@ -39,5 +39,5 @@ setnafill = function(x, type=c("const", "locf", "nocb"), fill=NA, nan=NA, cols=s
   if (!is.numeric(limit) || length(limit) != 1L || is.na(limit) || limit < 0)
      stopf("limit must be a non-negative scalar numeric")
 
-  invisible(.Call(CnafillR, x, type, fill, nan_is_na(nan), TRUE, cols, as.double(limit)))
+  invisible(.Call(CnafillR, x, type, fill, nan_is_na(nan), TRUE, cols, as.double(floor(limit))))
 }
